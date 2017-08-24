23fe994a675c0754a48cd8c0499b8387ff671ee1
Christian Pich <cmpich@zfin.org>
2017-08-24 08:17:45 -0700

    remove special fields for gene search on disease documents

--- src/main/java/org/alliancegenome/indexer/document/disease/DiseaseDocument.java
+++ src/main/java/org/alliancegenome/indexer/document/disease/DiseaseDocument.java
@@ -5,9 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.alliancegenome.indexer.document.ESDocument;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Getter
 @Setter
@@ -16,29 +14,12 @@ public class DiseaseDocument extends ESDocument {
     private String doId;
     private String primaryKey;
     private String name;
+    private String definition;
     private String species;
     private List<AnnotationDocument> annotations;
     private List<DiseaseDocument> parents;
     private List<DiseaseDocument> children;
     private List<String> synonyms;
-
-    // gene-search related fields
-    private Set<String> geneNames = new HashSet<>();
-    private Set<String> geneSymbols = new HashSet<>();
-    private Set<String> geneAliases = new HashSet<>();
-
-    public void addGeneName(String name) {
-        geneNames.add(name);
-    }
-
-    public void addGeneSymbol(String symbol) {
-        this.geneSymbols.add(symbol);
-    }
-
-    public void addGeneAliases(Set<String> aliases) {
-        this.geneAliases.addAll(aliases);
-    }
-
 
     @JsonIgnore
     public String getDocumentId() {
