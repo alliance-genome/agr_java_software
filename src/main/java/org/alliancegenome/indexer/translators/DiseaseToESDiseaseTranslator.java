ee68b248f46c53d9bae77c37a436b7e467983bc9
Olin Blodgett <nilo@nilosplace.net>
2017-08-24 19:12:22 -0400

    Removed log lines

--- src/main/java/org/alliancegenome/indexer/translators/DiseaseToESDiseaseTranslator.java
+++ src/main/java/org/alliancegenome/indexer/translators/DiseaseToESDiseaseTranslator.java
@@ -14,90 +14,90 @@ import java.util.stream.Collectors;
 
 public class DiseaseToESDiseaseTranslator extends EntityDocumentTranslator<DOTerm, DiseaseDocument> {
 
-    private GeneTranslator geneTranslator = new GeneTranslator();
+	private GeneTranslator geneTranslator = new GeneTranslator();
 
-    private Logger log = LogManager.getLogger(getClass());
+	private Logger log = LogManager.getLogger(getClass());
 
-    @Override
-    protected DiseaseDocument entityToDocument(DOTerm entity) {
+	@Override
+	protected DiseaseDocument entityToDocument(DOTerm entity) {
 
-        log.info(entity);
+		//log.info(entity);
 
-        DiseaseDocument doc = getTermDiseaseDocument(entity);
+		DiseaseDocument doc = getTermDiseaseDocument(entity);
 
-        // generate AnnotationDocument records
-        List<AnnotationDocument> annotationDocuments = entity.getAnnotations()
-                .stream().map(annotation -> {
-                    AnnotationDocument document = new AnnotationDocument();
-                    document.setGeneDocument(geneTranslator.entityToDocument(annotation.getGene()));
+		// generate AnnotationDocument records
+		List<AnnotationDocument> annotationDocuments = entity.getAnnotations()
+				.stream().map(annotation -> {
+					AnnotationDocument document = new AnnotationDocument();
+					document.setGeneDocument(geneTranslator.entityToDocument(annotation.getGene()));
 
-                    List<PublicationDocument> pubDocuments = annotation.getPublications().stream()
-                            .map(publication -> {
-                                PublicationDocument pubDoc = new PublicationDocument();
-                                pubDoc.setPrimaryKey(publication.getPrimaryKey());
-                                pubDoc.setPubMedId(publication.getPubMedId());
-                                pubDoc.setPubModId(publication.getPubModId());
-                                pubDoc.setPubModUrl(publication.getPubModUrl());
-                                List<String> evidencesDocument = publication.getEvidence().stream()
-                                        .map(EvidenceCode::getPrimaryKey)
-                                        .collect(Collectors.toList());
-                                pubDoc.setEvidenceCodes(evidencesDocument);
-                                return pubDoc;
-                            })
-                            .collect(Collectors.toList());
-                    document.setPublications(pubDocuments);
-                    return document;
-                })
-                .collect(Collectors.toList());
-        doc.setAnnotations(annotationDocuments);
+					List<PublicationDocument> pubDocuments = annotation.getPublications().stream()
+							.map(publication -> {
+								PublicationDocument pubDoc = new PublicationDocument();
+								pubDoc.setPrimaryKey(publication.getPrimaryKey());
+								pubDoc.setPubMedId(publication.getPubMedId());
+								pubDoc.setPubModId(publication.getPubModId());
+								pubDoc.setPubModUrl(publication.getPubModUrl());
+								List<String> evidencesDocument = publication.getEvidence().stream()
+										.map(EvidenceCode::getPrimaryKey)
+										.collect(Collectors.toList());
+								pubDoc.setEvidenceCodes(evidencesDocument);
+								return pubDoc;
+							})
+							.collect(Collectors.toList());
+					document.setPublications(pubDocuments);
+					return document;
+				})
+				.collect(Collectors.toList());
+		doc.setAnnotations(annotationDocuments);
 
-        return doc;
-    }
+		return doc;
+	}
 
-    private DiseaseDocument getTermDiseaseDocument(DOTerm doTerm) {
-        return getTermDiseaseDocument(doTerm, false);
-    }
+	private DiseaseDocument getTermDiseaseDocument(DOTerm doTerm) {
+		return getTermDiseaseDocument(doTerm, false);
+	}
 
 
-    private DiseaseDocument getTermDiseaseDocument(DOTerm doTerm, boolean shallow) {
-        DiseaseDocument document = new DiseaseDocument();
-        if (doTerm.getDoId() != null)
-            document.setDoId(doTerm.getDoId());
-        document.setPrimaryKey(doTerm.getPrimaryKey());
-        document.setName(doTerm.getName());
-        document.setDefinition(doTerm.getDefinition());
-        if(doTerm.getSynonyms() != null) {
-            List<String> synonymList = doTerm.getSynonyms().stream()
-                    .map(Synonym::getPrimaryKey)
-                    .collect(Collectors.toList());
-            document.setSynonyms(synonymList);
-        }
-        if (shallow)
-            return document;
+	private DiseaseDocument getTermDiseaseDocument(DOTerm doTerm, boolean shallow) {
+		DiseaseDocument document = new DiseaseDocument();
+		if (doTerm.getDoId() != null)
+			document.setDoId(doTerm.getDoId());
+		document.setPrimaryKey(doTerm.getPrimaryKey());
+		document.setName(doTerm.getName());
+		document.setDefinition(doTerm.getDefinition());
+		if(doTerm.getSynonyms() != null) {
+			List<String> synonymList = doTerm.getSynonyms().stream()
+					.map(Synonym::getPrimaryKey)
+					.collect(Collectors.toList());
+			document.setSynonyms(synonymList);
+		}
+		if (shallow)
+			return document;
 
-        // set parents
-        if (doTerm.getParents() != null) {
-            List<DiseaseDocument> parentDocs = doTerm.getParents().stream()
-                    .map(term -> getTermDiseaseDocument(doTerm, true))
-                    .collect(Collectors.toList());
-            document.setParents(parentDocs);
-        }
+		// set parents
+		if (doTerm.getParents() != null) {
+			List<DiseaseDocument> parentDocs = doTerm.getParents().stream()
+					.map(term -> getTermDiseaseDocument(doTerm, true))
+					.collect(Collectors.toList());
+			document.setParents(parentDocs);
+		}
 
-        // set children
-        if (doTerm.getChildren() != null) {
-            List<DiseaseDocument> childrenDocs = doTerm.getChildren().stream()
-                    .map(term -> getTermDiseaseDocument(doTerm, true))
-                    .collect(Collectors.toList());
-            document.setChildren(childrenDocs);
-        }
+		// set children
+		if (doTerm.getChildren() != null) {
+			List<DiseaseDocument> childrenDocs = doTerm.getChildren().stream()
+					.map(term -> getTermDiseaseDocument(doTerm, true))
+					.collect(Collectors.toList());
+			document.setChildren(childrenDocs);
+		}
 
-        return document;
-    }
+		return document;
+	}
 
-    @Override
-    protected DOTerm documentToEntity(DiseaseDocument document) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	@Override
+	protected DOTerm documentToEntity(DiseaseDocument document) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
