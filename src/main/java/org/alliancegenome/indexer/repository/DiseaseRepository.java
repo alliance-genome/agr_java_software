af9d060621a14ca2df4307ceba77061a732c7e4a
Christian Pich <cmpich@zfin.org>
2017-09-12 11:31:38 -0700

    add all parents for given disease on diseaseAnnotation records

--- src/main/java/org/alliancegenome/indexer/repository/DiseaseRepository.java
+++ src/main/java/org/alliancegenome/indexer/repository/DiseaseRepository.java
@@ -13,13 +13,13 @@ public class DiseaseRepository extends Neo4jRepository<DOTerm> {
     public List<DOTerm> getAllDiseaseTerms(int start, int maxSize) {
         String cypher = "match (root:DOTerm) WITH root SKIP " + start + " LIMIT " + maxSize + " " +
                 "optional match (a:Association)-[q:ASSOCIATION]->(root), " +
-                "(m:Gene)-[qq:ASSOCIATION]->(a), " +
-                "(p:Publication)<-[qqq*]-(a), " +
-                "(e:EvidenceCode)<-[ee:EVIDENCE]-(a), " +
-                "(s:Species)<-[ss:FROM_SPECIES]-(m), " +
-                "(root)-[ex:ALSO_KNOWN_AS]->(exx:ExternalId), " +
-                "(root)-[synonymRelation:ALSO_KNOWN_AS]->(synonym:Synonym)  " +
-                "return distinct root, q,a,qq,m,qqq,p, ee, e, s, ss, ex, exx ";
+                "(gene:Gene)-[qq:ASSOCIATION]->(a), " +
+                "(publication:Publication)<-[publicationRelation]-(a), " +
+                "(evidence:EvidenceCode)<-[evidenceRelation:EVIDENCE]-(a), " +
+                "(species:Species)<-[speciesRelation:FROM_SPECIES]-(m), " +
+                "(root)-[externalRelation:ALSO_KNOWN_AS]->(externalId:ExternalId) " +
+                "return distinct root, q,a,qq, gene,publicationRelation, publication, evidenceRelation, evidence, " +
+                "species, speciesRelation, externalId, externalRelation ";
         return (List<DOTerm>) query(cypher);
     }
 
@@ -40,29 +40,26 @@ public class DiseaseRepository extends Neo4jRepository<DOTerm> {
     }
 
     public List<DOTerm> getDiseaseTermsWithAnnotations() {
-        String cypher = "match (root:DOTerm), " +
+        String cypher = "match (root:DOTerm),  " +
                 "(a:Association)-[q:ASSOCIATION]->(root), " +
-                "(m:Gene)-[qq:ASSOCIATION]->(a), " +
-                "(p:Publication)<-[qqq*]-(a), " +
-                "(e:EvidenceCode)<-[ee:EVIDENCE]-(a), " +
-                "(s:Species)<-[ss:FROM_SPECIES]-(m), " +
-                "(root)-[ex:ALSO_KNOWN_AS]->(exx:ExternalId), " +
-                "(root)-[parentChildRelation:IS_A]->(parent:DOTerm) " +
-                "return root, q,a,qq,m,qqq,p, ee, e, s, ss, ex, exx, parent, parentChildRelation " ;
-/*
-        cypher = "match (do:DOTerm)-[r]-(gene:Gene), (do)-[rr]-(as:Association) return do, gene, collect(gene), collect(as) " +
-                "SKIP " + start + " " +
-                "LIMIT " + maxSize;
-*/
+                "(gene:Gene)-[geneAssociation:ASSOCIATION]->(a), " +
+                "(publication:Publication)<-[qqq]-(a), " +
+                "(evidence:EvidenceCode)<-[evidenceRelation:EVIDENCE]-(a), " +
+                "(species:Species)<-[speciesRelation:FROM_SPECIES]-(gene), " +
+                "(root)-[parentChildRelation:IS_A*]->(parent:DOTerm) " +
+                "return root, q,a, geneAssociation,gene ,qqq, publication, evidenceRelation, evidence, " +
+                "species, speciesRelation, parentChildRelation, parent";
+
         return (List<DOTerm>) query(cypher);
     }
 
     public List<DOTerm> getAllTerms() {
         String cypher = "match (root:DOTerm) " +
                 "optional match (parent:DOTerm)<-[parentRelation:IS_A]-(root:DOTerm), " +
-                "(child:DOTerm)-[childRelation:IS_A]->(root:DOTerm) " +
+                "(child:DOTerm)-[childRelation:IS_A]->(root:DOTerm), " +
+                "(synonym:Synonym)-[synonymRelation:ALSO_KNOWN_AS]-(root:DOTerm) " +
                 "return root, parent, " +
-                "parentRelation, child, childRelation";
+                "parentRelation, child, childRelation, synonym, synonymRelation";
         return (List<DOTerm>) query(cypher);
     }
 
--- src/main/java/org/alliancegenome/indexer/translators/DiseaseTranslator.java
+++ src/main/java/org/alliancegenome/indexer/translators/DiseaseTranslator.java
@@ -14,188 +14,194 @@ import static java.util.stream.Collectors.groupingBy;
 
 public class DiseaseTranslator extends EntityDocumentTranslator<DOTerm, DiseaseDocument> {
 
-	private GeneTranslator geneTranslator = new GeneTranslator();
-	private Map<String, DOTerm> fullDiseaseListMap = new HashMap<>(12000);
+    private GeneTranslator geneTranslator = new GeneTranslator();
+    private Map<String, DOTerm> fullDiseaseListMap = new HashMap<>(12000);
 
-	private Logger log = LogManager.getLogger(getClass());
+    private Logger log = LogManager.getLogger(getClass());
 
-	@Override
-	protected DiseaseDocument entityToDocument(DOTerm entity, int translationDepth) {
-		if (fullDiseaseListMap.isEmpty())
-			fetchFullList();
+    @Override
+    protected DiseaseDocument entityToDocument(DOTerm entity, int translationDepth) {
+        if (fullDiseaseListMap.isEmpty())
+            fetchFullList();
 
-		populateParentsAndChildren(entity);
-		DiseaseDocument doc = getTermDiseaseDocument(entity);
+        populateParentsAndChildren(entity);
+        DiseaseDocument doc = getTermDiseaseDocument(entity);
 
-		doc.setCategory("disease");
+        if (entity.getDiseaseGeneJoins() == null)
+            return doc;
 
-		if (entity.getDiseaseGeneJoins() == null) {
-			return doc;
-		}
+        // group by gene
+        Map<Gene, List<DiseaseGeneJoin>> geneAssociationMap = entity.getDiseaseGeneJoins().stream()
+                .collect(
+                        groupingBy(DiseaseGeneJoin::getGene,
+                                Collectors.mapping(association -> association, Collectors.toList())
+                        )
+                );
 
-		// group by gene
-		Map<Gene, List<DiseaseGeneJoin>> geneAssociationMap = entity.getDiseaseGeneJoins().stream()
-				.collect(
-						groupingBy(DiseaseGeneJoin::getGene,
-								Collectors.mapping(association -> association, Collectors.toList())
-								)
-						);
-		
-		// generate AnnotationDocument records
-		List<AnnotationDocument> annotationDocuments = geneAssociationMap.entrySet().stream()
-				// sort by gene symbol
-				.sorted(Map.Entry.comparingByKey())
-				.map(entry -> {
-					AnnotationDocument document = new AnnotationDocument();
-					if(translationDepth > 0) {
-						document.setGeneDocument(geneTranslator.translate(entry.getKey(), translationDepth - 1)); // This needs to not happen if being call from GeneTranslator
-					}
-					List<PublicationDoclet> publicationDocuments = entry.getValue().stream()
-							.map(association -> {
-								document.setAssoicationType(association.getJoinType());
-								Publication publication = association.getPublication();
-								return getPublicationDocument(association, publication);
-							})
-							.collect(Collectors.toList());
-					document.setPublications(publicationDocuments);
-					return document;
-				})
-				.collect(Collectors.toList());
+        // generate AnnotationDocument records
+        List<AnnotationDocument> annotationDocuments = geneAssociationMap.entrySet().stream()
+                // sort by gene symbol
+                .sorted(Map.Entry.comparingByKey())
+                .map(entry -> {
+                    AnnotationDocument document = new AnnotationDocument();
+                    if (translationDepth > 0) {
+                        document.setGeneDocument(geneTranslator.translate(entry.getKey(), translationDepth - 1)); // This needs to not happen if being call from GeneTranslator
+                    }
+                    List<PublicationDoclet> publicationDocuments = entry.getValue().stream()
+                            .map(association -> {
+                                document.setAssoicationType(association.getJoinType());
+                                Publication publication = association.getPublication();
+                                return getPublicationDocument(association, publication);
+                            })
+                            .collect(Collectors.toList());
+                    document.setPublications(publicationDocuments);
+                    return document;
+                })
+                .collect(Collectors.toList());
 
-		doc.setAnnotations(annotationDocuments);
+        doc.setAnnotations(annotationDocuments);
 
-		return doc;
-	}
+        return doc;
+    }
 
-	private void populateParentsAndChildren(DOTerm entity) {
-		DOTerm term = fullDiseaseListMap.get(entity.getPrimaryKey());
-		if (term == null)
-			return;
+    private void populateParentsAndChildren(DOTerm entity) {
+        DOTerm term = fullDiseaseListMap.get(entity.getPrimaryKey());
+        if (term == null)
+            return;
 
-		entity.setChildren(term.getChildren());
-		entity.setParents(term.getParents());
-		entity.setSynonyms(term.getSynonyms());
-	}
+        entity.setChildren(term.getChildren());
+        entity.setParents(term.getParents());
+        entity.setSynonyms(term.getSynonyms());
+    }
 
-	private void fetchFullList() {
-		DiseaseRepository diseaseRepository = new DiseaseRepository();
-		fullDiseaseListMap = diseaseRepository.getAllTerms().stream()
-				.collect((Collectors.toMap(DOTerm::getPrimaryKey, id -> id)));
-	}
+    private void fetchFullList() {
+        DiseaseRepository diseaseRepository = new DiseaseRepository();
+        fullDiseaseListMap = diseaseRepository.getAllTerms().stream()
+                .collect((Collectors.toMap(DOTerm::getPrimaryKey, id -> id)));
+    }
 
-	private PublicationDoclet getPublicationDocument(DiseaseGeneJoin association, Publication publication) {
-		PublicationDoclet pubDoc = new PublicationDoclet();
-		pubDoc.setPrimaryKey(publication.getPrimaryKey());
-		pubDoc.setPubMedId(publication.getPubMedId());
-		pubDoc.setPubModId(publication.getPubModId());
-		pubDoc.setPubModUrl(publication.getPubModUrl());
-		List<String> evidencesDocument = association.getEvidenceCodes().stream()
-				.map(EvidenceCode::getPrimaryKey)
-				.collect(Collectors.toList());
-		pubDoc.setEvidenceCodes(evidencesDocument);
-		return pubDoc;
-	}
+    private PublicationDoclet getPublicationDocument(DiseaseGeneJoin association, Publication publication) {
+        PublicationDoclet pubDoc = new PublicationDoclet();
+        pubDoc.setPrimaryKey(publication.getPrimaryKey());
+        pubDoc.setPubMedId(publication.getPubMedId());
+        pubDoc.setPubModId(publication.getPubModId());
+        pubDoc.setPubModUrl(publication.getPubModUrl());
+        List<String> evidencesDocument = association.getEvidenceCodes().stream()
+                .map(EvidenceCode::getPrimaryKey)
+                .collect(Collectors.toList());
+        pubDoc.setEvidenceCodes(evidencesDocument);
+        return pubDoc;
+    }
 
-	private DiseaseDocument getTermDiseaseDocument(DOTerm doTerm) {
-		return getTermDiseaseDocument(doTerm, false);
-	}
+    private DiseaseDocument getTermDiseaseDocument(DOTerm doTerm) {
+        return getTermDiseaseDocument(doTerm, false);
+    }
 
 
-	private DiseaseDocument getTermDiseaseDocument(DOTerm doTerm, boolean shallow) {
-		DiseaseDocument document = new DiseaseDocument();
-		if (doTerm.getDoId() != null)
-			document.setDoId(doTerm.getDoId());
-		document.setPrimaryKey(doTerm.getPrimaryKey());
-		document.setPrimaryId(doTerm.getPrimaryKey());
-		document.setName(doTerm.getName());
-		document.setNameKey(doTerm.getName());
-		document.setDefinition(doTerm.getDefinition());
-		if (doTerm.getSynonyms() != null) {
-			List<String> synonymList = doTerm.getSynonyms().stream()
-					.map(Synonym::getPrimaryKey)
-					.collect(Collectors.toList());
-			document.setSynonyms(synonymList);
-		}
-		// add External Ids
-		if (doTerm.getExternalIds() != null) {
-			List<String> externalIds = doTerm.getExternalIds().stream()
-					.map(ExternalId::getPrimaryKey)
-					.collect(Collectors.toList());
-			document.setExternal_ids(externalIds);
-		}
-		if (shallow)
-			return document;
+    private DiseaseDocument getTermDiseaseDocument(DOTerm doTerm, boolean shallow) {
+        DiseaseDocument document = new DiseaseDocument();
+        if (doTerm.getDoId() != null)
+            document.setDoId(doTerm.getDoId());
+        document.setPrimaryKey(doTerm.getPrimaryKey());
+        document.setPrimaryId(doTerm.getPrimaryKey());
+        document.setName(doTerm.getName());
+        document.setNameKey(doTerm.getName());
+        document.setDefinition(doTerm.getDefinition());
+        if (doTerm.getSynonyms() != null) {
+            List<String> synonymList = doTerm.getSynonyms().stream()
+                    .map(Synonym::getPrimaryKey)
+                    .collect(Collectors.toList());
+            document.setSynonyms(synonymList);
+        }
+        // add External Ids
+        if (doTerm.getExternalIds() != null) {
+            List<String> externalIds = doTerm.getExternalIds().stream()
+                    .map(ExternalId::getPrimaryKey)
+                    .collect(Collectors.toList());
+            document.setExternal_ids(externalIds);
+        }
+        if (shallow)
+            return document;
 
-		// set parents
-		if (doTerm.getParents() != null) {
-			List<DiseaseDocument> parentDocs = doTerm.getParents().stream()
-					.map(term -> getTermDiseaseDocument(term, true))
-					.collect(Collectors.toList());
-			document.setParents(parentDocs);
-		}
+        // set parents
+        if (doTerm.getParents() != null) {
+            List<DiseaseDocument> parentDocs = doTerm.getParents().stream()
+                    .map(term -> getTermDiseaseDocument(term, true))
+                    .collect(Collectors.toList());
+            document.setParents(parentDocs);
+        }
 
-		// set children
-		if (doTerm.getChildren() != null) {
-			List<DiseaseDocument> childrenDocs = doTerm.getChildren().stream()
-					.map(term -> getTermDiseaseDocument(term, true))
-					.collect(Collectors.toList());
-			document.setChildren(childrenDocs);
-		}
+        // set children
+        if (doTerm.getChildren() != null) {
+            List<DiseaseDocument> childrenDocs = doTerm.getChildren().stream()
+                    .map(term -> getTermDiseaseDocument(term, true))
+                    .collect(Collectors.toList());
+            document.setChildren(childrenDocs);
+        }
 
-		return document;
-	}
+        return document;
+    }
 
-	@Override
-	protected DOTerm documentToEntity(DiseaseDocument document, int translationDepth) {
-		return null;
-	}
+    @Override
+    protected DOTerm documentToEntity(DiseaseDocument document, int translationDepth) {
+        return null;
+    }
 
-	public Iterable<DiseaseAnnotationDocument> translateAnnotationEntities(List<DOTerm> geneDiseaseList, int translationDepth) {
-		Set<DiseaseAnnotationDocument> diseaseAnnotationDocuments = new HashSet<>();
-		geneDiseaseList.forEach(doTerm -> {
-			if (doTerm.getDiseaseGeneJoins() == null) {
-				DiseaseAnnotationDocument doc = new DiseaseAnnotationDocument();
-				doc.setPrimaryKey(doTerm.getPrimaryKey());
-				doc.setDiseaseID(doTerm.getPrimaryKey());
-				doc.setDiseaseName(doTerm.getName());
-				diseaseAnnotationDocuments.add(doc);
-			} else {
-				Set<DiseaseAnnotationDocument> docSet = doTerm.getDiseaseGeneJoins().stream()
-						.map(diseaseGeneJoin -> {
-							DiseaseAnnotationDocument doc = new DiseaseAnnotationDocument();
-							doc.setPrimaryKey(doTerm.getPrimaryKey() + ":" + diseaseGeneJoin.getGene().getPrimaryKey());
-							doc.setDiseaseName(doTerm.getName());
-							doc.setDiseaseID(doTerm.getPrimaryKey());
-							doc.setParentDiseaseIDs(getParentIdList(doTerm));
-							doc.setAssociationType(diseaseGeneJoin.getJoinType());
-							doc.setSpecies(getSpeciesDoclet(diseaseGeneJoin));
-							doc.setGeneDocument(geneTranslator.entityToDocument(diseaseGeneJoin.getGene(), translationDepth - 1));
-							List<PublicationDoclet> pubDocs = new ArrayList<>();
-							pubDocs.add(getPublicationDocument(diseaseGeneJoin, diseaseGeneJoin.getPublication()));
-							doc.setPublications(pubDocs);
-							return doc;
-						})
-						.collect(Collectors.toSet());
-				diseaseAnnotationDocuments.addAll(docSet);
-			}
-		});
-		return diseaseAnnotationDocuments;
-	}
+    public Iterable<DiseaseAnnotationDocument> translateAnnotationEntities(List<DOTerm> geneDiseaseList, int translationDepth) {
+        Set<DiseaseAnnotationDocument> diseaseAnnotationDocuments = new HashSet<>();
+        geneDiseaseList.forEach(doTerm -> {
+            if (doTerm.getDiseaseGeneJoins() == null) {
+                DiseaseAnnotationDocument doc = new DiseaseAnnotationDocument();
+                doc.setPrimaryKey(doTerm.getPrimaryKey());
+                doc.setDiseaseID(doTerm.getPrimaryKey());
+                doc.setDiseaseName(doTerm.getName());
+                diseaseAnnotationDocuments.add(doc);
+            } else {
+                Set<DiseaseAnnotationDocument> docSet = doTerm.getDiseaseGeneJoins().stream()
+                        .map(diseaseGeneJoin -> {
+                            DiseaseAnnotationDocument doc = new DiseaseAnnotationDocument();
+                            doc.setPrimaryKey(doTerm.getPrimaryKey() + ":" + diseaseGeneJoin.getGene().getPrimaryKey());
+                            doc.setDiseaseName(doTerm.getName());
+                            doc.setDiseaseID(doTerm.getPrimaryKey());
+                            doc.setParentDiseaseIDs(getParentIdList(doTerm));
+                            doc.setAssociationType(diseaseGeneJoin.getJoinType());
+                            doc.setSpecies(getSpeciesDoclet(diseaseGeneJoin));
+                            doc.setGeneDocument(geneTranslator.entityToDocument(diseaseGeneJoin.getGene(), translationDepth - 1));
+                            List<PublicationDoclet> pubDocs = new ArrayList<>();
+                            pubDocs.add(getPublicationDocument(diseaseGeneJoin, diseaseGeneJoin.getPublication()));
+                            doc.setPublications(pubDocs);
+                            return doc;
+                        })
+                        .collect(Collectors.toSet());
+                diseaseAnnotationDocuments.addAll(docSet);
+            }
+        });
+        return diseaseAnnotationDocuments;
+    }
 
-	private List<String> getParentIdList(DOTerm doTerm) {
-		List<String> idList = new ArrayList<>();
-		idList.add(doTerm.getPrimaryKey());
-		idList.addAll(doTerm.getParents().stream().map(DOTerm::getPrimaryKey).collect(Collectors.toSet()));
-		return idList;
-	}
+    /**
+     * Get all the parent termIDs compiled.
+     * @param doTerm
+     * @return
+     */
+    private Set<String> getParentIdList(DOTerm doTerm) {
+        Set<String> idList = new LinkedHashSet<>();
+        idList.add(doTerm.getPrimaryKey());
+        doTerm.getParents().forEach(term -> {
+            idList.add(term.getPrimaryKey());
+            if (term.getParents() != null)
+                idList.addAll(getParentIdList(term));
+        });
+        return idList;
+    }
 
-	private SpeciesDoclet getSpeciesDoclet(DiseaseGeneJoin diseaseGeneJoin) {
-		Species species = diseaseGeneJoin.getGene().getSpecies();
-		SpeciesType type = species.getType();
-		SpeciesDoclet doclet = new SpeciesDoclet();
-		doclet.setName(species.getName());
-		doclet.setTaxonID(species.getPrimaryKey());
-		doclet.setOrderID(type.ordinal());
-		return doclet;
-	}
+    private SpeciesDoclet getSpeciesDoclet(DiseaseGeneJoin diseaseGeneJoin) {
+        Species species = diseaseGeneJoin.getGene().getSpecies();
+        SpeciesType type = species.getType();
+        SpeciesDoclet doclet = new SpeciesDoclet();
+        doclet.setName(species.getName());
+        doclet.setTaxonID(species.getPrimaryKey());
+        doclet.setOrderID(type.ordinal());
+        return doclet;
+    }
 }
