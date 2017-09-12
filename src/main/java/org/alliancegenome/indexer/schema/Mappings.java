1833c7fbfb81112935ffc0068a3221a58c98926f
Kevin Schaper <kevinschaper@gmail.com>
2017-09-11 20:04:49 -0700

    AGR-543  Updated all fields that currently index a .raw to also index a .keyword version.  Will remove .raw when the development site has the new index

--- src/main/java/org/alliancegenome/indexer/schema/Mappings.java
+++ src/main/java/org/alliancegenome/indexer/schema/Mappings.java
@@ -11,13 +11,17 @@ public abstract class Mappings extends Builder {
 	public abstract void buildMappings();
 	
 
-	protected void buildGenericField(String name, String type, String analyzer, boolean symbol, boolean autocomplete, boolean raw) throws IOException {
+	protected void buildGenericField(String name, String type, String analyzer, boolean symbol, boolean autocomplete, boolean keyword) throws IOException {
 		builder.startObject(name);
 		if(type != null) builder.field("type", type);
 		if(analyzer != null) builder.field("analyzer", analyzer);
-		if(symbol || autocomplete || raw) {
+		if(symbol || autocomplete || keyword) {
 			builder.startObject("fields");
-			if(raw) buildProperty("raw", "keyword");
+			if(keyword) {
+				//temporarily index into both raw & keyword, for backwards compatibility with api repo
+				buildProperty("keyword", "keyword");
+				buildProperty("raw", "keyword");
+			}
 			if(symbol) buildGenericField("symbol", "text", "symbols", false, false, false);
 			if(autocomplete) buildProperty("autocomplete", "text", "autocomplete", "autocomplete_search");
 			builder.endObject();
