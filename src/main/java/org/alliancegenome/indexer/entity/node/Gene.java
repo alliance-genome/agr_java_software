alliance-genome/agr_java_software

commit 105e4433eadf9af7d4f68e05e36d882f88295a11

handle null evidence code, group by gene and associationtype

src/main/java/org/alliancegenome/indexer/entity/node/Gene.java
@@ -1,90 +1,93 @@
 package org.alliancegenome.indexer.entity.node;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
+import lombok.Getter;
+import lombok.Setter;
 import org.alliancegenome.indexer.entity.Neo4jEntity;
 import org.alliancegenome.indexer.entity.relationship.GenomeLocation;
 import org.alliancegenome.indexer.entity.relationship.Orthologous;
 import org.neo4j.ogm.annotation.NodeEntity;
 import org.neo4j.ogm.annotation.Relationship;
 
-import lombok.Getter;
-import lombok.Setter;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @NodeEntity
 @Getter
 @Setter
 public class Gene extends Neo4jEntity implements Comparable<Gene> {
 
-	private String primaryKey;
-	private String taxonId;
-	private String geneLiterature;
-	private String geneLiteratureUrl;
-	private String geneSynopsis;
-	private String geneSynopsisUrl;
-	private String dataProvider;
-	private String name;
+    private String primaryKey;
+    private String taxonId;
+    private String geneLiterature;
+    private String geneLiteratureUrl;
+    private String geneSynopsis;
+    private String geneSynopsisUrl;
+    private String dataProvider;
+    private String name;
 
-	//private Date dateProduced;
-	private String description;
-	private String symbol;
-	private String geneticEntityExternalUrl;
+    //private Date dateProduced;
+    private String description;
+    private String symbol;
+    private String geneticEntityExternalUrl;
 
-	// Used to be in modCrossReference
-	private String modCrossRefCompleteUrl;
-	private String modLocalId;
-	private String modGlobalCrossRefId;
-	private String modGlobalId;
-	
-	private Entity createdBy;
-	private SOTerm sOTerm;
+    // Used to be in modCrossReference
+    private String modCrossRefCompleteUrl;
+    private String modLocalId;
+    private String modGlobalCrossRefId;
+    private String modGlobalId;
 
-	@Relationship(type = "FROM_SPECIES")
-	private Species species;
+    private Entity createdBy;
+    private SOTerm sOTerm;
 
-	@Relationship(type = "ALSO_KNOWN_AS")
-	private Set<Synonym> synonyms = new HashSet<>();
+    @Relationship(type = "FROM_SPECIES")
+    private Species species;
 
-	@Relationship(type = "ALSO_KNOWN_AS")
-	private Set<SecondaryId> secondaryIds = new HashSet<>();
+    @Relationship(type = "ALSO_KNOWN_AS")
+    private Set<Synonym> synonyms = new HashSet<>();
 
-	@Relationship(type = "ANNOTATED_TO")
-	private Set<GOTerm> gOTerms = new HashSet<>();
+    @Relationship(type = "ALSO_KNOWN_AS")
+    private Set<SecondaryId> secondaryIds = new HashSet<>();
 
-	@Relationship(type = "IS_IMPLICATED_IN")
-	private Set<DOTerm> dOTerms = new HashSet<>();
-	
-	@Relationship(type = "ORTHOLOGOUS", direction=Relationship.OUTGOING)
-	private List<Orthologous> orthoGenes;
-	
-	@Relationship(type = "LOCATED_ON")
-	private List<GenomeLocation> genomeLocations;
+    @Relationship(type = "ANNOTATED_TO")
+    private Set<GOTerm> gOTerms = new HashSet<>();
 
-	@Relationship(type = "CROSS_REFERENCE")
-	private List<CrossReference> crossReferences;
-	
-	@Relationship(type = "ASSOCIATION", direction=Relationship.UNDIRECTED)
-	private List<DiseaseGeneJoin> diseaseGeneJoins = new ArrayList<DiseaseGeneJoin>();
-	@Relationship(type = "ASSOCIATION", direction=Relationship.UNDIRECTED)
-	private List<OrthologyGeneJoin> orthologyGeneJoins = new ArrayList<OrthologyGeneJoin>();
-	
+    @Relationship(type = "IS_IMPLICATED_IN")
+    private Set<DOTerm> dOTerms = new HashSet<>();
 
-	@Override
-	public int compareTo(Gene gene) {
-		if (gene == null)
-			return -1;
-		if (!species.equals(gene.species))
-			return species.compareTo(gene.species);
-		if (symbol == null && gene.getSymbol() == null)
-			return 0;
-		if (symbol == null)
-			return 1;
-		if (gene.symbol == null)
-			return -1;
-		return symbol.compareTo(gene.getSymbol());
-	}
+    @Relationship(type = "ORTHOLOGOUS", direction = Relationship.UNDIRECTED)
+    private List<Orthologous> orthoGenes;
+
+    @Relationship(type = "LOCATED_ON")
+    private List<GenomeLocation> genomeLocations;
+
+    @Relationship(type = "CROSS_REFERENCE")
+    private List<CrossReference> crossReferences;
+
+    @Relationship(type = "ASSOCIATION", direction = Relationship.UNDIRECTED)
+    private List<DiseaseGeneJoin> diseaseGeneJoins = new ArrayList<DiseaseGeneJoin>();
+    @Relationship(type = "ASSOCIATION", direction = Relationship.UNDIRECTED)
+    private List<OrthologyGeneJoin> orthologyGeneJoins = new ArrayList<OrthologyGeneJoin>();
+
+
+    @Override
+    public int compareTo(Gene gene) {
+        if (gene == null)
+            return -1;
+        if (species == null && gene.getSpecies() != null)
+            return -1;
+        if (species != null && gene.getSpecies() == null)
+            return 1;
+        if (species != null && gene.getSpecies() != null && !species.equals(gene.species))
+            return species.compareTo(gene.species);
+        if (symbol == null && gene.getSymbol() == null)
+            return 0;
+        if (symbol == null)
+            return 1;
+        if (gene.symbol == null)
+            return -1;
+        return symbol.compareTo(gene.getSymbol());
+    }
 
 }

src/main/java/org/alliancegenome/indexer/document/AnnotationDocument.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 public class AnnotationDocument extends ESDocument implements Comparable<AnnotationDocument> {
 	
 	private String primaryKey;
-	private String assoicationType;
+	private String associationType;
 	private GeneDocument geneDocument;
 	private List<PublicationDoclet> publications;
 

src/main/java/org/alliancegenome/indexer/repository/DiseaseRepository.java
@@ -1,7 +1,9 @@
 package org.alliancegenome.indexer.repository;
 
 import org.alliancegenome.indexer.entity.node.DOTerm;
+import org.alliancegenome.indexer.entity.node.DiseaseGeneJoin;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class DiseaseRepository extends Neo4jRepository<DOTerm> {
@@ -74,5 +76,21 @@ public class DiseaseRepository extends Neo4jRepository<DOTerm> {
                 "return count(root)";
         Long s = queryCount(cypher);
         return s;
+    }
+
+    public DOTerm getDiseaseTerm(String primaryKey) {
+        String cypher = "match (term:DOTerm), " +
+                "(annotation:Association)-[annotationRelation]->(term), " +
+                "(gene:Gene)-[geneAssociation:ASSOCIATION]->(annotation), " +
+                "(publication:Publication)<-[publicationRelation:EVIDENCE]-(annotation), " +
+                "(evidence:EvidenceCode)<-[evidenceRelation:EVIDENCE]-(annotation) " +
+                "where term.primaryKey =  {primaryKey} " +
+                "return term, annotation, annotationRelation, gene, geneAssociation, publication, publicationRelation, " +
+                "evidence, evidenceRelation ";
+        HashMap<String, String> map = new HashMap<>();
+        map.put("primaryKey", primaryKey);
+
+        DOTerm term = query(cypher, map).iterator().next();
+        return term;
     }
 }

src/main/java/org/alliancegenome/indexer/repository/GeneRepository.java
@@ -21,7 +21,7 @@ public class GeneRepository extends Neo4jRepository<Gene> {
 	}
 	
 	public Gene getOneGene(String primaryKey) {		
-		HashMap<String, String> map = new HashMap<String, String>();
+		HashMap<String, String> map = new HashMap<>();
 
 		map.put("primaryKey", primaryKey);
 		String query = "";
@@ -45,7 +45,7 @@ public class GeneRepository extends Neo4jRepository<Gene> {
 	}
 	
 	public Iterable<Gene> getGenes(List<String> ids) {
-		HashMap<String, Object> map = new HashMap<String, Object>();
+		HashMap<String, Object> map = new HashMap<>();
 		map.put("list", ids);
 		String query = "";
 		query += " MATCH p1=(q:Species)-[:FROM_SPECIES]-(g:Gene)--(s) WHERE g.primaryKey IN {list}";
@@ -61,7 +61,7 @@ public class GeneRepository extends Neo4jRepository<Gene> {
 		Result r = queryForResult(query);
 		Iterator<Map<String, Object>> i = r.iterator();
 		
-		ArrayList<String> list = new ArrayList<String>();
+		ArrayList<String> list = new ArrayList<>();
 		
 		while(i.hasNext()) {
 			Map<String, Object> map2 = i.next();

src/main/java/org/alliancegenome/indexer/translators/DiseaseTranslator.java
@@ -12,6 +12,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toMap;
 
 public class DiseaseTranslator extends EntityDocumentTranslator<DOTerm, DiseaseDocument> {
 
@@ -31,34 +32,43 @@ public class DiseaseTranslator extends EntityDocumentTranslator<DOTerm, DiseaseD
         if (entity.getDiseaseGeneJoins() == null)
             return doc;
 
-        // group by gene
-        Map<Gene, List<DiseaseGeneJoin>> geneAssociationMap = entity.getDiseaseGeneJoins().stream()
+        // group by gene then by association type
+        Map<Gene, Map<String, List<DiseaseGeneJoin>>> geneAssociationMap = entity.getDiseaseGeneJoins().stream()
                 .collect(
                         groupingBy(DiseaseGeneJoin::getGene,
-                                Collectors.mapping(association -> association, Collectors.toList())
-                        )
+                                groupingBy(DiseaseGeneJoin::getJoinType))
                 );
 
+        // sort by gene symbol
+        Map<Gene, Map<String, List<DiseaseGeneJoin>>> sortedGeneAssociationMap =
+                geneAssociationMap.entrySet().stream().sorted(Map.Entry.comparingByKey()).collect(toMap(Map.Entry::getKey, Map.Entry::getValue));
+
         // generate AnnotationDocument records
-        List<AnnotationDocument> annotationDocuments = geneAssociationMap.entrySet().stream()
-                // sort by gene symbol
-                .sorted(Map.Entry.comparingByKey())
-                .map(entry -> {
-                    AnnotationDocument document = new AnnotationDocument();
-                    if (translationDepth > 0) {
-                        document.setGeneDocument(geneTranslator.translate(entry.getKey(), translationDepth - 1)); // This needs to not happen if being call from GeneTranslator
-                    }
-                    List<PublicationDoclet> publicationDocuments = entry.getValue().stream()
-                            .map(association -> {
-                                document.setAssoicationType(association.getJoinType());
-                                Publication publication = association.getPublication();
-                                return getPublicationDocument(association, publication);
-                            })
-                            .collect(Collectors.toList());
-                    document.setPublications(publicationDocuments);
-                    return document;
-                })
-                .collect(Collectors.toList());
+        List<AnnotationDocument> annotationDocuments = new ArrayList<>();
+        sortedGeneAssociationMap.forEach( (gene, associationMap) ->
+                {
+                    List<AnnotationDocument> annotationDocs = associationMap.entrySet().stream()
+                            .map(associationEntry -> {
+                                AnnotationDocument document = new AnnotationDocument();
+                                if (translationDepth > 0) {
+                                    document.setGeneDocument(geneTranslator.translate(gene, translationDepth - 1)); // This needs to not happen if being call from GeneTranslator
+                                }
+                                document.setAssociationType(associationEntry.getKey());
+                                List<PublicationDoclet> publicationDocuments = associationEntry.getValue().stream()
+                                        .filter(diseaseGeneJoin ->
+                                                getPublicationDoclet(diseaseGeneJoin, diseaseGeneJoin.getPublication()) != null
+                                        )
+                                        .map(diseaseGeneJoin -> {
+                                            Publication publication = diseaseGeneJoin.getPublication();
+                                            return getPublicationDoclet(diseaseGeneJoin, publication);
+                                        })
+                                        .collect(Collectors.toList());
+                                document.setPublications(publicationDocuments);
+
+                                return document;
+                            }).collect(Collectors.toList());
+                    annotationDocuments.addAll(annotationDocs);
+                });
 
         doc.setAnnotations(annotationDocuments);
 
@@ -81,7 +91,7 @@ public class DiseaseTranslator extends EntityDocumentTranslator<DOTerm, DiseaseD
                 .collect((Collectors.toMap(DOTerm::getPrimaryKey, id -> id)));
     }
 
-    private PublicationDoclet getPublicationDocument(DiseaseGeneJoin association, Publication publication) {
+    private PublicationDoclet getPublicationDoclet(DiseaseGeneJoin association, Publication publication) {
         PublicationDoclet pubDoc = new PublicationDoclet();
         pubDoc.setPrimaryKey(publication.getPrimaryKey());
 
@@ -90,8 +100,10 @@ public class DiseaseTranslator extends EntityDocumentTranslator<DOTerm, DiseaseD
         pubDoc.setPubModId(publication.getPubModId());
         pubDoc.setPubModUrl(publication.getPubModUrl());
 
-        if (association.getEvidenceCodes() == null)
-            throw new RuntimeException("Could not find any evidence codes for " + association.getGene().getPrimaryKey() + " and publication " + publication.getPubModUrl());
+        if (association.getEvidenceCodes() == null) {
+            log.error("Could not find any evidence codes for " + association.getGene().getPrimaryKey() + " and publication " + publication.getPrimaryKey());
+            return null;
+        }
 
         List<String> evidencesDocument = association.getEvidenceCodes().stream()
                 .map(EvidenceCode::getPrimaryKey)
@@ -218,7 +230,7 @@ public class DiseaseTranslator extends EntityDocumentTranslator<DOTerm, DiseaseD
                             doc.setSpecies(getSpeciesDoclet(diseaseGeneJoin));
                             doc.setGeneDocument(geneTranslator.entityToDocument(diseaseGeneJoin.getGene(), translationDepth - 1));
                             List<PublicationDoclet> pubDocs = new ArrayList<>();
-                            pubDocs.add(getPublicationDocument(diseaseGeneJoin, diseaseGeneJoin.getPublication()));
+                            pubDocs.add(getPublicationDoclet(diseaseGeneJoin, diseaseGeneJoin.getPublication()));
                             doc.setPublications(pubDocs);
                             return doc;
                         })

src/test/java/org/alliancegenome/indexer/DiseaseTest.java
@@ -4,6 +4,7 @@ import org.alliancegenome.indexer.config.ConfigHelper;
 import org.alliancegenome.indexer.entity.node.DOTerm;
 import org.alliancegenome.indexer.repository.DiseaseRepository;
 import org.alliancegenome.indexer.repository.Neo4jRepository;
+import org.alliancegenome.indexer.translators.DiseaseTranslator;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -35,6 +36,11 @@ public class DiseaseTest {
 */
 
         Neo4jRepository<DOTerm> neo4jService = new Neo4jRepository<>(DOTerm.class);
+
+        DiseaseTranslator translator = new DiseaseTranslator();
+
+        DOTerm diseaseTerm = diseaseRepository.getDiseaseTerm("DOID:9452");
+        translator.translate(diseaseTerm, 1);
 
         //List<DOTerm> geneDiseaseList = diseaseRepository.getAllDiseaseTerms(0, 10);
 /*

commit 48aa2f248c35825c0ca5f02eb5104e0e1669434e

Fixes bug introduced in 105e4433eadf9af7d4f68e05e36d882f88295a11

src/main/java/org/alliancegenome/indexer/entity/node/Gene.java
@@ -56,7 +56,7 @@ public class Gene extends Neo4jEntity implements Comparable<Gene> {
     @Relationship(type = "IS_IMPLICATED_IN")
     private Set<DOTerm> dOTerms = new HashSet<>();
 
-    @Relationship(type = "ORTHOLOGOUS", direction = Relationship.UNDIRECTED)
+    @Relationship(type = "ORTHOLOGOUS", direction = Relationship.OUTGOING)
     private List<Orthologous> orthoGenes;
 
     @Relationship(type = "LOCATED_ON")