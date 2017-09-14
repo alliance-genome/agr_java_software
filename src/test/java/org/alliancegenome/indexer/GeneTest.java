50df4f1b13afdfd9f5a887d1780e14e989a4e7be
Olin Blodgett <nilo@nilosplace.net>
2017-09-14 10:39:33 -0400

    Added Testing

---
+++ src/test/java/org/alliancegenome/indexer/GeneTest.java
@@ -0,0 +1,25 @@
+package org.alliancegenome.indexer;
+
+import org.alliancegenome.indexer.config.ConfigHelper;
+import org.alliancegenome.indexer.entity.node.Gene;
+import org.alliancegenome.indexer.repository.GeneRepository;
+import org.alliancegenome.indexer.translators.GeneTranslator;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class GeneTest {
+
+	public static void main(String[] args) throws Exception {
+		ConfigHelper.init();
+		
+		GeneRepository repo = new GeneRepository();
+		GeneTranslator trans = new GeneTranslator();
+		
+		Gene gene = repo.getOneGene("ZFIN:ZDB-GENE-000210-7");
+
+		ObjectMapper mapper = new ObjectMapper();
+		String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(trans.translate(gene));
+		System.out.println(json);
+	}
+
+}
