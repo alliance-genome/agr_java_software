b49f868990804abc62ef2f8a8b38b59d7b919ff6
Olin Blodgett <nilo@nilosplace.net>
2018-01-17 16:37:58 -0500

    Removed

--- src/main/java/org/alliancegenome/api/service/helper/git/S3Helper.java
+++ src/main/java/org/alliancegenome/api/service/helper/git/S3Helper.java
@@ -35,54 +35,37 @@ public class S3Helper {
     public int listFiles(String prefix) {
         int count = 0;
         try {
+            log.info("Getting S3 file listing");
             AmazonS3 s3 = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(config.getAWSAccessKey(), config.getAWSSecretKey()))).withRegion(Regions.US_EAST_1).build();
-
             ObjectListing ol = s3.listObjects(bucketName, prefix);
-            log.info(ol.getObjectSummaries().size());
+            log.debug(ol.getObjectSummaries().size());
             count = ol.getObjectSummaries().size();
             for (S3ObjectSummary summary : ol.getObjectSummaries()) {
-
-                //if(!summary.getKey().contains("/")) {
-                log.info(" - " + summary.getKey() + "\t(size = " + summary.getSize() + ")\t(lastModified = " + summary.getLastModified() + ")");
-                //}
+                log.debug(" - " + summary.getKey() + "\t(size = " + summary.getSize() + ")\t(lastModified = " + summary.getLastModified() + ")");
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
         return count;
     }
 
-    public void saveFile(String path, String data) throws GenericException {
+    public void saveFile(String path, String fileData) throws GenericException {
         try {
             Date d = new Date();
             String outFileName = "tmp.data_" + d.getTime();
             File outfile = new File(outFileName);
-            FileUtils.writeStringToFile(outfile, data);
+            log.info("Saving file to local filesystem: " + outfile.getAbsolutePath());
+            FileUtils.writeStringToFile(outfile, fileData);
+            log.info("Save file to local filesystem complete");
 
-            log.info("Saving Path: " + path);
-            saveFile(path, outfile);
-
-            outfile.delete();
-            log.info("Upload Complete: " + path);
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new FileSavingException(e.getMessage());
-        }
-    }
-
-    public void saveFile(String path, File data) throws GenericException {
-        try {
-            log.info("Uploading: "  + path + " -> s3://" + bucketName + "/" + path);
+            log.info("Uploading file to S3: " + path + " -> s3://" + bucketName + "/" + path);
             AmazonS3 s3 = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(config.getAWSAccessKey(), config.getAWSSecretKey()))).withRegion(Regions.US_EAST_1).build();
-            //AmazonS3 s3 = AmazonS3ClientBuilder.standard().withCredentials(new BasicAWSCredentials("AKIAJYVKAGPPSZ47VJVQ", "1PK/iA0Lk4apNL2Cdov1mY7stgACCFklGEgLk8PL")).withRegion(Regions.US_EAST_1).build();
             TransferManager tm = TransferManagerBuilder.standard().withS3Client(s3).build();
-            //TransferManager tm = new TransferManager(new AWSStaticCredentialsProvider(new BasicAWSCredentials("AKIAJYVKAGPPSZ47VJVQ", "1PK/iA0Lk4apNL2Cdov1mY7stgACCFklGEgLk8PL")));
-
-            final Upload uploadFile = tm.upload(bucketName, path, data);
+            final Upload uploadFile = tm.upload(bucketName, path, outfile);
             uploadFile.waitForCompletion();
             tm.shutdownNow();
-            log.info("Upload complete.");
+            outfile.delete();
+            log.info("S3 Upload complete");
         } catch (Exception e) {
             e.printStackTrace();
             throw new FileSavingException(e.getMessage());
