f88fbffd15cc2056dc4025e13199d3c6b8925a15
Olin Blodgett <nilo@nilosplace.net>
2018-03-15 09:26:18 -0400

    Refactored to new system

--- src/main/java/org/alliancegenome/api/service/AlleleService.java
+++ src/main/java/org/alliancegenome/api/service/AlleleService.java
@@ -1,20 +1,18 @@
 package org.alliancegenome.api.service;
 
-import org.alliancegenome.api.dao.AlleleDAO;
+import java.util.Map;
 
 import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
-import java.util.Map;
+
+import org.alliancegenome.shared.es.dao.site_index.AlleleDAO;
 
 @RequestScoped
 public class AlleleService {
     
-    @Inject
-    private AlleleDAO alleleDAO;
+    private static AlleleDAO alleleDAO = new AlleleDAO();
 
     public Map<String, Object> getById(String id) {
         return alleleDAO.getById(id);
     }
-
     
 }
