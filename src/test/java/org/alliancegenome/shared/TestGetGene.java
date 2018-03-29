b6edfd05c7c0ccc9354ae4120c4cc2e59a1cd629
Christian Pich <cmpich@zfin.org>
2018-03-29 14:38:50 -0700

    AGR-733 handle secondary gene IDs

--- src/test/java/org/alliancegenome/shared/TestGetGene.java
+++ src/test/java/org/alliancegenome/shared/TestGetGene.java
@@ -1,21 +1,47 @@
 package org.alliancegenome.shared;
 
-import java.util.HashMap;
-
+import jdk.nashorn.internal.ir.annotations.Ignore;
+import org.alliancegenome.core.config.ConfigHelper;
+import org.alliancegenome.es.index.site.dao.DiseaseDAO;
+import org.alliancegenome.es.index.site.dao.GeneDAO;
 import org.alliancegenome.neo4j.entity.node.Gene;
 import org.alliancegenome.neo4j.repository.GeneRepository;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertNotNull;
 
 public class TestGetGene {
 
-	public static void main(String[] args) {
-		GeneRepository repo = new GeneRepository();
-		
-		//"MGI:97490" OR g.primaryKey = "RGD:3258"
-		
-		System.out.println("MGI:97490");
-		HashMap<String, Gene> geneMap = repo.getGene("RGD:3258");
-		System.out.println(geneMap);
+    private GeneDAO geneService = new GeneDAO();
 
-	}
+    @Before
+    public void before() {
+        Configurator.setRootLevel(Level.WARN);
+        ConfigHelper.init();
+        geneService = new GeneDAO();
+    }
 
+    public static void main(String[] args) {
+        GeneRepository repo = new GeneRepository();
+
+        //"MGI:97490" OR g.primaryKey = "RGD:3258"
+
+        System.out.println("MGI:97490");
+        HashMap<String, Gene> geneMap = repo.getGene("RGD:3258");
+        System.out.println(geneMap);
+
+    }
+
+    @Test
+    @Ignore
+    public void checkSecondaryId() {
+        Map<String, Object> result = geneService.getGeneBySecondary("ZFIN:ZDB-GENE-030131-3355");
+        assertNotNull(result);
+    }
 }
