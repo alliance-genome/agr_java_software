8c78ea390cef085d70b24c285197831a615f3edd
Olin Blodgett <nilo@nilosplace.net>
2018-04-09 12:42:13 -0400

    Update GenericException.java

--- src/main/java/org/alliancegenome/core/exceptions/GenericException.java
+++ src/main/java/org/alliancegenome/core/exceptions/GenericException.java
@@ -1,6 +1,5 @@
 package org.alliancegenome.core.exceptions;
 
-@SuppressWarnings("serial")
 public class GenericException extends Exception {
 	public GenericException(String message) {
 		super(message);
