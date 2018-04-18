313d81f2e0e5d6964e425988e44ce5dac4bc6459
Olin Blodgett <nilo@nilosplace.net>
2018-04-18 14:17:07 -0400

    Spaces for tabs

--- src/main/java/org/alliancegenome/agr_elasticsearch_util/commands/CommandType.java
+++ src/main/java/org/alliancegenome/agr_elasticsearch_util/commands/CommandType.java
@@ -1,24 +1,24 @@
 package org.alliancegenome.agr_elasticsearch_util.commands;
 
 public enum CommandType {
-	
-	repo(RepoCommand.class),
-	quit(QuitCommand.class),
-	snapshot(SnapShotCommand.class),
-	alias(AliasCommand.class),
-	index(IndexCommand.class),
-	config(ConfigCommand.class),
-	dataindex(DataIndexCommand.class),
-	;
+    
+    repo(RepoCommand.class),
+    quit(QuitCommand.class),
+    snapshot(SnapShotCommand.class),
+    alias(AliasCommand.class),
+    index(IndexCommand.class),
+    config(ConfigCommand.class),
+    dataindex(DataIndexCommand.class),
+    ;
 
-	private Class<?> implClass;
+    private Class<?> implClass;
 
-	public Class<?> getImplClass() {
-		return implClass;
-	}
-	
-	private CommandType(Class<?> implClass) {
-		this.implClass = implClass;
-	}
+    public Class<?> getImplClass() {
+        return implClass;
+    }
+    
+    private CommandType(Class<?> implClass) {
+        this.implClass = implClass;
+    }
 
 }
