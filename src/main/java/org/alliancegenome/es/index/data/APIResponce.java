ecb243516c3279064a93ec6df92ea0f549b7fc18
Olin Blodgett <nilo@nilosplace.net>
2018-05-07 16:07:48 -0400

    Added code for snapshots

---
+++ src/main/java/org/alliancegenome/es/index/data/APIResponce.java
@@ -0,0 +1,11 @@
+package org.alliancegenome.es.index.data;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class APIResponce {
+    private String status;
+    private String message;
+}
--- src/main/java/org/alliancegenome/es/index/data/dao/DataFileDAO.java
+++ src/main/java/org/alliancegenome/es/index/data/dao/DataFileDAO.java
@@ -8,13 +8,20 @@ import java.util.List;
 import org.alliancegenome.es.index.ESDocumentDAO;
 import org.alliancegenome.es.index.data.document.DataFileDocument;
 import org.apache.commons.collections4.keyvalue.MultiKey;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.elasticsearch.index.query.MatchQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 
 public class DataFileDAO extends ESDocumentDAO<DataFileDocument> {
 
+    private Log log = LogFactory.getLog(getClass());
+    
     public List<DataFileDocument> search(String schemaVersion, Date snapShotDate) {
+        log.debug("Schema: " + schemaVersion);
+        log.debug("SnapShotDate: " + snapShotDate);
         MatchQueryBuilder mqb = QueryBuilders.matchQuery("schemaVersion", schemaVersion);
+        log.debug("Query: " + mqb);
         List<DataFileDocument> docs = search(mqb);
         HashMap<MultiKey<String>, DataFileDocument> map = new HashMap<MultiKey<String>, DataFileDocument>();
         
