9c304d4ca4d06366f5e25f78552d4fc1a0611823
Olin Blodgett <Olin.Blodgett@jax.org>
2018-10-17 13:15:40 -0400

    Added fix for interactions

--- agr_indexer/src/test/java/org/alliancegenome/indexer/InteractionsTest.java
+++ agr_indexer/src/test/java/org/alliancegenome/indexer/InteractionsTest.java
@@ -4,17 +4,21 @@ import java.util.List;
 
 import org.alliancegenome.neo4j.entity.node.InteractionGeneJoin;
 import org.alliancegenome.neo4j.repository.InteractionRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class InteractionsTest {
 
     public static InteractionRepository repo = new InteractionRepository();
     
     public static void main(String[] args) {
-        List<InteractionGeneJoin> list = repo.getInteractions("MGI:99604");
+        Logger log = LogManager.getLogger(InteractionsTest.class);
         
+        //List<InteractionGeneJoin> list = repo.getInteractions("MGI:99604");
+        List<InteractionGeneJoin> list = repo.getInteractions("MGI:97490");
+        log.info("Count: " + list.size());
         for(InteractionGeneJoin join: list) {
-            System.out.println(join);
+            log.info(join);
         }
     }
-
 }
--- agr_java_core/src/main/java/org/alliancegenome/neo4j/repository/InteractionRepository.java
+++ agr_java_core/src/main/java/org/alliancegenome/neo4j/repository/InteractionRepository.java
@@ -11,19 +11,26 @@ public class InteractionRepository extends Neo4jRepository<InteractionGeneJoin> 
     public InteractionRepository() {
         super(InteractionGeneJoin.class);
     }
-
+    
     public List<InteractionGeneJoin> getInteractions(String primaryKey) {
         HashMap<String, String> map = new HashMap<>();
-
+        List<InteractionGeneJoin> ret = new ArrayList<>();
+        
         map.put("primaryKey", primaryKey);
-        String query = "MATCH p1=(sp1:Species)-[:FROM_SPECIES]-(g1:Gene)-[iw:INTERACTS_WITH]-(g2:Gene)-[:FROM_SPECIES]-(sp2:Species), p2=(g1)-->(igj:InteractionGeneJoin)-->(g2) where (g1.primaryKey = {primaryKey} or g2.primaryKey = {primaryKey}) and iw.uuid = igj.primaryKey RETURN p1, p2";
+        String query = "MATCH p1=(sp1:Species)-[:FROM_SPECIES]-(g1:Gene)-[iw:INTERACTS_WITH]->(g2:Gene)-[:FROM_SPECIES]-(sp2:Species), p2=(g1:Gene)-->(igj:InteractionGeneJoin)-->(g2:Gene) where (g1.primaryKey = {primaryKey}) and iw.uuid = igj.primaryKey RETURN p1, p2";
         //String query = "MATCH p1=(g:Gene)-[iw:INTERACTS_WITH]->(g2:Gene), p2=(g:Gene)-->(igj:InteractionGeneJoin)--(s) where g.primaryKey = {primaryKey} and iw.uuid = igj.primaryKey RETURN p1, p2";
 
         Iterable<InteractionGeneJoin> joins = query(query, map);
-        List<InteractionGeneJoin> ret = new ArrayList<>();
         for (InteractionGeneJoin join: joins) {
             ret.add(join);
         }
+        
+        String query2 = "MATCH p1=(sp1:Species)-[:FROM_SPECIES]-(g1:Gene)-[iw:INTERACTS_WITH]->(g2:Gene)-[:FROM_SPECIES]-(sp2:Species), p2=(g1:Gene)-->(igj:InteractionGeneJoin)-->(g2:Gene) where (g2.primaryKey = {primaryKey}) and iw.uuid = igj.primaryKey RETURN p1, p2";
+        Iterable<InteractionGeneJoin> joins2 = query(query2, map);
+        for (InteractionGeneJoin join: joins2) {
+            ret.add(join);
+        }
+
         return ret;
     }
 }
