721b319d465b83bb360215799743b034bd6fbb5c
Christian Pich <cmpich@zfin.org>
2018-11-02 20:24:06 +0100

    AGR-1271 add crossReference info for features from new node.

--- agr_java_core/src/main/java/org/alliancegenome/neo4j/repository/GeneRepository.java
+++ agr_java_core/src/main/java/org/alliancegenome/neo4j/repository/GeneRepository.java
@@ -58,15 +58,17 @@ public class GeneRepository extends Neo4jRepository<Gene> {
 
         query += " MATCH p1=(q:Species)-[:FROM_SPECIES]-(g:Gene)--(s) WHERE g.primaryKey = {primaryKey}";
         query += " OPTIONAL MATCH p5=(g:Gene)--(s:DiseaseEntityJoin)--(feature:Feature)";
+        query += " OPTIONAL MATCH featureCrossRef=(feature:Feature)--(crossRef:CrossReference)";
         query += " OPTIONAL MATCH p12=(g:Gene)--(s:DiseaseEntityJoin)--(orthoGene:Gene)";
         query += " OPTIONAL MATCH p2=(do:DOTerm)--(s:DiseaseEntityJoin)-[:EVIDENCE]-(ea)";
         query += " OPTIONAL MATCH p4=(g:Gene)--(s:OrthologyGeneJoin)--(a:OrthoAlgorithm), p3=(g)-[o:ORTHOLOGOUS]-(g2:Gene)-[:FROM_SPECIES]-(q2:Species), (s)--(g2)";
         query += " OPTIONAL MATCH p6=(g:Gene)--(s:PhenotypeEntityJoin)--(tt) ";
-        query += " OPTIONAL MATCH p8=(g:Gene)--(s:PhenotypeEntityJoin)--(ff:Feature)";
+        query += " OPTIONAL MATCH p8=(g:Gene)--(s:PhenotypeEntityJoin)--(ff:Feature)--(crossRef1:CrossReference)";
+        query += " OPTIONAL MATCH featureCrossRef1=(ff:Feature)--(crossRef1:CrossReference)";
         query += " OPTIONAL MATCH p9=(g:Gene)--(s:GOTerm)-[:IS_A|PART_OF*]->(:GOTerm)";
         query += " OPTIONAL MATCH p10=(g:Gene)--(s:BioEntityGeneExpressionJoin)--(t) ";
         query += " OPTIONAL MATCH pTerm=(g:Gene)--(s:ExpressionBioEntity)--(:Ontology) ";
-        query += " RETURN p1, p2, p3, p4, p5, p6, p8, p9, p10, p12, pTerm";
+        query += " RETURN p1, p2, p3, p4, p5, p6, p8, p9, p10, p12, pTerm, featureCrossRef, featureCrossRef1";
 
         Iterable<Gene> genes = query(query, map);
         for (Gene g : genes) {
