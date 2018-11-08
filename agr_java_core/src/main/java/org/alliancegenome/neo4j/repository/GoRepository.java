e12e512f09fac241bfc30f180a9b6a34162f56c3
Kevin Schaper <kevinschaper@gmail.com>
2018-11-08 14:31:49 -0800

    Commented out genes from GoRepository.getAllTerms()

--- agr_java_core/src/main/java/org/alliancegenome/neo4j/repository/GoRepository.java
+++ agr_java_core/src/main/java/org/alliancegenome/neo4j/repository/GoRepository.java
@@ -29,9 +29,9 @@ public class GoRepository extends Neo4jRepository<GOTerm> {
     
     public Iterable<GOTerm> getAllTerms() {
         String query = "MATCH p0=(go:GOTerm) " + 
-                " OPTIONAL MATCH p1=(go)-[:ANNOTATED_TO]-(:Gene)-[:FROM_SPECIES]-(:Species)" +
+//                " OPTIONAL MATCH p1=(go)-[:ANNOTATED_TO]-(:Gene)-[:FROM_SPECIES]-(:Species)" +
                 " OPTIONAL MATCH p2=(go)-[:ALSO_KNOWN_AS]-(:Synonym)";
-        query += " RETURN p0, p1, p2";
+        query += " RETURN p0, p2";
         
         return query(query);
     }
