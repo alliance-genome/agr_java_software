7b59c2dde7cf0f917b1ed1e395790afa6b7b375f
Christian Pich <cmpich@zfin.org>
2019-01-15 19:28:55 +0100

    make id and url attributes for Publication JSON attributes for phenotype

--- agr_java_core/src/main/java/org/alliancegenome/neo4j/entity/node/Publication.java
+++ agr_java_core/src/main/java/org/alliancegenome/neo4j/entity/node/Publication.java
@@ -1,5 +1,6 @@
 package org.alliancegenome.neo4j.entity.node;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Getter;
 import lombok.Setter;
@@ -18,30 +19,51 @@ public class Publication extends Neo4jEntity implements Comparable<Publication> 
 
     @JsonView({View.Interaction.class, View.Expression.class})
     private String primaryKey;
-    @JsonView({View.Phenotype.class,View.Interaction.class, View.Expression.class})
+    @JsonView({View.Interaction.class, View.Expression.class})
     private String pubMedId;
-    @JsonView({View.Phenotype.class,View.Interaction.class, View.Expression.class})
+    @JsonView({View.Interaction.class, View.Expression.class})
     private String pubMedUrl;
-    @JsonView({View.Phenotype.class,View.Interaction.class, View.Expression.class})
+    @JsonView({View.Interaction.class, View.Expression.class})
     private String pubModId;
-    @JsonView({View.Phenotype.class,View.Interaction.class, View.Expression.class})
+    @JsonView({View.Interaction.class, View.Expression.class})
     private String pubModUrl;
-    @JsonView({View.Phenotype.class, View.Expression.class})
+    @JsonView({View.Expression.class})
     private String pubId;
 
     @Relationship(type = "ANNOTATED_TO")
     private List<EvidenceCode> evidence;
 
     public void setPubIdFromId() {
-        if (StringUtils.isNotEmpty(pubMedId))
+        if (StringUtils.isNotEmpty(pubMedId)) {
             pubId = pubMedId;
-        else
+        } else {
             pubId = pubModId;
+        }
+    }
+
+    @JsonView({View.Phenotype.class, View.Expression.class})
+    @JsonProperty("id")
+    private String getPublicationId() {
+        if (StringUtils.isNotEmpty(pubMedId)) {
+            return pubMedId;
+        } else {
+            return pubModId;
+        }
+    }
+
+    @JsonView({View.Phenotype.class, View.Expression.class})
+    @JsonProperty("url")
+    private String getPublicationUrl() {
+        if (StringUtils.isNotEmpty(pubMedId)) {
+            return pubMedUrl;
+        } else {
+            return pubModUrl;
+        }
     }
 
     @Override
     public String toString() {
-        return primaryKey;
+        return getPublicationId() + " : " + getPublicationUrl();
     }
 
     @Override
