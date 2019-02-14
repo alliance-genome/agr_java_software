49375affcc164ae06d614e3fa0621df8799777ff
Olin Blodgett <Olin.Blodgett@jax.org>
2019-02-14 17:34:27 -0500

    Initial Code for Submission System before Testing

--- agr_submission/src/main/java/org/alliancegenome/agr_submission/entities/DataType.java
+++ agr_submission/src/main/java/org/alliancegenome/agr_submission/entities/DataType.java
@@ -7,9 +7,6 @@ import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
