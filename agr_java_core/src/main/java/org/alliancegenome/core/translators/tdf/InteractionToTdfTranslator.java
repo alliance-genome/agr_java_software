55feecfb36d9660fb66561ad24d4365a28093599
Olin Blodgett <Olin.Blodgett@jax.org>
2019-06-06 14:54:54 -0400

    Added fix for download file

--- agr_java_core/src/main/java/org/alliancegenome/core/translators/tdf/InteractionToTdfTranslator.java
+++ agr_java_core/src/main/java/org/alliancegenome/core/translators/tdf/InteractionToTdfTranslator.java
@@ -11,25 +11,69 @@ public class InteractionToTdfTranslator {
     public String getAllRows(List<InteractionGeneJoin> annotations) {
         StringBuilder builder = new StringBuilder();
         StringJoiner headerJoiner = new StringJoiner("\t");
-        headerJoiner.add("Molecule Type");
-        headerJoiner.add("Interactor Gene Symbol");
-        headerJoiner.add("Interactor Gene ID");
-        headerJoiner.add("Species");
-        headerJoiner.add("Interactor Molecule Type");
-        headerJoiner.add("Detection Methods");
-        headerJoiner.add("Source");
+
+        headerJoiner.add("Focus gene molecule type ID");
+        headerJoiner.add("Focus gene molecule type");
+        headerJoiner.add("Focus gene experimental role ID");
+        headerJoiner.add("Focus gene experimental role");
+        
+        headerJoiner.add("Interactor gene ID");
+        headerJoiner.add("Interactor gene");
+        headerJoiner.add("Interactor species ID");
+        headerJoiner.add("Interactor species");
+        
+        headerJoiner.add("Interactor molecule type ID");
+        headerJoiner.add("Interactor molecule type");
+        headerJoiner.add("Interactor experimental role ID");
+        headerJoiner.add("Interactor experimental role");
+        
+        headerJoiner.add("Interaction type ID");
+        headerJoiner.add("Interaction type");
+        
+        headerJoiner.add("Detection method IDs");
+        headerJoiner.add("Detection methods");
+        
+        headerJoiner.add("Source ID");
+        headerJoiner.add("Source DB ID");
+        headerJoiner.add("Source DB");
+        headerJoiner.add("Aggregation DB ID");
+        headerJoiner.add("Aggregation DB");
         headerJoiner.add("Reference");
+
         builder.append(headerJoiner.toString());
         builder.append(ConfigHelper.getJavaLineSeparator());
 
         annotations.forEach(interactionGeneJoin -> {
             StringJoiner joiner = new StringJoiner("\t");
+
+            joiner.add(interactionGeneJoin.getInteractorAType().getPrimaryKey());
             joiner.add(interactionGeneJoin.getInteractorAType().getLabel());
-            joiner.add(interactionGeneJoin.getGeneB().getSymbol());
+            joiner.add(interactionGeneJoin.getInteractorARole().getPrimaryKey());
+            joiner.add(interactionGeneJoin.getInteractorARole().getLabel());
+            
             joiner.add(interactionGeneJoin.getGeneB().getPrimaryKey());
+            joiner.add(interactionGeneJoin.getGeneB().getName());
+            joiner.add(interactionGeneJoin.getGeneB().getSpecies().getPrimaryKey());
             joiner.add(interactionGeneJoin.getGeneB().getSpecies().getName());
+            
+            joiner.add(interactionGeneJoin.getInteractorBType().getPrimaryKey());
             joiner.add(interactionGeneJoin.getInteractorBType().getLabel());
-            // add list of detectionMethods
+            joiner.add(interactionGeneJoin.getInteractorBRole().getPrimaryKey());
+            joiner.add(interactionGeneJoin.getInteractorBRole().getLabel());
+            
+            joiner.add(interactionGeneJoin.getInteractionType().getPrimaryKey());
+            joiner.add(interactionGeneJoin.getInteractionType().getLabel());
+            
+            // add list of detectionMethods Ids
+            String detectionMethodIds = "";
+            if (interactionGeneJoin.getDetectionsMethods() != null) {
+                StringJoiner methodJoiner = new StringJoiner(",");
+                interactionGeneJoin.getDetectionsMethods().forEach(method -> methodJoiner.add(method.getPrimaryKey()));
+                detectionMethodIds = methodJoiner.toString();
+            }
+            joiner.add(detectionMethodIds);
+            
+            // add list of detectionMethods Labels
             String detectionMethods = "";
             if (interactionGeneJoin.getDetectionsMethods() != null) {
                 StringJoiner methodJoiner = new StringJoiner(",");
@@ -37,8 +81,24 @@ public class InteractionToTdfTranslator {
                 detectionMethods = methodJoiner.toString();
             }
             joiner.add(detectionMethods);
+
+            // add list of detectionMethods Labels
+            String sourceIds = "";
+            if (interactionGeneJoin.getCrossReferences() != null) {
+                StringJoiner methodJoiner = new StringJoiner(",");
+                interactionGeneJoin.getCrossReferences().forEach(method -> methodJoiner.add(method.getPrimaryKey()));
+                sourceIds = methodJoiner.toString();
+            }
+            joiner.add(sourceIds);
+            
+            joiner.add(interactionGeneJoin.getSourceDatabase().getPrimaryKey());
             joiner.add(interactionGeneJoin.getSourceDatabase().getLabel());
+            
+            joiner.add(interactionGeneJoin.getAggregationDatabase().getPrimaryKey());
+            joiner.add(interactionGeneJoin.getAggregationDatabase().getLabel());
+            
             joiner.add(interactionGeneJoin.getPublication().getPubId());
+
             builder.append(joiner.toString());
             builder.append(ConfigHelper.getJavaLineSeparator());
         });
