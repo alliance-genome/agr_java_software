12d42b5b6e27154396c3ff72c3b4404170000d67
Christian Pich <cmpich@zfin.org>
2019-09-17 17:03:19 +0200

    fix unit tests

--- agr_api/src/test/java/org/alliancegenome/api/tests/integration/DiseaseIT.java
+++ agr_api/src/test/java/org/alliancegenome/api/tests/integration/DiseaseIT.java
@@ -77,7 +77,7 @@ public class DiseaseIT {
         // Menkes
         String diseaseID = "DOID:1838";
         JsonResultResponse<DiseaseAnnotation> response = diseaseService.getDiseaseAnnotationsWithGenes(diseaseID, pagination);
-        assertResponse(response, 10, 10);
+        assertLimitResponse(response, 20, 20);
     }
 
     @Test
@@ -394,11 +394,11 @@ public class DiseaseIT {
                 "Cowden syndrome\tMGI:2156086\tPten<sup>tm1Hwu</sup>\tallele\tis_implicated_in\tECO:0000033\tPMID:12163417,PMID:17237784,PMID:18757421,PMID:23873941,PMID:27889578\n" +
                 "endometrial cancer\tMGI:2156086\tPten<sup>tm1Hwu</sup>\tallele\tis_implicated_in\tECO:0000033\tPMID:18632614,PMID:20418913\n" +
                 "fatty liver disease\tMGI:2156086\tPten<sup>tm1Hwu</sup>\tallele\tis_implicated_in\tECO:0000033\tPMID:24802098\n" +
-                "follicular thyroid carcinoma\tMGI:2156086\tPten<sup>tm1Hwu</sup>\tallele\tis_implicated_in\tECO:0000033\tPMID:22167068\n" +
                 "hepatocellular carcinoma\tMGI:2156086\tPten<sup>tm1Hwu</sup>\tallele\tis_implicated_in\tECO:0000033\tPMID:20837017,PMID:24027047,PMID:25132272\n" +
                 "intestinal pseudo-obstruction\tMGI:2156086\tPten<sup>tm1Hwu</sup>\tallele\tis_implicated_in\tECO:0000033\tPMID:19884655\n" +
                 "persistent fetal circulation syndrome\tMGI:2156086\tPten<sup>tm1Hwu</sup>\tallele\tis_implicated_in\tECO:0000033\tPMID:23023706\n" +
                 "prostate cancer\tMGI:2156086\tPten<sup>tm1Hwu</sup>\tallele\tis_implicated_in\tECO:0000033\tPMID:14522255,PMID:16489020,PMID:21620777,PMID:22350410,PMID:22836754,PMID:23300485,PMID:23348745,PMID:23434594,PMID:23610450,PMID:25455686,PMID:25526087,PMID:25693195,PMID:25948589,PMID:26640144,PMID:27345403,PMID:27357679,PMID:28059767,PMID:28515147,PMID:29720449\n" +
+                "thyroid gland follicular carcinoma	MGI:2156086	Pten<sup>tm1Hwu</sup>	allele	is_implicated_in	ECO:0000033	PMID:22167068\n" +
                 "urinary bladder cancer\tMGI:2156086\tPten<sup>tm1Hwu</sup>\tallele\tis_implicated_in\tECO:0000033\tPMID:19261747,PMID:25533675\n";
         assertEquals(result, output);
     }
