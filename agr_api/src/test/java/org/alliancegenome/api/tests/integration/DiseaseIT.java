ab992ff78fb2af0f7003b879ac2894fa6735fa39
Christian Pich <cmpich@zfin.org>
2019-10-04 21:37:24 +0200

    undo unit test change

--- agr_api/src/test/java/org/alliancegenome/api/tests/integration/DiseaseIT.java
+++ agr_api/src/test/java/org/alliancegenome/api/tests/integration/DiseaseIT.java
@@ -313,7 +313,7 @@ public class DiseaseIT {
     public void checkEmpiricalDiseaseByGene() {
         Pagination pagination = new Pagination(1, 10, null, null);
         // Pten
-        String geneID = "ZFIN:ZDB-GENE-040625-147";
+        String geneID = "MGI:109583";
         JsonResultResponse<DiseaseAnnotation> response = diseaseService.getDiseaseAnnotations(geneID, pagination);
         assertResponse(response, 10, 50);
 
