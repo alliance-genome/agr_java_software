81cf3f4e85b3af0b6908ad31b632b77b538c024e
Christian Pich <cmpich@zfin.org>
2019-10-08 20:10:57 +0200

    refactor Consequence for variants as the db schema has changed

--- agr_cacher/src/main/java/org/alliancegenome/cacher/cachers/AlleleCacher.java
+++ agr_cacher/src/main/java/org/alliancegenome/cacher/cachers/AlleleCacher.java
@@ -1,19 +1,19 @@
 package org.alliancegenome.cacher.cachers;
 
-import static java.util.stream.Collectors.groupingBy;
-
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.alliancegenome.cache.CacheAlliance;
 import org.alliancegenome.cache.manager.AlleleAllianceCacheManager;
 import org.alliancegenome.core.service.JsonResultResponse;
 import org.alliancegenome.neo4j.entity.node.Allele;
 import org.alliancegenome.neo4j.repository.AlleleRepository;
 import org.alliancegenome.neo4j.view.View;
+import org.apache.commons.collections.CollectionUtils;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static java.util.stream.Collectors.groupingBy;
 
 public class AlleleCacher extends Cacher {
 
@@ -29,6 +29,12 @@ public class AlleleCacher extends Cacher {
 
         Map<String, List<Allele>> map = allAlleles.stream().collect(groupingBy(allele -> allele.getGene().getPrimaryKey()));
 
+        allAlleles.forEach(allele -> {
+            if (CollectionUtils.isNotEmpty(allele.getVariants())) {
+                String name = "";
+            }
+        });
+
         AlleleAllianceCacheManager manager = new AlleleAllianceCacheManager();
         for (Map.Entry<String, List<Allele>> entry : map.entrySet()) {
             JsonResultResponse<Allele> result = new JsonResultResponse<>();
@@ -42,7 +48,7 @@ public class AlleleCacher extends Cacher {
         setCacheStatus(allAlleles.size(), CacheAlliance.ALLELE.getCacheName());
 
         alleleRepository.clearCache();
-        
+
     }
 
 }
