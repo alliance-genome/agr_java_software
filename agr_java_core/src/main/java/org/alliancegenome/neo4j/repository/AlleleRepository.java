63187843f8893242776e79ed3eb5ae6c252aee4c
Christian Pich <cmpich@zfin.org>
2020-01-27 20:42:11 +0100

    Make Gene-Variant relationship optional

--- agr_java_core/src/main/java/org/alliancegenome/neo4j/repository/AlleleRepository.java
+++ agr_java_core/src/main/java/org/alliancegenome/neo4j/repository/AlleleRepository.java
@@ -76,11 +76,11 @@ public class AlleleRepository extends Neo4jRepository<Allele> {
         String paramName = "alleleID";
         map.put(paramName, id);
         String query = "";
-        query += " MATCH p1=(a:Allele)<-[:VARIATION]-(variant:Variant)--(soTerm:SOTerm)," +
-                "        p2=(variant:Variant)-[]-(:Gene)-[:ASSOCIATION]->(:GenomicLocation)-[:ASSOCIATION]->(:Chromosome) ";
+        query += " MATCH p1=(a:Allele)<-[:VARIATION]-(variant:Variant)--(soTerm:SOTerm) ";
         query += " WHERE a.primaryKey = {" + paramName + "}";
         query += " OPTIONAL MATCH consequence=(:GeneLevelConsequence)<-[:ASSOCIATION]-(variant:Variant)";
         query += " OPTIONAL MATCH loc=(variant:Variant)-[:ASSOCIATION]->(:GenomicLocation)-[:ASSOCIATION]->(:Chromosome)";
+        query += " OPTIONAL MATCH p2=(variant:Variant)-[]-(:Gene)-[:ASSOCIATION]->(:GenomicLocation)-[:ASSOCIATION]->(:Chromosome)";
         query += " RETURN p1, p2, loc, consequence ";
 
         Iterable<Variant> alleles = query(Variant.class, query, map);
