9f211256c4488cce2b5ce7b9cc09426653a12cd0
Prita Mani <prita@uoregon.edu>
2020-02-28 13:21:40 -0800

    AGR-1936 disease-allele table download

--- agr_api/src/main/java/org/alliancegenome/api/rest/interfaces/DiseaseRESTInterface.java
+++ agr_api/src/main/java/org/alliancegenome/api/rest/interfaces/DiseaseRESTInterface.java
@@ -107,7 +107,7 @@ public interface DiseaseRESTInterface {
     @GET
     @Path("/{id}/alleles/download")
     @Produces(MediaType.TEXT_PLAIN)
-    @ApiOperation(value = "Retrieve all DiseaseAnnotation records for a given disease id", hidden=true)
+    @ApiOperation(value = "Retrieve all DiseaseAnnotation records for a given disease id")
     Response getDiseaseAnnotationsByAlleleDownload(
             @ApiParam(name = "id", value = "Disease by DOID: e.g. DOID:9952", required = true, type = "String")
             @PathParam("id") String id,
