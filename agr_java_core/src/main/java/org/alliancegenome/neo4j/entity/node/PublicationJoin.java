73292c0257fc762fb5acdfc644ad9eade270bb0a
Christian Pich <cmpich@zfin.org>
2020-03-12 18:31:12 +0100

    AGR-2051 make sure all PublicationJoin nodes are retrieved from Neo: removing equals() method

--- agr_java_core/src/main/java/org/alliancegenome/neo4j/entity/node/PublicationJoin.java
+++ agr_java_core/src/main/java/org/alliancegenome/neo4j/entity/node/PublicationJoin.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import org.alliancegenome.neo4j.view.View;
 import org.neo4j.ogm.annotation.NodeEntity;
 import org.neo4j.ogm.annotation.Relationship;
@@ -48,25 +49,4 @@ public class PublicationJoin extends Association {
         return publication.getPubId() + ":" + ecos;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        PublicationJoin that = (PublicationJoin) o;
-        if (Objects.equals(joinType, that.joinType) &&
-                Objects.equals(publication, that.publication)) {
-            if (Objects.equals(ecoCode, that.ecoCode))
-                return true;
-            if (ecoCode == null || that.ecoCode == null)
-                return false;
-            return ecoCode.containsAll(that.ecoCode) && that.ecoCode.containsAll(ecoCode);
-
-        }
-        return false;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(joinType, publication, ecoCode);
-    }
 }
