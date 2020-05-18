7697f57100890cadf3a0f5588a9157478a83ed24
Christian Pich <cmpich@zfin.org>
2020-05-18 14:17:15 +0200

    AGR-2169 remove View.DefaultView from Json View

--- agr_java_core/src/main/java/org/alliancegenome/neo4j/entity/node/TranscriptLevelConsequence.java
+++ agr_java_core/src/main/java/org/alliancegenome/neo4j/entity/node/TranscriptLevelConsequence.java
@@ -12,46 +12,46 @@ import org.neo4j.ogm.annotation.NodeEntity;
 @Setter
 public class TranscriptLevelConsequence extends Neo4jEntity implements Comparable<TranscriptLevelConsequence> {
 
-    @JsonView({View.Default.class, View.API.class})
+    @JsonView({View.API.class})
     private String aminoAcidChange;
 
-    @JsonView({View.Default.class, View.API.class})
+    @JsonView({View.API.class})
     private String aminoAcidVariation;
 
-    @JsonView({View.Default.class, View.API.class})
+    @JsonView({View.API.class})
     private String aminoAcidReference;
 
-    @JsonView({View.Default.class, View.API.class})
+    @JsonView({View.API.class})
     private String codonChange;
 
-    @JsonView({View.Default.class, View.API.class})
+    @JsonView({View.API.class})
     private String codonReference;
 
-    @JsonView({View.Default.class, View.API.class})
+    @JsonView({View.API.class})
     private String codonVariation;
 
-    @JsonView({View.Default.class, View.API.class})
+    @JsonView({View.API.class})
     private String transcriptLevelConsequence;
 
-    @JsonView({View.Default.class, View.API.class})
+    @JsonView({View.API.class})
     private String cdsStartPosition;
 
-    @JsonView({View.Default.class, View.API.class})
+    @JsonView({View.API.class})
     private String cdsEndPosition;
 
-    @JsonView({View.Default.class, View.API.class})
+    @JsonView({View.API.class})
     private String cdnaStartPosition;
 
-    @JsonView({View.Default.class, View.API.class})
+    @JsonView({View.API.class})
     private String cdnaEndPosition;
 
-    @JsonView({View.Default.class, View.API.class})
+    @JsonView({View.API.class})
     private String proteinStartPosition;
 
-    @JsonView({View.Default.class, View.API.class})
+    @JsonView({View.API.class})
     private String proteinEndPosition;
 
-    @JsonView({View.Default.class, View.API.class})
+    @JsonView({View.API.class})
     private String hgvsProteinNomenclature;
 
     @Override
