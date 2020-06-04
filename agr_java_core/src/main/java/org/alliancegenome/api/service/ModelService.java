02f960063d4c8039defb619a8cfe57e80020e61b
Christian Pich <cmpich@zfin.org>
2020-06-04 20:26:10 +0200

    AGR-2193 fix the cacher for gene-indexed model DAs

---
+++ agr_java_core/src/main/java/org/alliancegenome/api/service/ModelService.java
@@ -0,0 +1,25 @@
+package org.alliancegenome.api.service;
+
+import org.alliancegenome.neo4j.entity.DiseaseAnnotation;
+import org.alliancegenome.neo4j.entity.PrimaryAnnotatedEntity;
+import org.alliancegenome.neo4j.entity.node.AffectedGenomicModel;
+import org.alliancegenome.neo4j.entity.node.GeneticEntity;
+
+public class ModelService {
+
+    public static PrimaryAnnotatedEntity getPrimaryAnnotatedEntity(DiseaseAnnotation annotation) {
+        AffectedGenomicModel model = annotation.getModel();
+        PrimaryAnnotatedEntity entity = new PrimaryAnnotatedEntity();
+        entity.setId(model.getPrimaryKey());
+        entity.setEntityJoinPk(annotation.getPrimaryKey());
+        entity.setName(model.getName());
+        entity.setDisplayName(model.getNameText());
+        entity.setUrl(model.getModCrossRefCompleteUrl());
+        entity.setType(GeneticEntity.CrossReferenceType.getCrossReferenceType(model.getSubtype()));
+        entity.addPublicationEvidenceCode(annotation.getPublicationJoins());
+        entity.addDisease(annotation.getDisease());
+        entity.setDataProvider(model.getDataProvider());
+        entity.setSpecies(model.getSpecies());
+        return entity;
+    }
+}
