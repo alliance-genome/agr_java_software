201a4f34c50cb72ac8462e8cd434efe9c58cb08e
Kalpana Karra <kkarra@stanford.edu>
2020-07-14 10:47:20 -0700

    refactored GeneExtractor

--- agr_intermine_data_extractor/src/main/java/org/alliancegenome/data_extractor/extractors/GeneExtractor.java
+++ agr_intermine_data_extractor/src/main/java/org/alliancegenome/data_extractor/extractors/GeneExtractor.java
@@ -6,20 +6,20 @@ import java.util.Map;
 
 import org.alliancegenome.data_extractor.translators.GeneTSVTranslator;
 import org.alliancegenome.neo4j.entity.node.Gene;
-import org.alliancegenome.neo4j.repository.GeneDataExtractorRepository;
+import org.alliancegenome.neo4j.repository.DataExtractorRepository;
 import org.alliancegenome.neo4j.repository.GeneRepository;
 import org.neo4j.ogm.model.Result;
 
 public class GeneExtractor extends DataExtractor {
 
-    private GeneDataExtractorRepository geneRepo = new GeneDataExtractorRepository();
+    private DataExtractorRepository repo = new DataExtractorRepository();
 
     @Override
     protected void extract(PrintWriter writer) {
         
         GeneTSVTranslator translator = new GeneTSVTranslator(writer);
         
-        Result gene_res = geneRepo.getAllGenes();
+        Result gene_res = repo.getAllGenes();
         
         translator.translateResult(gene_res);
 
