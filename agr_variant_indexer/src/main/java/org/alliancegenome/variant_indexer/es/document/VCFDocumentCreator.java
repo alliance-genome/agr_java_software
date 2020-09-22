01a756c71d09236e5a8666100ffe969c2ebcb945
Olin Blodgett <nilo@nilosplace.net>
2020-09-22 19:18:01 -0400

    Added new queuing for less memory

--- agr_variant_indexer/src/main/java/org/alliancegenome/variant_indexer/es/document/VCFDocumentCreator.java
+++ agr_variant_indexer/src/main/java/org/alliancegenome/variant_indexer/es/document/VCFDocumentCreator.java
@@ -1,12 +1,18 @@
 package org.alliancegenome.variant_indexer.es.document;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.RejectedExecutionHandler;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.zip.GZIPOutputStream;
 
 import org.alliancegenome.es.util.EsClientFactory;
 import org.alliancegenome.es.util.ProcessDisplayHelper;
@@ -36,33 +42,28 @@ public class VCFDocumentCreator extends Thread {
 
     private String vcfFilePath;
     private int taxon;
+    private String speciesName;
     
     private BulkProcessor.Builder builder;
     private BulkProcessor bulkProcessor;
     public static String indexName;
-    private ProcessDisplayHelper ph = new ProcessDisplayHelper(12000);
+    
+    private ProcessDisplayHelper ph1 = new ProcessDisplayHelper(12000);
     private ProcessDisplayHelper ph2 = new ProcessDisplayHelper(12000);
-
-    //private double json_avg;
-
-    //private LinkedBlockingDeque<Runnable> runningQueue = new LinkedBlockingDeque<Runnable>(VariantConfigHelper.getContextProcessorTaskQueueSize());
+    private ProcessDisplayHelper ph3 = new ProcessDisplayHelper(12000);
     
-    private VariantContextConverter converter;
     private RestHighLevelClient client = EsClientFactory.createNewClient();
-    
-//  private ThreadPoolExecutor variantContextProcessorTaskExecuter = new ThreadPoolExecutor(
-//      1, 
-//      VariantConfigHelper.getContextProcessorTaskThreads(), 
-//      10, 
-//      TimeUnit.MILLISECONDS, 
-//      runningQueue
-//  );
+
+    private LinkedBlockingDeque<VariantContext> vcQueue = new LinkedBlockingDeque<VariantContext>(VariantConfigHelper.getDocumentCreatorContextQueueSize());
+    private LinkedBlockingDeque<String> jsonQueue = new LinkedBlockingDeque<String>(VariantConfigHelper.getDocumentCreatorJsonQueueSize());
     
     public VCFDocumentCreator(DownloadableFile downloadFile, String speciesName, int taxon) {
         this.vcfFilePath = downloadFile.getLocalGzipFilePath();
-        this.taxon = taxon;
-        converter = VariantContextConverter.getConverter(speciesName);
-
+        this.speciesName = speciesName;
+    }
+    
+    public void run() {
+        
         BulkProcessor.Listener listener = new BulkProcessor.Listener() { 
             @Override
             public void beforeBulk(long executionId, BulkRequest request) {
@@ -83,122 +84,91 @@ public class VCFDocumentCreator extends Thread {
                 log.error("Finished Adding requests to Queue:");
             }
         };
-
+        
         log.info("Creating Bulk Processor");
         builder = BulkProcessor.builder((request, bulkListener) -> client.bulkAsync(request, RequestOptions.DEFAULT, bulkListener), listener);
         builder.setBulkActions(VariantConfigHelper.getEsBulkActionSize());
-        builder.setConcurrentRequests(VariantConfigHelper.getEsBulkConcurrentRequestsAmount());
+        builder.setConcurrentRequests(VariantConfigHelper.getEsBulkConcurrentRequests());
         builder.setBulkSize(new ByteSizeValue(VariantConfigHelper.getEsBulkSizeMB(), ByteSizeUnit.MB));
         builder.setFlushInterval(TimeValue.timeValueSeconds(180L));
         builder.setBackoffPolicy(BackoffPolicy.exponentialBackoff(TimeValue.timeValueSeconds(1L), 60));
 
         bulkProcessor = builder.build();
+        
+        VCFReader reader = new VCFReader();
+        reader.start();
+        
+        VCFTransformer trans = new VCFTransformer();
+        trans.start();
+        
+        VCFJsonIndexer indexer = new VCFJsonIndexer();
+        indexer.start();
 
-    }
-
-    public void run() {
-
-        ph.startProcess("Reading VC/s: ", 0);
-        ph2.startProcess("Bulk Request: ", 0);
         try {
-            VCFFileReader reader = new VCFFileReader(new File(vcfFilePath), false);
-            CloseableIterator<VariantContext> iter1 = reader.iterator();
-
-//          variantContextProcessorTaskExecuter.setRejectedExecutionHandler(new RejectedExecutionHandler() {
-//              public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
-//                  try {
-//                      executor.getQueue().offer(r, 10, TimeUnit.DAYS);
-//                  } catch (InterruptedException e) {
-//                      e.printStackTrace();
-//                  }
-//              }
-//          });
-
-            List<VariantContext> workChunk = new ArrayList<>();
-
-            while(iter1.hasNext()) {
-                try {
-                    VariantContext vc = iter1.next();
-
-                    workChunk.add(vc);
-                    
-                    if(workChunk.size() >= VariantConfigHelper.getDocumentCreatorWorkChunkSize()) {
-                        //variantContextProcessorTaskExecuter.execute(new VariantContextProcessorTask(workChunk, taxon));
-                        
-                        for(VariantContext ctx: workChunk) {
-                            List<String> docs = converter.convertVariantContext(ctx, taxon);
-                            
-                            for(String doc: docs) {
-                                //json_avg = runningAverage(json_avg, doc.length(), 1_000_000);
-                                bulkProcessor.add(new IndexRequest(indexName).source(doc, XContentType.JSON));
-                                ph2.progressProcess();
-                            }
-                        }
-                        
-                        workChunk = new ArrayList<>();
-
-                    }
-                    ph.progressProcess();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-            if(workChunk.size() > 0) {
-                //variantContextProcessorTaskExecuter.execute(new VariantContextProcessorTask(workChunk, taxon));
-                
-                for(VariantContext ctx: workChunk) {
-                    List<String> docs = converter.convertVariantContext(ctx, taxon);
-                    
-                    for(String doc: docs) {
-                        //json_avg = runningAverage(json_avg, doc.length(), 1_000_000);
-                        bulkProcessor.add(new IndexRequest(indexName).source(doc, XContentType.JSON));
-                        ph2.progressProcess();
-                    }
-                }
-            }
-            ph.finishProcess();
-
-//          while(runningQueue.size() > 0) {
-//              Thread.sleep(1000);
-//          }
-            
-//          variantContextProcessorTaskExecuter.shutdown();
-//          while (!variantContextProcessorTaskExecuter.isTerminated()) {
-//              Thread.sleep(1000);
-//          }
-            ph2.finishProcess();
-            log.debug("Finished all threads");
-            
-            reader.close();
-        } catch (Exception e) {
+            reader.join();
+            trans.join();
+            indexer.join();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
         }
 
     }
     
-    private double runningAverage(double avg, double new_sample, int size) {
-        return (avg * (size - 1) / size) + (new_sample / size);
-    }
-
-    private class VariantContextProcessorTask implements Runnable {
-
-        private List<VariantContext> workChunk;
-        private int taxon;
-        public VariantContextProcessorTask(List<VariantContext> workChunk, int taxon) {
-            this.workChunk = workChunk;
-            this.taxon = taxon;
-        }
-
+    private class VCFReader extends Thread {
         public void run() {
-            for(VariantContext ctx: workChunk) {
-                List<String> docs = converter.convertVariantContext(ctx, taxon);
-                
-                for(String doc: docs) {
-                    //json_avg = runningAverage(json_avg, doc.length(), 1_000_000);
-                    bulkProcessor.add(new IndexRequest(indexName).source(doc, XContentType.JSON));
-                    //ph2.progressProcess();
+            ph1.startProcess("VCFReader: " + vcfFilePath);
+            VCFFileReader reader = new VCFFileReader(new File(vcfFilePath), false);
+            CloseableIterator<VariantContext> iter1 = reader.iterator();
+            
+            while(iter1.hasNext()) {
+                try {
+                    VariantContext vc = iter1.next();
+                    ph1.progressProcess();
+                    vcQueue.offer(vc, 10, TimeUnit.DAYS);
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
+            ph1.finishProcess();
+            reader.close();
+        }
+    }
+    
+    private class VCFTransformer extends Thread {
+        public void run() {
+            ph2.startProcess("VCFTransformer: " + speciesName);
+            VariantContextConverter converter = VariantContextConverter.getConverter(speciesName);
+            try {
+                while(true) {
+                    VariantContext ctx = vcQueue.takeFirst();
+                    List<String> docs = converter.convertVariantContext(ctx, taxon);
+                    
+                    for(String doc: docs) {
+                        ph2.progressProcess("VCQueue: " + vcQueue.size() + " JsonQueue: " + jsonQueue.size());
+                        jsonQueue.offer(doc, 10, TimeUnit.DAYS);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            ph2.finishProcess();
+        }
+    }
+    
+    private class VCFJsonIndexer extends Thread {
+        public void run() {
+            ph3.startProcess("VCFJsonIndexer: " + indexName);
+            try {
+                while(true) {
+                    String doc = jsonQueue.takeFirst();
+                    ph3.progressProcess();
+                    bulkProcessor.add(new IndexRequest(indexName).source(doc, XContentType.JSON));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            ph3.finishProcess();
         }
     }
 
