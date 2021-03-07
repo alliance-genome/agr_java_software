d5e513905c6e4dcdd3b47d53ed701c2a310c39d1
Pinglei Zhou <zhou@morgan.harvard.edu>
2021-03-06 21:55:38 -0500

    AGR-2637 fix column filters: Detection Methods, Source and Reference in Molecular Interaction, Source columm in Genetic Interaction

--- agr_java_core/src/main/java/org/alliancegenome/api/service/InteractionColumnFieldMapping.java
+++ agr_java_core/src/main/java/org/alliancegenome/api/service/InteractionColumnFieldMapping.java
@@ -2,8 +2,10 @@ package org.alliancegenome.api.service;
 
 import static org.alliancegenome.api.service.Column.*;
 
+import java.sql.Ref;
 import java.util.*;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import org.alliancegenome.es.model.query.FieldFilter;
 import org.alliancegenome.neo4j.entity.node.InteractionGeneJoin;
@@ -23,7 +25,7 @@ public class InteractionColumnFieldMapping extends ColumnFieldMapping<Interactio
         mapColumnFieldName.put(INTERACTOR_SPECIES, FieldFilter.INTERACTOR_SPECIES);
         mapColumnFieldName.put(INTERACTOR_GENE_SYMBOL, FieldFilter.INTERACTOR_GENE_SYMBOL);
         mapColumnFieldName.put(INTERACTOR_SOURCE, FieldFilter.SOURCE);
-        mapColumnFieldName.put(INTERACTOR_REFERENCE, FieldFilter.FREFERENCE);
+        mapColumnFieldName.put(INTERACTOR_REFERENCE, FieldFilter.INTERACTOR_REFERENCE);
         mapColumnFieldName.put(INTERACTOR_DETECTION_METHOD, FieldFilter.DETECTION_METHOD);
         //genetic interaction
         mapColumnFieldName.put(ROLE, FieldFilter.ROLE);
@@ -33,18 +35,41 @@ public class InteractionColumnFieldMapping extends ColumnFieldMapping<Interactio
         mapColumnFieldName.put(PHENOTYPES, FieldFilter.PHENOTYPES);
         mapColumnFieldName.put(INTERACTION_TYPE, FieldFilter.INTERACTION_TYPE);
         
+        //those will used for DistinctFieldValueSupplementalData
         mapColumnAttribute.put(INTERACTOR_MOLECULE_TYPE, (join) -> Set.of(join.getInteractorBType().getDisplayName()));
         mapColumnAttribute.put(MOLECULE_TYPE, (join) -> Set.of(join.getInteractorAType().getDisplayName()));
         mapColumnAttribute.put(INTERACTOR_SPECIES, (join) -> Set.of(join.getGeneB().getSpecies().getName()));
+        mapColumnAttribute.put(INTERACTOR_DETECTION_METHOD, (join) -> {
+        	if (join.getDetectionsMethods()==null)
+        		return Set.of("");
+        	else {
+              return join.getDetectionsMethods().stream().map(method-> method.getLabel()).collect(Collectors.toSet()   );
+             }
+         });
+        mapColumnAttribute.put(INTERACTOR_REFERENCE, (join)->Set.of(join.getPublication().getPubId()));
         //for genetic interaction
         mapColumnAttribute.put(ROLE, (join) -> Set.of(join.getInteractorARole().getDisplayName()));
         mapColumnAttribute.put(INTERACTOR_ROLE, (join) -> Set.of(join.getInteractorBRole().getDisplayName()));
         mapColumnAttribute.put(INTERACTION_TYPE, (join) -> Set.of(join.getInteractionType().getDisplayName()));
+        mapColumnAttribute.put(INERACTION_SOURCE, (join)->{
+        	Set<String> hashSet = new HashSet<String>();
+        	if (join.getCrossReferences()!=null) {
+        		hashSet=join.getCrossReferences().stream().map(cross->cross.getPrefix() + ":" + cross.getDisplayName()).collect(Collectors.toSet());
+        	}
+        	if (join.getSourceDatabase()!=null)
+        		hashSet.add(join.getSourceDatabase().getLabel() + " " + join.getSourceDatabase().getDisplayName());
+        	if (join.getAggregationDatabase() !=null)
+        		hashSet.add(join.getAggregationDatabase().getLabel() + " " + join.getAggregationDatabase().getDisplayName());
+        	return hashSet;
+        }
+        );
         
         singleValueDistinctFieldColumns.add(INTERACTOR_MOLECULE_TYPE);
         singleValueDistinctFieldColumns.add(MOLECULE_TYPE);
         singleValueDistinctFieldColumns.add(JOIN_TYPE);
         singleValueDistinctFieldColumns.add(INTERACTOR_SPECIES);
+        singleValueDistinctFieldColumns.add(INTERACTOR_DETECTION_METHOD);
+        singleValueDistinctFieldColumns.add(INTERACTOR_REFERENCE);
         //genetic interaction
         singleValueDistinctFieldColumns.add(ROLE);
         singleValueDistinctFieldColumns.add(GENETIC_PERTURBATION);
