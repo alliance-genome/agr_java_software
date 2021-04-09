0b5da73f573b6158f2e8c22e208a71264ac9a89f
Adam <agibson@mcw.edu>
2021-04-09 10:24:06 -0500

    Removed getDocumentId from ESDocument

--- agr_java_core/src/main/java/org/alliancegenome/es/index/ESDocument.java
+++ agr_java_core/src/main/java/org/alliancegenome/es/index/ESDocument.java
@@ -4,9 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public abstract class ESDocument {
 
-//    @JsonIgnore
-//    public abstract String getDocumentId();
-
     @JsonIgnore
     public abstract String getType();
 }
