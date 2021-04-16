20c1411702c4d5b789ba033995adea1b3e72aa67
Olin Blodgett <nilo@nilosplace.net>
2021-04-16 16:45:00 -0400

    Added performance tuning

--- agr_indexer/src/main/java/org/alliancegenome/indexer/config/IndexerConfig.java
+++ agr_indexer/src/main/java/org/alliancegenome/indexer/config/IndexerConfig.java
@@ -4,25 +4,38 @@ import org.alliancegenome.indexer.indexers.*;
 
 public enum IndexerConfig {
 
-    // These numbers target a 10KB document batch size
-    GeneIndexer("gene", GeneIndexer.class, 1, 240),
-    DatasetIndexer("dataset", DatasetIndexer.class, 2, 3000),
-    DiseaseIndexer("disease", DiseaseIndexer.class, 2, 150),
-    AlleleIndexer("allele", AlleleIndexer.class, 2, 500),
-    GoIndexer("go", GoIndexer.class, 2, 2300),
-    ModelIndexer("model", ModelIndexer.class, 2, 750), //just guessing at batching number
-    VariantIndexer("variant", VariantIndexer.class, 2, 3000);
-
+    // These numbers target a 1MB document batch size
+    // Buffer size = 1MB / average size
+    // Bulk Action Size = Buffer Size
+    // Bulk Size = 1MB
+    // ConCurrent Reqests = Threads * 2
+    // Document Stats as of 4/16/2021 ---- (doc count, min size, max size, average size) 
+    GeneIndexer("gene", GeneIndexer.class, 2, 359, 359, 4, 1),          // (281090, 616, 156692, 2785)
+    DatasetIndexer("dataset", DatasetIndexer.class, 2, 566, 566, 4, 1), // (8094,   358,  90542, 1765)
+    DiseaseIndexer("disease", DiseaseIndexer.class, 2, 680, 680, 4, 1), // (10751,  191, 330099, 1472)
+    AlleleIndexer("allele", AlleleIndexer.class, 2, 1517, 1517, 4, 1),  // (407492, 334,  69968,  659) 
+    GoIndexer("go", GoIndexer.class, 2, 914, 914, 4, 1),                // (44087,  262, 460247, 1093)
+    ModelIndexer("model", ModelIndexer.class, 2, 1426, 1426, 4, 1),     // (132454, 314,  18593,  701)
+    // still not implemented  VariantIndexer("variant", VariantIndexer.class, 2, 3000, 400, 4, 4) 
+    ;
+    
+    
     private String typeName;
     private Class<?> indexClazz;
     private int threadCount;
     private int bufferSize;
+    private int bulkActions;
+    private int concurrentRequests;
+    private int bulkSize;
 
-    IndexerConfig(String typeName, Class<?> indexClazz, int threadCount, int bufferSize) {
+    IndexerConfig(String typeName, Class<?> indexClazz, int threadCount, int bufferSize, int bulkActions, int concurrentRequests, int bulkSize) {
         this.typeName = typeName;
         this.indexClazz = indexClazz;
         this.threadCount = threadCount;
         this.bufferSize = bufferSize;
+        this.bulkActions = bulkActions;
+        this.concurrentRequests = concurrentRequests;
+        this.bufferSize = bulkSize;
     }
 
     public String getTypeName() {
@@ -39,6 +52,18 @@ public enum IndexerConfig {
 
     public int getBufferSize() {
         return bufferSize;
+    }
+    
+    public int getBulkActions() {
+        return bulkActions;
+    }
+    
+    public int getConcurrentRequests() {
+        return concurrentRequests;
+    }
+    
+    public int getBulkSize() {
+        return bulkSize;
     }
 
 }
