2a1555698d26a263498e048bc316d4a4c8ea3c33
gildossantos <gilbertodossantos74@gmail.com>
2021-04-27 15:59:35 -0400

    add back regexpQuery step

--- agr_api/src/main/java/org/alliancegenome/api/service/SearchService.java
+++ agr_api/src/main/java/org/alliancegenome/api/service/SearchService.java
@@ -178,7 +178,7 @@ public class SearchService {
     }
 
     private FunctionScoreQueryBuilder.FilterFunctionBuilder rnaBoost() {
-        return new FunctionScoreQueryBuilder.FilterFunctionBuilder(matchQuery("soTermName",".*RNA"),
+        return new FunctionScoreQueryBuilder.FilterFunctionBuilder(regexpQuery("soTermName",".*RNA"),
                 ScoreFunctionBuilders.weightFactorFunction(1.2F));
     }
 
