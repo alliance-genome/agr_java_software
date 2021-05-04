85e2ce594a53087f8c6d2e8c70cc09e8aa42b555
adamgibs <adamgibs@outlook.com>
2021-05-04 16:45:46 -0500

    AGR-2842 added species to TranscriptLevelConsequence

--- agr_java_core/src/main/java/org/alliancegenome/core/variant/converters/AlleleVariantSequenceConverter.java
+++ agr_java_core/src/main/java/org/alliancegenome/core/variant/converters/AlleleVariantSequenceConverter.java
@@ -70,7 +70,7 @@ public class AlleleVariantSequenceConverter {
             boolean first=true;
             Set<String> molecularConsequences = new HashSet<>();
             Set<String> genes = new HashSet<>();
-            List<TranscriptLevelConsequence> htpConsequences = getConsequences(ctx, a.getBaseString(), header);
+            List<TranscriptLevelConsequence> htpConsequences = getConsequences(ctx, a.getBaseString(), header, species);
             String hgvsNomenclature = htpConsequences != null ? htpConsequences.stream()
                     .findFirst()
                     .map(TranscriptLevelConsequence::getHgvsVEPGeneNomenclature)
@@ -132,7 +132,7 @@ public class AlleleVariantSequenceConverter {
 
     
     
-    private List<TranscriptLevelConsequence> getConsequences(VariantContext ctx, String varNuc, String[] header) throws Exception {
+    private List<TranscriptLevelConsequence> getConsequences(VariantContext ctx, String varNuc, String[] header, Species species) throws Exception {
         List<TranscriptLevelConsequence> features = new ArrayList<>();
         List<String> alreadyAdded = new ArrayList<>();
         
@@ -142,7 +142,7 @@ public class AlleleVariantSequenceConverter {
 
                 if (header.length == infos.length) {
                     if (infos[0].equalsIgnoreCase(varNuc)) {
-                        TranscriptLevelConsequence feature = new TranscriptLevelConsequence(header, infos);
+                        TranscriptLevelConsequence feature = new TranscriptLevelConsequence(header, infos, species);
                         if(!alreadyAdded.contains(feature.getTranscriptID())) {
                             features.add(feature);
                             alreadyAdded.add(feature.getTranscriptID());
