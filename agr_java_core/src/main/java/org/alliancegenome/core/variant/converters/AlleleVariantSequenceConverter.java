d36e811a5e2353ba041832fc035e44fd8870a3b2
Olin Blodgett <nilo@nilosplace.net>
2021-06-08 08:29:57 -0400

    Not vs None

--- agr_java_core/src/main/java/org/alliancegenome/core/variant/converters/AlleleVariantSequenceConverter.java
+++ agr_java_core/src/main/java/org/alliancegenome/core/variant/converters/AlleleVariantSequenceConverter.java
@@ -81,7 +81,7 @@ public class AlleleVariantSequenceConverter {
                     .orElse(null) : null;
 
             StringBuilder hgvsSynonym = new StringBuilder();
-            if(StringUtils.isNoneEmpty(hgvsNomenclature)){
+            if(StringUtils.isNotEmpty(hgvsNomenclature)){
                 hgvsSynonym.append('(')
                         .append(speciesType.getAssembly())
                         .append(')')
