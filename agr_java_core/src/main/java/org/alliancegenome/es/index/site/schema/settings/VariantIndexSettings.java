c4a3f6056c0a93b13dbff3d25752eba32bf56e2b
Olin Blodgett <nilo@nilosplace.net>
2021-06-10 20:53:14 -0400

    Added translog option

--- agr_java_core/src/main/java/org/alliancegenome/es/index/site/schema/settings/VariantIndexSettings.java
+++ agr_java_core/src/main/java/org/alliancegenome/es/index/site/schema/settings/VariantIndexSettings.java
@@ -20,15 +20,15 @@ public class VariantIndexSettings extends Settings {
             .startObject("index")
                 .field("number_of_replicas", "0")
                 .field("refresh_interval", "-1")
-                .field("number_of_shards", "" + shardCount);
+                .field("number_of_shards", "" + shardCount)
                 //.field("merge.scheduler.auto_throttle", "false")
                 //.field("merge.scheduler.max_merge_count", "10")
                 //.field("merge.scheduler.max_thread_count", "10")
                 //.field("merge.policy.floor_segment", "200mb")
                 //.field("merge.policy.max_merged_segment", "500mb")
-                //.field("translog.sync_interval", "15s")
-                //.field("translog.flush_threshold_size", "4096mb")
-                //.field("translog.durability", "async");
+                .field("translog.sync_interval", "60s")
+                .field("translog.flush_threshold_size", "4096mb")
+                .field("translog.durability", "async");
                 //.field("codec", "best_compression");
             //buildAnalysis(true);
             builder.endObject();
