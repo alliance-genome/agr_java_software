3fa5b7df10fc51c433eba497d30ae1f6c1294e09
Olin Blodgett <nilo@nilosplace.net>
2022-01-14 04:45:57 -0500

    Added code to use buffers

--- agr_variant_indexer/src/test/java/org/alliancegenome/variant_indexer/TestDownloadSplitUploadHuman.java
+++ agr_variant_indexer/src/test/java/org/alliancegenome/variant_indexer/TestDownloadSplitUploadHuman.java
@@ -2,7 +2,7 @@ package org.alliancegenome.variant_indexer;
 
 import java.io.File;
 import java.util.*;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 import org.alliancegenome.core.config.ConfigHelper;
 import org.alliancegenome.es.util.ProcessDisplayHelper;
@@ -15,12 +15,11 @@ import com.amazonaws.services.s3.transfer.*;
 import htsjdk.samtools.util.CloseableIterator;
 import htsjdk.variant.variantcontext.VariantContext;
 import htsjdk.variant.variantcontext.writer.*;
-import htsjdk.variant.vcf.VCFFileReader;
+import htsjdk.variant.vcf.*;
 import lombok.extern.jbosslog.JBossLog;
 
 @JBossLog
 public class TestDownloadSplitUploadHuman {
-
     
     private AmazonS3 s3Client;
 
@@ -36,9 +35,9 @@ public class TestDownloadSplitUploadHuman {
         String type = "HUMAN";
         String bucket = "mod-datadumps";
         String inputDir =  "/Users/olinblodgett/git/agr_java_software/agr_variant_indexer/data";
-        String outputDir = "/Users/olinblodgett/git/agr_java_software/agr_variant_indexer/data";
+        String outputDir = "/Volumes/Cardano_Backup/Variants";
 
-        String variantFile = type + "_HTPOSTVEPVCF_20210728.vcf.gz";
+        String variantFile = type + "_HTPOSTVEPVCF_20211214.vcf.gz";
 
         File inputFile = new File(inputDir + "/" + variantFile);
 
@@ -52,7 +51,7 @@ public class TestDownloadSplitUploadHuman {
                 while(!myDownload.isDone()) {
                     int pct = (int)myDownload.getProgress().getPercentTransferred();
                     if(lastPct != pct) {
-                        log.info("Percent: " + pct);
+                        log.info(variantFile + ": Percent: " + pct);
                         lastPct = pct;
                     } else {
                         TimeUnit.MILLISECONDS.sleep(500);
@@ -69,9 +68,9 @@ public class TestDownloadSplitUploadHuman {
 
         VCFFileReader reader = new VCFFileReader(inputFile, false);
 
-        String chr = "";
+        
 
-        VariantContextWriter writer = null;
+        
 
         ProcessDisplayHelper ph = new ProcessDisplayHelper(10000);
 
@@ -79,44 +78,123 @@ public class TestDownloadSplitUploadHuman {
 
         ph.startProcess("VCFReader");
 
-        TransferManager tx = TransferManagerBuilder.standard().withS3Client(s3Client).build();
-        List<Upload> uploads = new ArrayList<>();
-
+        
+        
+        //TransferManager tx = TransferManagerBuilder.standard().withS3Client(s3Client).build();
+        //List<Upload> uploads = new ArrayList<>();
+        List<Thread> threads = new ArrayList<>();
+        WriterThread current = null;
+        String chr = "";
         while(iter1.hasNext()) {
             try {
                 VariantContext vc = iter1.next();
-                if(!vc.getChr().equals(chr)) {
-                    if(writer != null) {
-                        log.info("Closing: " + outputDir + "/" + type + ".vep." + chr + ".vcf.gz");
-                        writer.close();
-                        log.info("Uploading new file: " + outputDir + "/" + type + ".vep." + chr + ".vcf.gz");
-                        Upload upload = tx.upload(bucket, type + "/" + type + ".vep." + chr + ".vcf.gz", new File(outputDir + "/" + type + ".vep." + chr + ".vcf.gz"));
-                        uploads.add(upload);
-                    }
+                if(!vc.getContig().equals(chr)) {
                     chr = vc.getContig();
-                    VariantContextWriterBuilder builder = new VariantContextWriterBuilder();
-                    log.info("Opening New file: " + outputDir + "/" + type + ".vep." + chr + ".vcf.gz");
-                    builder.setOutputFile(outputDir + "/" + type + ".vep." + chr + ".vcf.gz");
-                    writer = builder.build();
-                    writer.writeHeader(reader.getFileHeader());
+                    WriterThread thread = new WriterThread(reader.getFileHeader(), outputDir, type, chr);
+                    thread.start();
+                    threads.add(thread);
+                    if(current != null) current.finished();
+                    current = thread;
                 }
-                writer.add(vc);
+                current.add(vc);
                 ph.progressProcess();
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-        log.info("Waiting for uploads to finish: ");
-        for(Upload u: uploads) {
+        if(current != null) current.finished();
+        log.info("Waiting for threads to finish: ");
+        for (Thread t : threads) {
             try {
-                u.waitForCompletion();
-            } catch (AmazonClientException | InterruptedException e) {
+                t.join();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
         }
-        log.info("Uploads finished shuting down");
-        tx.shutdownNow();
-        writer.close();
+        log.info("Threads finished shuting down");
+        
+        //log.info("Waiting for uploads to finish: ");
+//      
+        
+//      for(Upload u: uploads) {
+//          try {
+//              u.waitForCompletion();
+//          } catch (AmazonClientException | InterruptedException e) {
+//              e.printStackTrace();
+//          }
+//      }
+        //log.info("Uploads finished shuting down");
+        //tx.shutdownNow();
         ph.finishProcess();
+    }
+    
+    public class WriterThread extends Thread {
+
+        VariantContextWriter writer = null;
+        //ConcurrentLinkedBlockingQueue<VariantContext> queue = new ConcurrentLinkedBlockingQueue<VariantContext>();
+        LinkedBlockingQueue<List<VariantContext>> queue = new LinkedBlockingQueue<List<VariantContext>>(200);
+        
+        private int workBucketSize = 250;
+        private volatile boolean finished = false;
+        private String filePath = "";
+        List<VariantContext> workBucket = new ArrayList<>();
+        
+        public WriterThread(VCFHeader vcfHeader, String outputDir, String type, String chr) {
+            VariantContextWriterBuilder builder = new VariantContextWriterBuilder();
+            filePath = outputDir + "/" + type + ".vep." + chr + ".vcf.gz";
+            log.info("Opening New file: " + filePath);
+            builder.setOutputFile(filePath);
+            writer = builder.build();
+            writer.writeHeader(vcfHeader);
+        }
+        
+        public void run() {
+            while(true) {
+                try {
+                    List<VariantContext> vcList = queue.poll(1, TimeUnit.SECONDS);
+                    if(vcList != null) {
+                        for(VariantContext vc: vcList) {
+                            writer.add(vc);
+                        }
+                        vcList.clear();
+                        
+                        if(finished) System.out.println("Remaining Items: " + queue.size());
+                    } else if(finished) {
+                        close();
+                        break;
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        
+        public void add(VariantContext vc) throws InterruptedException {
+            workBucket.add(vc);
+            if (workBucket.size() >= workBucketSize) {
+                queue.put(workBucket);
+                workBucket = new ArrayList<>();
+            }
+        }
+
+        public void finished() throws InterruptedException {
+            if(workBucket.size() > 0) {
+                queue.put(workBucket);
+                workBucket = new ArrayList<>();
+            }
+            finished = true;
+        }
+
+        private void close() {
+            if(writer != null) {
+                log.info("Closing: " + filePath);
+                writer.close();
+                //log.info("Uploading new file: " + outputDir + "/" + type + ".vep." + chr + ".vcf.gz");
+                //Upload upload = tx.upload(bucket, type + "/" + type + ".vep." + chr + ".vcf.gz", new File(outputDir + "/" + type + ".vep." + chr + ".vcf.gz"));
+                //uploads.add(upload);
+            }
+        }
+        
     }
 }
