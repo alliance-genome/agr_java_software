e55bdf5b8b37806daa8259d9501c83272d29a086
Olin Blodgett <nilo@nilosplace.net>
2022-09-01 13:53:04 -0400

    Fixed release dates

--- agr_java_core/src/main/java/org/alliancegenome/es/util/DateConverter.java
+++ agr_java_core/src/main/java/org/alliancegenome/es/util/DateConverter.java
@@ -1,47 +1,46 @@
 package org.alliancegenome.es.util;
 
 import java.text.*;
+import java.time.OffsetDateTime;
 import java.util.Date;
 
 import org.neo4j.ogm.typeconversion.AttributeConverter;
 
 public class DateConverter implements AttributeConverter<Date, String> {
 
-    private DateFormat format1;
-    private DateFormat format2;
-    private DateFormat format3;
-    
-    @Override
-    public String toGraphProperty(Date value) {
-        return value.toString();
-    }
+	private DateFormat format1;
+	private DateFormat format2;
+	private DateFormat format3;
+	
+	@Override
+	public String toGraphProperty(Date value) {
+		return value.toString();
+	}
 
-    @Override
-    public Date toEntityAttribute(String value) {
+	@Override
+	public Date toEntityAttribute(String value) {
 
-        //allele: 2021-02-22T18:39:40-05:00     
-        //format1 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss Z");//AllianceReleaseInfo: 2021-06-23T04:00:00.000+0000 
+		format1 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");//AllianceReleaseInfo: 2021-06-23T04:00:00.000+0000
+		format2 = new SimpleDateFormat("dd:MM:yyyy HH:mm");//OntologyFileMata.date 11:05:2020 13:57
+		format3 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");//ModeFileMetadata: 2021-01-25T18:47:29Z
 
-        format1 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");//AllianceReleaseInfo: 2021-06-23T04:00:00.000+0000
-        format2 = new SimpleDateFormat("dd:MM:yyyy HH:mm");//OntologyFileMata.date 11:05:2020 13:57
-        format3 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");//ModeFileMetadata: 2021-01-25T18:47:29Z
+		try {
+			return format1.parse(value);
+		} catch (Exception e) {
+			try {
+				return format2.parse(value);
+			} catch (Exception e1) {
+				try {
+				   return format3.parse(value);
+				} catch (Exception e2) {
+					try {
+						return new Date(OffsetDateTime.parse(value).toInstant().toEpochMilli()); // This tries a bunch of other formats
+					} catch (Exception e3) {
+						return null;
+					}
+				}
+			}
+		}
+	}
 
-
-        try {
-            return format1.parse(value);
-            //return new Date();
-        } catch (Exception e) {
-            try {
-                return format2.parse(value);
-            } catch (Exception e1) {
-                try {
-                   return format3.parse(value);
-                } catch (Exception e2) {
-                    return null;
-                }
-            }
-        }
-    }
-
-    //    
 }
