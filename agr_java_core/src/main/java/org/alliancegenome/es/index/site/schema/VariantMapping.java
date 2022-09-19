436b114ab12ca1b539f4eedc20eb4826803e65ac
Olin Blodgett <nilo@nilosplace.net>
2022-09-19 16:04:56 -0400

    Round 1... Fight

--- agr_java_core/src/main/java/org/alliancegenome/es/index/site/schema/VariantMapping.java
+++ agr_java_core/src/main/java/org/alliancegenome/es/index/site/schema/VariantMapping.java
@@ -4,41 +4,41 @@ import java.io.IOException;
 
 public class VariantMapping extends Mapping {
 
-    public VariantMapping(Boolean pretty) {
-        super(pretty);
-    }
+	public VariantMapping(Boolean pretty) {
+		super(pretty);
+	}
 
-    @Override
-    public void buildMapping() {
-        try {
-            builder.startObject().startObject("properties");
-            new FieldBuilder(builder, "category", "keyword").symbol().autocomplete().keyword().build();
-            new FieldBuilder(builder, "name", "keyword").build();
-            new FieldBuilder(builder, "name_key", "keyword").build();
-            new FieldBuilder(builder, "alterationType", "text").keyword().build();
-            new FieldBuilder(builder, "genes", "text").keyword().build();
-            
-            builder.startObject("allele");
-            builder.startObject("properties");
-            builder.startObject("variants");
-            builder.startObject("properties");
-            builder.startObject("transcriptLevelConsequence");
-            builder.startObject("properties");
-            new FieldBuilder(builder, "cdsStartPosition", "long").notIndexed().build();
-            new FieldBuilder(builder, "cdnaStartPosition", "long").notIndexed().build();
-            new FieldBuilder(builder, "proteinStartPosition", "long").notIndexed().build();
-            new FieldBuilder(builder, "siftScore", "double").notIndexed().build();
-            new FieldBuilder(builder, "polyphenScore", "double").notIndexed().build();
-            builder.endObject();
-            builder.endObject();
-            builder.endObject();
-            builder.endObject();
-            builder.endObject();
-            builder.endObject();
-            builder.endObject().endObject();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+	@Override
+	public void buildMapping() {
+		try {
+			builder.startObject().startObject("properties");
+			new FieldBuilder(builder, "category", "keyword").symbol().autocomplete().keyword().build();
+			new FieldBuilder(builder, "name", "keyword").build();
+			new FieldBuilder(builder, "name_key", "keyword").build();
+			new FieldBuilder(builder, "alterationType", "text").keyword().build();
+			new FieldBuilder(builder, "genes", "text").keyword().build();
+			
+			builder.startObject("allele");
+			builder.startObject("properties");
+			builder.startObject("variants");
+			builder.startObject("properties");
+			builder.startObject("transcriptLevelConsequence");
+			builder.startObject("properties");
+			new FieldBuilder(builder, "cdsStartPosition", "long").notIndexed().build();
+			new FieldBuilder(builder, "cdnaStartPosition", "long").notIndexed().build();
+			new FieldBuilder(builder, "proteinStartPosition", "long").notIndexed().build();
+			new FieldBuilder(builder, "siftScore", "double").notIndexed().build();
+			new FieldBuilder(builder, "polyphenScore", "double").notIndexed().build();
+			builder.endObject();
+			builder.endObject();
+			builder.endObject();
+			builder.endObject();
+			builder.endObject();
+			builder.endObject();
+			builder.endObject().endObject();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
