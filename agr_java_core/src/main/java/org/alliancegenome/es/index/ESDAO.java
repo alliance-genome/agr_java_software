b2520fd6ea152c78e3db9a399ec0cb3b5748a77c
Olin Blodgett <nilo@nilosplace.net>
2022-10-20 14:46:45 -0400

    Added closable ES client and closes after 60 seconds

--- agr_java_core/src/main/java/org/alliancegenome/es/index/ESDAO.java
+++ agr_java_core/src/main/java/org/alliancegenome/es/index/ESDAO.java
@@ -1,33 +1,6 @@
 package org.alliancegenome.es.index;
 
-import java.io.IOException;
-
-import org.alliancegenome.es.util.EsClientFactory;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.elasticsearch.client.RestHighLevelClient;
-
 public class ESDAO {
 
-	private Log log = LogFactory.getLog(getClass());
-
-	protected static RestHighLevelClient searchClient = null; // Make sure to only have 1 of these clients to save on resources
-
-	public ESDAO() {
-		init();
-	}
-
-	public void init() {
-		searchClient = EsClientFactory.getDefaultEsClient();
-	}
-
-	public void close() {
-		log.info("Closing Down ES Client");
-		try {
-			searchClient.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
 
 }
