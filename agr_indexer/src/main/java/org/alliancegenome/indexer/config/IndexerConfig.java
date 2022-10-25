782cbb19524278c0d644bf799a2f9b0628825e8f
Christian Pich <cmpich@zfin.org>
2022-10-25 16:22:20 +0200

    re-add indexer

--- agr_indexer/src/main/java/org/alliancegenome/indexer/config/IndexerConfig.java
+++ agr_indexer/src/main/java/org/alliancegenome/indexer/config/IndexerConfig.java
@@ -11,14 +11,12 @@ public enum IndexerConfig {
 	// Bulk Size = 1MB
 	// ConCurrent Reqests = Threads * 2
 	// Document Stats as of 4/16/2021 ---- (doc count, min size, max size, average size)
-/*
 	GeneIndexer("gene", GeneIndexer.class, 4, 359, 359, 8, 1),            // (280954, 616, 156692, 2785)
 	DatasetIndexer("dataset", DatasetIndexer.class, 4, 566, 566, 8, 1), // (8094,	358,  90542, 1765)
 	DiseaseIndexer("disease", DiseaseIndexer.class, 4, 680, 680, 8, 1), // (10751,	191, 330099, 1472)
 	AlleleIndexer("allele", AlleleIndexer.class, 4, 1517, 1517, 8, 1),    // (407830, 334,  69968,  659)
 	GoIndexer("go", GoIndexer.class, 4, 914, 914, 8, 1),                // (44087,	262, 460247, 1093)
 	ModelIndexer("model", ModelIndexer.class, 4, 1426, 1426, 8, 1),        // (132447, 314,  18593,  701)
-*/
 	DiseaseAnnotationMlIndexer("diseaseAnnotation", DiseaseAnnotationMLIndexer.class, 4, 1426, 1426, 8, 1),        // (132447, 314,  18593,  701)
 	// still not implemented  VariantIndexer("variant", VariantIndexer.class, 2, 3000, 400, 4, 4)
 	;
