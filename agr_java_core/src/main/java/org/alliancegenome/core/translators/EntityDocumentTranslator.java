a7ab13e720417f755e110f1be15cc603adda3d1a
Olin Blodgett <nilo@nilosplace.net>
2022-10-31 15:42:04 -0400

    Added code for split DA's

--- agr_java_core/src/main/java/org/alliancegenome/core/translators/EntityDocumentTranslator.java
+++ agr_java_core/src/main/java/org/alliancegenome/core/translators/EntityDocumentTranslator.java
@@ -7,22 +7,22 @@ import org.alliancegenome.neo4j.entity.Neo4jEntity;
 
 public abstract class EntityDocumentTranslator<E extends Neo4jEntity, D extends ESDocument> {
 
-    public D translate(E entity) {
-        return translate(entity, 1);
-    }
+	public D translate(E entity) {
+		return translate(entity, 1);
+	}
 
-    public D translate(E entity, int depth) {
-        return entityToDocument(entity, depth);
-    }
+	public D translate(E entity, int depth) {
+		return entityToDocument(entity, depth);
+	}
 
-    public Iterable<D> translateEntities(Iterable<E> entities) {
-        ArrayList<D> douments = new ArrayList<D>();
-        for (E entity : entities) {
-            douments.add(translate(entity, 1));
-        }
-        return douments;
-    }
+	public Iterable<D> translateEntities(Iterable<E> entities) {
+		ArrayList<D> douments = new ArrayList<D>();
+		for (E entity : entities) {
+			douments.add(translate(entity, 1));
+		}
+		return douments;
+	}
 
-    protected abstract D entityToDocument(E entity, int translationDepth);
+	protected abstract D entityToDocument(E entity, int translationDepth);
 
 }
