8affa9652694ac33a308d248afcec94f0425ed0f
Olin Blodgett <nilo@nilosplace.net>
2023-02-15 21:32:46 -0500

    First attempt at code

--- agr_indexer/src/main/java/org/alliancegenome/indexer/Main.java
+++ agr_indexer/src/main/java/org/alliancegenome/indexer/Main.java
@@ -1,8 +1,6 @@
 package org.alliancegenome.indexer;
 
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
 
 import org.alliancegenome.core.config.ConfigHelper;
 import org.alliancegenome.es.util.IndexManager;
@@ -25,14 +23,14 @@ public class Main {
 
 		IndexManager im = new IndexManager();
 
-		ESDocumentProcessor.indexName = im.startSiteIndex();
+		ESDocumentProcessor processor = new ESDocumentProcessor(im.startSiteIndex());
 
-		Thread.setDefaultUncaughtExceptionHandler((t, e) -> {
-			log.error("Thread: " + t.getId() + " has uncaught exceptions");
-			e.printStackTrace();
-			System.exit(-1);
-		});
-
+//		Thread.setDefaultUncaughtExceptionHandler((t, e) -> {
+//			log.error("Thread: " + t.getId() + " has uncaught exceptions");
+//			e.printStackTrace();
+//			System.exit(-1);
+//		});
+//
 		HashMap<String, Indexer> indexers = new HashMap<>();
 		for (IndexerConfig ic : IndexerConfig.values()) {
 			try {
@@ -44,42 +42,42 @@ public class Main {
 				System.exit(-1);
 			}
 		}
-
-		Set<String> argumentSet = new HashSet<>();
-		for (int i = 0; i < args.length; i++) {
-			argumentSet.add(args[i]);
-			log.info("Args[" + i + "]: " + args[i]);
-		}
-
-		for (String type : indexers.keySet()) {
-			if (argumentSet.size() == 0 || argumentSet.contains(type)) {
-				if (ConfigHelper.isThreaded()) {
-					log.info("Starting in threaded mode for: " + type);
-					indexers.get(type).start();
-				} else {
-					log.info("Starting indexer sequentially: " + type);
-					indexers.get(type).runIndex();
-				}
-			} else {
-				log.info("Not Starting: " + type);
-			}
-		}
-
-		log.info("Waiting for Indexers to finish");
-		for (Indexer i: indexers.values()) {
-			try {
-				if (i.isAlive()) {
-					i.join();
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-				log.error(e.getMessage());
-				System.exit(-1);
-			}
-		}
+//
+//		Set<String> argumentSet = new HashSet<>();
+//		for (int i = 0; i < args.length; i++) {
+//			argumentSet.add(args[i]);
+//			log.info("Args[" + i + "]: " + args[i]);
+//		}
+//
+//		for (String type : indexers.keySet()) {
+//			if (argumentSet.size() == 0 || argumentSet.contains(type)) {
+//				if (ConfigHelper.isThreaded()) {
+//					log.info("Starting in threaded mode for: " + type);
+//					indexers.get(type).start();
+//				} else {
+//					log.info("Starting indexer sequentially: " + type);
+//					indexers.get(type).runIndex();
+//				}
+//			} else {
+//				log.info("Not Starting: " + type);
+//			}
+//		}
+//
+//		log.info("Waiting for Indexers to finish");
+//		for (Indexer i: indexers.values()) {
+//			try {
+//				if (i.isAlive()) {
+//					i.join();
+//				}
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//				log.error(e.getMessage());
+//				System.exit(-1);
+//			}
+//		}
 		
-		log.info("Need to read all the files and load them into ES");
-		
+		processor.processIndexes(indexers);
+		processor.close();
 
 		im.finishIndex();
 
--- agr_indexer/src/main/java/org/alliancegenome/indexer/processors/ESDocumentProcessor.java
+++ agr_indexer/src/main/java/org/alliancegenome/indexer/processors/ESDocumentProcessor.java
@@ -1,7 +1,19 @@
 package org.alliancegenome.indexer.processors;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+import org.alliancegenome.es.util.EsClientFactory;
 import org.alliancegenome.indexer.config.IndexerConfig;
+import org.alliancegenome.indexer.indexers.Indexer;
+import org.elasticsearch.action.DocWriteRequest;
 import org.elasticsearch.action.bulk.BulkProcessor;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.RestHighLevelClient;
 
 import lombok.extern.slf4j.Slf4j;
@@ -9,37 +21,40 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ESDocumentProcessor {
 
-	public static String indexName;
-	protected IndexerConfig indexerConfig;
+	private String indexName;
+	private IndexerConfig indexerConfig;
 	private RestHighLevelClient searchClient;
-	protected BulkProcessor bulkProcessor;
-	
+	private BulkProcessor bulkProcessor;
 
-	public ESDocumentProcessor() {
+	public ESDocumentProcessor(String indexName) {
+		this.indexName = indexName;
+	}
 
-//		searchClient = EsClientFactory.getDefaultEsClient();
-//
-//		BulkProcessor.Listener listener = new BulkProcessor.Listener() {
-//			@Override
-//			public void beforeBulk(long executionId, BulkRequest request) {
-//			}
-//
-//			@Override
-//			public void afterBulk(long executionId, BulkRequest request, BulkResponse response) {
-//				//log.info("Size: " + request.requests().size() + " MB: " + request.estimatedSizeInBytes() + " Time: " + response.getTook() + " Bulk Requet Finished");
-//			}
-//
-//			@Override
-//			public void afterBulk(long executionId, BulkRequest request, Throwable failure) {
-//				log.error("Bulk Request Failure: " + failure.getMessage());
-//				for(DocWriteRequest<?> req: request.requests()) {
-//					IndexRequest idxreq = (IndexRequest)req;
-//					bulkProcessor.add(idxreq);
-//				}
-//				log.error("Finished Adding failed requests to bulkProcessor: ");
-//			}
-//		};
-//
+	public void readFiles() {
+
+		searchClient = EsClientFactory.getMustCloseSearchClient();
+
+		BulkProcessor.Listener listener = new BulkProcessor.Listener() {
+			@Override
+			public void beforeBulk(long executionId, BulkRequest request) {
+			}
+
+			@Override
+			public void afterBulk(long executionId, BulkRequest request, BulkResponse response) {
+				//log.info("Size: " + request.requests().size() + " MB: " + request.estimatedSizeInBytes() + " Time: " + response.getTook() + " Bulk Requet Finished");
+			}
+
+			@Override
+			public void afterBulk(long executionId, BulkRequest request, Throwable failure) {
+				log.error("Bulk Request Failure: " + failure.getMessage());
+				for(DocWriteRequest<?> req: request.requests()) {
+					IndexRequest idxreq = (IndexRequest)req;
+					bulkProcessor.add(idxreq);
+				}
+				log.error("Finished Adding failed requests to bulkProcessor: ");
+			}
+		};
+
 //		BulkProcessor.Builder builder = BulkProcessor.builder((request, bulkListener) -> searchClient.bulkAsync(request, RequestOptions.DEFAULT, bulkListener), listener, getClass().getSimpleName());
 //		builder.setBulkActions(indexerConfig.getBulkActions());
 //		builder.setBulkSize(new ByteSizeValue(indexerConfig.getBulkSize(), ByteSizeUnit.MB));
@@ -47,5 +62,30 @@ public class ESDocumentProcessor {
 //		builder.setBackoffPolicy(BackoffPolicy.exponentialBackoff(TimeValue.timeValueSeconds(10L), 60));
 //
 //		bulkProcessor = builder.build();
+		
+		
+		
+	}
+
+
+	public void processIndexes(HashMap<String, Indexer> indexers) {
+		
+		for(Entry<String, Indexer> entry: indexers.entrySet()) {
+			try {
+				BufferedReader reader = new BufferedReader(new FileReader(new File("/data/" + getClass().getSimpleName() + "_data.json")));
+				reader.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			
+			log.info("Indexer: " + entry.getValue().getClass().getSimpleName());
+		}
+		
+		
+	}
+
+	public void close() {
+		searchClient.close();
 	}
 }
