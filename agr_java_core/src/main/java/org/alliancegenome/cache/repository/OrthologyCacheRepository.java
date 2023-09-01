6cbc9d57c27626292a7195aab0fce0ee3d255d20
Christian Pich <cmpich@zfin.org>
2023-09-01 15:58:41 +0200

    SCRUM-3221 replace old fields on Paralogus object with length, identity, similarity and rank (#1096)
    * SCRUM-3221 replace old fields on Paralogus object with length, identity, similarity and rank
    
    * SCRUM-3221 rename ParalogView to ParalogBean and use @Data
    
    * SCRUM-3221 adjust @Schema description of ParalogBean

--- agr_java_core/src/main/java/org/alliancegenome/cache/repository/OrthologyCacheRepository.java
+++ agr_java_core/src/main/java/org/alliancegenome/cache/repository/OrthologyCacheRepository.java
@@ -21,10 +21,7 @@ import org.alliancegenome.neo4j.entity.node.Gene;
 import org.alliancegenome.neo4j.entity.node.OrthoAlgorithm;
 import org.alliancegenome.neo4j.entity.node.OrthologyGeneJoin;
 import org.alliancegenome.neo4j.entity.relationship.Orthologous;
-import org.alliancegenome.neo4j.view.HomologView;
-import org.alliancegenome.neo4j.view.OrthologyFilter;
-import org.alliancegenome.neo4j.view.OrthologyModule;
-import org.alliancegenome.neo4j.view.View;
+import org.alliancegenome.neo4j.view.*;
 
 import com.fasterxml.jackson.annotation.JsonView;
 import com.fasterxml.jackson.databind.MapperFeature;
@@ -214,14 +211,10 @@ public class OrthologyCacheRepository {
 		return response;
 	}
 
-	public JsonResultResponse<HomologView> getParalogyMultiGeneJson(List<String> geneIDs, Pagination pagination) {
+	public JsonResultResponse<ParalogBean> getParalogyMultiGeneJson(List<String> geneIDs, Pagination pagination) {
 		long start = System.currentTimeMillis();
-		List<HomologView> homologViewList = repo.getAllParalogyGenes(geneIDs);
-		//filtering
-		FilterService<HomologView> filterService = new FilterService<>(new OrthologyFiltering());
-		List<HomologView> homologViewFiltered = filterService.filterAnnotations(homologViewList, pagination.getFieldFilterValueMap());
-
-		List<HomologView> paginatedViewFiltered = homologViewFiltered.stream()
+		List<ParalogBean> homologViewList = repo.getAllParalogyGenes(geneIDs);
+		List<ParalogBean> paginatedViewFiltered = homologViewList.stream()
 				.skip(pagination.getStart())
 				.limit(pagination.getLimit()).sorted(Comparator.comparing(o -> o.getHomologGene().getSpecies().getPhylogeneticOrder()))
 				.collect(Collectors.toList());
@@ -235,9 +228,9 @@ public class OrthologyCacheRepository {
 			putGeneInfo(map, orthologView.getGene());
 			putGeneInfo(map, orthologView.getHomologGene());
 		});
-		JsonResultResponse<HomologView> response = new JsonResultResponse<>();
+		JsonResultResponse<ParalogBean> response = new JsonResultResponse<>();
 		response.setResults(paginatedViewFiltered);
-		response.setTotal(homologViewFiltered.size());
+		response.setTotal(homologViewList.size());
 		response.setSupplementalData(map);
 		response.calculateRequestDuration(start);
 		return response;
---
+++ agr_java_core/src/main/java/org/alliancegenome/neo4j/view/ParalogBean.java
@@ -0,0 +1,67 @@
+package org.alliancegenome.neo4j.view;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.JsonView;
+import lombok.Data;
+import org.alliancegenome.neo4j.entity.node.Gene;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@Schema(name="OrthologView", description="POJO that represents the Paralog view")
+@JsonPropertyOrder({"gene", "homologGene", "length", "similarity", "identity", "rank", "predictionMethodsMatched", "predictionMethodsNotMatched", "predictionMethodsNotCalled", "methodCount", "totalMethodCount"})
+public class ParalogBean implements Serializable {
+
+	@JsonView(View.Homology.class)
+	private Gene gene;
+	@JsonView(View.Homology.class)
+	private Gene homologGene;
+
+	@JsonView(View.Homology.class)
+	private Float length;
+	@JsonView(View.Homology.class)
+	private Float similarity;
+	@JsonView(View.Homology.class)
+	private Integer rank;
+	@JsonView(View.Homology.class)
+	private Float identity;
+
+	@JsonView(View.Homology.class)
+	private List<String> predictionMethodsNotCalled;
+	@JsonView(View.Homology.class)
+	private List<String> predictionMethodsMatched;
+	@JsonView(View.Homology.class)
+	private List<String> predictionMethodsNotMatched;
+
+	@JsonView(View.Homology.class)
+	@JsonProperty(value = "methodCount")
+	public Integer getMethodCount() {
+		if (predictionMethodsMatched == null)
+			return 0;
+		return predictionMethodsMatched.size();
+	}
+
+	@JsonProperty(value = "methodCount")
+	public void setMethodCount(Integer count) {
+	}
+
+	@JsonView(View.Homology.class)
+	@JsonProperty(value = "totalMethodCount")
+	public Integer getTotalMethodCount() {
+		if (predictionMethodsMatched == null && predictionMethodsNotMatched == null)
+			return 0;
+		if (predictionMethodsMatched == null)
+			return predictionMethodsNotMatched.size();
+		if (predictionMethodsNotMatched == null)
+			return predictionMethodsMatched.size();
+		return predictionMethodsMatched.size() + predictionMethodsNotMatched.size();
+	}
+
+	@JsonProperty(value = "totalMethodCount")
+	public void setTotalMethodCount(Integer count) {
+	}
+
+}
