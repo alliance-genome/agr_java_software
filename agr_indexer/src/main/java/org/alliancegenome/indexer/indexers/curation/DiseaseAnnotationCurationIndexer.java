088fbc67feddaae0f7d75912c87c00052ea22558
Olin Blodgett <nilo@nilosplace.net>
2023-11-30 13:56:14 -0500

    Better style

--- agr_indexer/src/main/java/org/alliancegenome/indexer/indexers/curation/DiseaseAnnotationCurationIndexer.java
+++ agr_indexer/src/main/java/org/alliancegenome/indexer/indexers/curation/DiseaseAnnotationCurationIndexer.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.Set;
-import java.util.StringJoiner;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.stream.Collectors;
 
@@ -111,16 +110,15 @@ public class DiseaseAnnotationCurationIndexer extends Indexer {
 				}
 
 				String key = relation.getName() + "_" + da.getObject().getName() + "_" + da.getNegated();
+				
 				if(da.getDiseaseQualifiers() != null) {
-					StringJoiner joiner = new StringJoiner("_");
-					da.getDiseaseQualifiers().stream().map(VocabularyTerm::getName).sorted().collect(Collectors.toList()).forEach(t -> { joiner.add(t); });
-					key += "_" + joiner.toString();
+					key += "_" + da.getDiseaseQualifiers().stream().map(VocabularyTerm::getName).sorted().collect(Collectors.toList());
 				}
 				
 				if (da.getWith() != null && da.getWith().size() > 0) {
-					List<String> withIds = da.getWith().stream().map(Gene::getCurie).sorted().collect(Collectors.toList());
-					key += "_" + String.join("_", withIds);
+					key += "_" + String.join("_", da.getWith().stream().map(Gene::getCurie).sorted().collect(Collectors.toList()));
 				}
+				
 				GeneDiseaseAnnotationDocument gdad = lookup.get(key);
 
 				if (gdad == null) {
