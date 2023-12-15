64b6e567d5479c2392b4083464c00d2e2a321afa
Olin Blodgett <nilo@nilosplace.net>
2023-12-15 12:19:25 -0500

    Updated data extractor

--- agr_intermine_data_extractor/src/main/java/org/alliancegenome/data_extractor/extractors/DiseaseExtractor.java
+++ agr_intermine_data_extractor/src/main/java/org/alliancegenome/data_extractor/extractors/DiseaseExtractor.java
@@ -9,32 +9,32 @@ import org.alliancegenome.neo4j.repository.DiseaseRepository;
 
 public class DiseaseExtractor extends DataExtractor {
 
-    DiseaseRepository repo = new DiseaseRepository();
-    
-    @Override
-    protected void extract(PrintWriter writer) {
-        
-        Set<String> ids = repo.getAllDiseaseWithAnnotationsKeys();
-        DiseaseTSVTranslator translator = new DiseaseTSVTranslator(writer);
-        
-        startProcess("Starting Diseases: ", ids.size());
-        for(String id: ids) {
-            DOTerm doTerm = repo.getDiseaseTerm(id);
-            translator.translateEntity(doTerm);
-            progressProcess();
-        }
-        finishProcess();
-    }
+	DiseaseRepository repo = new DiseaseRepository();
+	
+	@Override
+	protected void extract(PrintWriter writer) {
+		
+		Set<String> ids = repo.getAllDiseaseWithAnnotationsKeys();
+		DiseaseTSVTranslator translator = new DiseaseTSVTranslator(writer);
+		
+		startProcess("Starting Diseases: ", ids.size());
+		for(String id: ids) {
+			DOTerm doTerm = repo.getOneTerm(id);
+			translator.translateEntity(doTerm);
+			progressProcess();
+		}
+		finishProcess();
+	}
 
-    @Override
-    protected String getFileName() {
-        return "Diseases.tsv";
-    }
-    
-    @Override
-    protected String getDirName() {
-        return "diseases";
-    }
+	@Override
+	protected String getFileName() {
+		return "Diseases.tsv";
+	}
+	
+	@Override
+	protected String getDirName() {
+		return "diseases";
+	}
 
 }
 
