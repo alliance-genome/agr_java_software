fadb0db9eaaef0dcff2d5e2d6851565c1134b909
Christian Pich <cmpich@zfin.org>
2024-03-29 16:37:52 +0100

    SCRUM-3847 allow to filter by species through taxon.name.keyword field in ES (#1176)

--- agr_api/src/main/java/org/alliancegenome/api/controller/DiseaseController.java
+++ agr_api/src/main/java/org/alliancegenome/api/controller/DiseaseController.java
@@ -218,7 +218,7 @@ public class DiseaseController implements DiseaseRESTInterface {
 		long startTime = System.currentTimeMillis();
 		Pagination pagination = new Pagination(page, limit, sortBy, asc);
 		pagination.addFilterOption("subject.geneSymbol.displayText", geneName);
-		pagination.addFilterOption("subject.taxon.name", species);
+		pagination.addFilterOption("subject.taxon.name.keyword", species);
 		pagination.addFilterOption("evidenceCodes.abbreviation", evidenceCode);
 		pagination.addFilterOption("generatedRelationString.keyword", associationType);
 		pagination.addFilterOption("diseaseQualifiers.keyword", diseaseQualifier);
