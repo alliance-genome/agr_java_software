d2308c4a5aaf7ee24abf72366ee5c610fcbac095
Christian Pich <cmpich@zfin.org>
2024-04-12 15:10:57 +0200

    do the sorting on the keyword fields in the API

--- agr_api/src/main/java/org/alliancegenome/api/service/DiseaseESService.java
+++ agr_api/src/main/java/org/alliancegenome/api/service/DiseaseESService.java
@@ -455,8 +455,8 @@ public class DiseaseESService {
 		// Sorting sets for different names of the sorting selection box
 		Map<String, List<String>> sortingSetMap = new HashMap<>();
 		sortingSetMap.put("default", List.of("phylogeneticSortingIndex", "subject.alleleSymbol.displayText"));
-		sortingSetMap.put("allele", List.of("subject.alleleSymbol.displayText", "phylogeneticSortingIndex"));
-		sortingSetMap.put("disease", List.of("object.name", "phylogeneticSortingIndex", "subject.alleleSymbol.displayText.keyword"));
+		sortingSetMap.put("allele", List.of("subject.alleleSymbol.displayText.keyword", "phylogeneticSortingIndex"));
+		sortingSetMap.put("disease", List.of("object.name.keyword", "phylogeneticSortingIndex", "subject.alleleSymbol.displayText.keyword"));
 		sortingSetMap.put("species", List.of("subject.taxon.name.keyword", "subject.alleleSymbol.displayText.keyword"));
 
 		LinkedHashMap<String, SortOrder> sortingMap = new LinkedHashMap<>();
