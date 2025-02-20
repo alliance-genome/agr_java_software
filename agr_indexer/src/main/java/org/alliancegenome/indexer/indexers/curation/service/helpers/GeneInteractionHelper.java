dc7925b4091a639195cfbf62ed866bec31b48c63
Mark Quinton-Tulloch <markquintontulloch@gmail.com>
2025-02-20 11:27:17 +0000

    Use mapper from RestConfig

--- agr_indexer/src/main/java/org/alliancegenome/indexer/indexers/curation/service/helpers/GeneInteractionHelper.java
+++ agr_indexer/src/main/java/org/alliancegenome/indexer/indexers/curation/service/helpers/GeneInteractionHelper.java
@@ -6,20 +6,23 @@ import java.util.Objects;
 import org.alliancegenome.curation_api.model.entities.GeneGeneticInteraction;
 import org.alliancegenome.curation_api.model.entities.GeneInteraction;
 import org.alliancegenome.curation_api.model.entities.GeneMolecularInteraction;
+import org.alliancegenome.indexer.RestConfig;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class GeneInteractionHelper {
 
+	private ObjectMapper mapper = RestConfig.config.getJacksonObjectMapperFactory().createObjectMapper();
+	
 	public GeneGeneticInteraction generateReverseInteraction(GeneGeneticInteraction forwardInteraction) throws IOException {
-		
-		GeneGeneticInteraction newInteraction = new GeneGeneticInteraction();
+		GeneGeneticInteraction newInteraction = mapper.readValue(mapper.writeValueAsString(forwardInteraction), GeneGeneticInteraction.class);
 		newInteraction.setInteractorAGeneticPerturbation(forwardInteraction.getInteractorBGeneticPerturbation());
 		newInteraction.setInteractorBGeneticPerturbation(forwardInteraction.getInteractorAGeneticPerturbation());
 		return reverseInteraction(forwardInteraction, newInteraction);
 	}
 	
-	public GeneMolecularInteraction generateReverseInteraction(GeneMolecularInteraction forwardInteraction) {
-		
-		GeneMolecularInteraction newInteraction = new GeneMolecularInteraction();
+	public GeneMolecularInteraction generateReverseInteraction(GeneMolecularInteraction forwardInteraction) throws IOException {
+		GeneMolecularInteraction newInteraction = mapper.readValue(mapper.writeValueAsString(forwardInteraction), GeneMolecularInteraction.class);
 		return reverseInteraction(forwardInteraction, newInteraction);
 	}
 	
@@ -37,19 +40,6 @@ public class GeneInteractionHelper {
 		
 		reverseInteraction.setInteractorAType(forwardInteraction.getInteractorBType());
 		reverseInteraction.setInteractorBType(forwardInteraction.getInteractorAType());
-		
-		reverseInteraction.setInteractionId(forwardInteraction.getInteractionId());
-		reverseInteraction.setInteractionSource(forwardInteraction.getInteractionSource());
-		reverseInteraction.setInteractionType(forwardInteraction.getInteractionType());
-		reverseInteraction.setCrossReferences(forwardInteraction.getCrossReferences());
-
-		reverseInteraction.setRelation(forwardInteraction.getRelation());
-		
-		reverseInteraction.setEvidence(forwardInteraction.getEvidence());
-
-		reverseInteraction.setDateCreated(forwardInteraction.getDateCreated());
-		reverseInteraction.setInternal(forwardInteraction.getInternal());
-		reverseInteraction.setObsolete(forwardInteraction.getObsolete());
 		
 		return reverseInteraction;
 	}
