96cd884812072f027394860f81d0fc0f9a7570a4
Olin Blodgett <nilo@nilosplace.net>
2025-03-03 07:32:18 -0500

    Removed tabs spaces issues

--- agr_indexer/src/main/java/org/alliancegenome/indexer/config/IndexerConfig.java
+++ agr_indexer/src/main/java/org/alliancegenome/indexer/config/IndexerConfig.java
@@ -1,12 +1,17 @@
 package org.alliancegenome.indexer.config;
 
-import org.alliancegenome.indexer.indexers.*;
+import org.alliancegenome.indexer.indexers.AlleleIndexer;
+import org.alliancegenome.indexer.indexers.DatasetIndexer;
+import org.alliancegenome.indexer.indexers.DiseaseIndexer;
+import org.alliancegenome.indexer.indexers.GeneIndexer;
+import org.alliancegenome.indexer.indexers.GoIndexer;
+import org.alliancegenome.indexer.indexers.ModelIndexer;
 import org.alliancegenome.indexer.indexers.curation.DiseaseAnnotationCurationIndexer;
 import org.alliancegenome.indexer.indexers.curation.GeneExpressionAnnotationIndexer;
 import org.alliancegenome.indexer.indexers.curation.GeneGeneticInteractionCurationIndexer;
 import org.alliancegenome.indexer.indexers.curation.GeneMolecularInteractionCurationIndexer;
-import org.alliancegenome.indexer.indexers.curation.PhenotypeAnnotationCurationIndexer;
 import org.alliancegenome.indexer.indexers.curation.GeneToGeneParalogyIndexer;
+import org.alliancegenome.indexer.indexers.curation.PhenotypeAnnotationCurationIndexer;
 
 public enum IndexerConfig {
 
@@ -16,19 +21,20 @@ public enum IndexerConfig {
 	// Bulk Size = 1MB
 	// ConCurrent Reqests = Threads * 2
 	// Document Stats as of 4/16/2021 ---- (doc count, min size, max size, average size)
-	GeneIndexer("gene", GeneIndexer.class, 4, 359, 359, 8, 1),              // (280954, 616, 156692, 2785)
-	DatasetIndexer("dataset", DatasetIndexer.class, 4, 566, 566, 8, 1), // (8094,	358,  90542, 1765)
-	DiseaseIndexer("disease", DiseaseIndexer.class, 4, 680, 680, 8, 1), // (10751,	191, 330099, 1472)
-	AlleleIndexer("allele", AlleleIndexer.class, 4, 1517, 1517, 8, 1),      // (407830, 334,	69968,	659)
-	GoIndexer("go", GoIndexer.class, 4, 914, 914, 8, 1),                // (44087,	262, 460247, 1093)
-	ModelIndexer("model", ModelIndexer.class, 4, 1426, 1426, 8, 1),           // (132447, 314,	 18593,	 701)
-	DiseaseAnnotationMlIndexer("diseaseAnnotation", DiseaseAnnotationCurationIndexer.class, 1, 1426, 1426, 2, 1),
+	
+	GeneIndexer("gene", GeneIndexer.class, 4, 359, 359, 8, 1),
+	DatasetIndexer("dataset", DatasetIndexer.class, 4, 566, 566, 8, 1),
+	DiseaseIndexer("disease", DiseaseIndexer.class, 4, 680, 680, 8, 1),
+	AlleleIndexer("allele", AlleleIndexer.class, 4, 1517, 1517, 8, 1),
+	GoIndexer("go", GoIndexer.class, 4, 914, 914, 8, 1),
+	ModelIndexer("model", ModelIndexer.class, 4, 1426, 1426, 8, 1),
+	DiseaseAnnotationIndexer("diseaseAnnotation", DiseaseAnnotationCurationIndexer.class, 1, 1500, 1500, 2, 1),
 	GeneExpressionAnnotationIndexer("geneExpressionAnnotation", GeneExpressionAnnotationIndexer.class, 4, 1500, 1500, 2, 1),
 	GeneGeneticInteractionIndexers("geneGeneticInteraction", GeneGeneticInteractionCurationIndexer.class, 4, 1500, 1500, 2, 1),
 	GeneMolecularInteractionIndexers("geneMolecularInteraction", GeneMolecularInteractionCurationIndexer.class, 4, 1500, 1500, 2, 1),
 	//PhenotypeAnnotationMlIndexer("phenotypeAnnotation", PhenotypeCurationIndexer.class, 1, 1426, 1426, 2, 1),
-	ParalogyIndexer("paralogy", GeneToGeneParalogyIndexer.class, 4, 5000, 5000, 8, 1),   // (3918629, 735, 1514, 1065)
-	PhenotypeAnnotationMlIndexer("phenotypeAnnotation", PhenotypeAnnotationCurationIndexer.class, 1, 1426, 1426, 2, 1),
+	ParalogyIndexer("paralogy", GeneToGeneParalogyIndexer.class, 4, 5000, 5000, 8, 1),
+	PhenotypeAnnotationIndexer("phenotypeAnnotation", PhenotypeAnnotationCurationIndexer.class, 1, 1500, 1500, 2, 1),
 	// still not implemented  VariantIndexer("variant", VariantIndexer.class, 2, 3000, 400, 4, 4)
 	;
 
