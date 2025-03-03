c9808e3decc036e491ae437816a7d6b2f628b265
Olin Blodgett <nilo@nilosplace.net>
2025-03-03 07:34:04 -0500

    Cleaned up indexer config

--- agr_indexer/src/main/java/org/alliancegenome/indexer/config/IndexerConfig.java
+++ agr_indexer/src/main/java/org/alliancegenome/indexer/config/IndexerConfig.java
@@ -14,28 +14,22 @@ import org.alliancegenome.indexer.indexers.curation.GeneToGeneParalogyIndexer;
 import org.alliancegenome.indexer.indexers.curation.PhenotypeAnnotationCurationIndexer;
 
 public enum IndexerConfig {
-
-	// These numbers target a 1MB document batch size
-	// Buffer size = 1MB / average size
-	// Bulk Action Size = Buffer Size
-	// Bulk Size = 1MB
-	// ConCurrent Reqests = Threads * 2
-	// Document Stats as of 4/16/2021 ---- (doc count, min size, max size, average size)
 	
+	// Neo Indexers
 	GeneIndexer("gene", GeneIndexer.class, 4, 359, 359, 8, 1),
 	DatasetIndexer("dataset", DatasetIndexer.class, 4, 566, 566, 8, 1),
 	DiseaseIndexer("disease", DiseaseIndexer.class, 4, 680, 680, 8, 1),
 	AlleleIndexer("allele", AlleleIndexer.class, 4, 1517, 1517, 8, 1),
 	GoIndexer("go", GoIndexer.class, 4, 914, 914, 8, 1),
 	ModelIndexer("model", ModelIndexer.class, 4, 1426, 1426, 8, 1),
+	
+	// Curation Indexers
 	DiseaseAnnotationIndexer("diseaseAnnotation", DiseaseAnnotationCurationIndexer.class, 1, 1500, 1500, 2, 1),
 	GeneExpressionAnnotationIndexer("geneExpressionAnnotation", GeneExpressionAnnotationIndexer.class, 4, 1500, 1500, 2, 1),
 	GeneGeneticInteractionIndexers("geneGeneticInteraction", GeneGeneticInteractionCurationIndexer.class, 4, 1500, 1500, 2, 1),
 	GeneMolecularInteractionIndexers("geneMolecularInteraction", GeneMolecularInteractionCurationIndexer.class, 4, 1500, 1500, 2, 1),
-	//PhenotypeAnnotationMlIndexer("phenotypeAnnotation", PhenotypeCurationIndexer.class, 1, 1426, 1426, 2, 1),
 	ParalogyIndexer("paralogy", GeneToGeneParalogyIndexer.class, 4, 5000, 5000, 8, 1),
 	PhenotypeAnnotationIndexer("phenotypeAnnotation", PhenotypeAnnotationCurationIndexer.class, 1, 1500, 1500, 2, 1),
-	// still not implemented  VariantIndexer("variant", VariantIndexer.class, 2, 3000, 400, 4, 4)
 	;
 
 
