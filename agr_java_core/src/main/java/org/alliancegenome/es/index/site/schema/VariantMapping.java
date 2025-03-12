0691db5358314efea8679500f1f8e3d240aa7ea7
Olin Blodgett <nilo@nilosplace.net>
2025-03-12 12:20:19 -0400

    Updated field to be keywork so that it can be agged on

--- agr_java_core/src/main/java/org/alliancegenome/es/index/site/schema/VariantMapping.java
+++ agr_java_core/src/main/java/org/alliancegenome/es/index/site/schema/VariantMapping.java
@@ -15,7 +15,7 @@ public class VariantMapping extends Mapping {
 			new FieldBuilder(builder, "category", "keyword").symbol().autocomplete().keyword().build();
 			new FieldBuilder(builder, "name", "keyword").build();
 			new FieldBuilder(builder, "name_key", "keyword").build();
-			new FieldBuilder(builder, "alterationType", "text").keyword().build();
+			new FieldBuilder(builder, "alterationType", "keyword").build();
 			new FieldBuilder(builder, "genes", "text").keyword().build();
 			
 			builder.startObject("allele");
