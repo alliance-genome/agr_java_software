862f590123c27da785d8be63d0c5a52497f1cc9b
Olin Blodgett <nilo@nilosplace.net>
2025-03-26 14:55:45 -0400

    Refactor indexer

--- agr_indexer/src/main/java/org/alliancegenome/indexer/indexers/curation/GeneToGeneOrthologyIndexer.java
+++ agr_indexer/src/main/java/org/alliancegenome/indexer/indexers/curation/GeneToGeneOrthologyIndexer.java
@@ -1,9 +1,13 @@
 package org.alliancegenome.indexer.indexers.curation;
 
+import java.util.HashMap;
 import java.util.concurrent.LinkedBlockingDeque;
 
 import org.alliancegenome.api.entity.GeneToGeneOrthologyDocument;
+import org.alliancegenome.core.config.ConfigHelper;
 import org.alliancegenome.curation_api.response.SearchResponse;
+import org.alliancegenome.curation_api.interfaces.document.GeneToGeneOrthologyDocumentInterface;
+import org.alliancegenome.curation_api.model.document.es.GeneSearchResultDocument;
 import org.alliancegenome.indexer.RestConfig;
 import org.alliancegenome.indexer.config.IndexerConfig;
 import org.alliancegenome.indexer.indexers.Indexer;
@@ -13,34 +17,34 @@ import org.apache.commons.collections4.CollectionUtils;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.extern.slf4j.Slf4j;
+import si.mazi.rescu.RestProxyFactory;
 
 @Slf4j
 public class GeneToGeneOrthologyIndexer extends Indexer {
 
-	private GeneToGeneOrthologyService service = new GeneToGeneOrthologyService();
+	private final GeneToGeneOrthologyDocumentInterface orthologyApi = RestProxyFactory.createProxy(GeneToGeneOrthologyDocumentInterface.class, ConfigHelper.getCurationApiUrl(), RestConfig.config);
 
 	public GeneToGeneOrthologyIndexer(IndexerConfig config) {
 		super(config);
 	}
-
+	
+	
 	@Override
 	public void index() {
+		HashMap<String, Object> params = new HashMap<>();
+		params.put("internal", false);
+		params.put("obsolete", false);
+		
 		try {
-			log.info("Getting orthologs");
-
-			SearchResponse<GeneToGeneOrthologyDocument> orthologyResponse = service.getGeneToGeneOrthology(0, 0);
-
-			log.info("GeneToGeneOrthology count: " + orthologyResponse.getTotalResults());
-
-			int totalPages = (int) (orthologyResponse.getTotalResults() / indexerConfig.getBufferSize());
-
+			SearchResponse<GeneSearchResultDocument> resp = orthologyApi.orthologyApi(0, 0, params);
+			log.info("GeneToGeneOrthology count: " + resp.getTotalResults());
+			int totalPages = (int) (resp.getTotalResults() / indexerConfig.getBufferSize());
+			
 			LinkedBlockingDeque<String> queue = new LinkedBlockingDeque<>();
 			for (int i = 0; i <= totalPages; i++) {
 				queue.add(String.valueOf(i));
 			}
-
 			initiateThreading(queue);
-
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -50,21 +54,31 @@ public class GeneToGeneOrthologyIndexer extends Indexer {
 	
 	@Override
 	protected void startSingleThread(LinkedBlockingDeque<String> queue) {
+		HashMap<String, Object> params = new HashMap<>();
+		params.put("internal", false);
+		params.put("obsolete", false);
+		// params.put("primaryExternalId", "Xenbase:XB-GENE-17345583"); //
+		// params.put("primaryExternalId", "RGD:621017");
+		// params.put("primaryExternalId", "WB:WBGene00003883"); // alleles
+		// params.put("primaryExternalId", "ZFIN:ZDB-GENE-110114-3");
+
 		while (true) {
 			try {
 				if (queue.isEmpty()) {
 					return;
 				}
+
 				String page = queue.takeFirst();
+				// log.info(queue.size() + " pages to process " +
+				// Thread.currentThread().getName() + " starting page: " + page);
 
-				SearchResponse<GeneToGeneOrthologyDocument> resp = service.getGeneToGeneOrthology(Integer.valueOf(page), indexerConfig.getBufferSize());
-
-				if (resp == null || CollectionUtils.isEmpty(resp.getResults())) {
+				SearchResponse<GeneSearchResultDocument> response = orthologyApi.findSearchResult(Integer.valueOf(page), indexerConfig.getBufferSize(), params);
+				// log.info("Search Response: " + response);
+				if (response == null || CollectionUtils.isEmpty(response.getResults())) {
 					return;
 				}
 
-				indexDocuments(resp.getResults());
-				
+				indexDocuments(response.getResults());
 			} catch (Exception e) {
 				log.error("Error while indexing...", e);
 				System.exit(-1);
