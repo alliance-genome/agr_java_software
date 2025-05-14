c444e22658b7f2633eeaabbfdf33c95cc5852019
Christian Pich <cmpich@zfin.org>
2025-05-14 14:52:20 +0200

    SCRUM-4817 create Affected Genomic Model Indexer for model table on gene page. (#1321)
    * model indexer
    
    * SCRUM-4817 index model entities: pull out of the API and put into ES
    
    * in the middle of writing the indexer
    
    * adjust indexer to use paginated endpoint for affected genomic models
    
    * refactoring to remove ModelService class
    
    * more refactoring
    
    * more refactoring
    
    * refeactor
    
    * remove unused imports
    
    * remove unused code
    
    * remove unneeded paren, fix AGM name setting in genetic modifier population
    
    * undo AGM NameSlot annotation

--- agr_indexer/src/main/java/org/alliancegenome/indexer/config/IndexerConfig.java
+++ agr_indexer/src/main/java/org/alliancegenome/indexer/config/IndexerConfig.java
@@ -1,31 +1,18 @@
 package org.alliancegenome.indexer.config;
 
-import org.alliancegenome.indexer.indexers.AlleleIndexer;
-import org.alliancegenome.indexer.indexers.DatasetIndexer;
-import org.alliancegenome.indexer.indexers.DiseaseIndexer;
-import org.alliancegenome.indexer.indexers.GeneIndexer;
-import org.alliancegenome.indexer.indexers.GoIndexer;
-import org.alliancegenome.indexer.indexers.ModelIndexer;
-import org.alliancegenome.indexer.indexers.curation.DiseaseAnnotationCurationIndexer;
-import org.alliancegenome.indexer.indexers.curation.DiseaseSummaryCurationIndexer;
-import org.alliancegenome.indexer.indexers.curation.GOSearchResultCurationIndexer;
-import org.alliancegenome.indexer.indexers.curation.GeneGeneticInteractionCurationIndexer;
-import org.alliancegenome.indexer.indexers.curation.GeneMolecularInteractionCurationIndexer;
-import org.alliancegenome.indexer.indexers.curation.GeneToGeneOrthologyIndexer;
-import org.alliancegenome.indexer.indexers.curation.GeneToGeneParalogyIndexer;
-import org.alliancegenome.indexer.indexers.curation.PhenotypeAnnotationCurationIndexer;
-import org.alliancegenome.indexer.indexers.curation.ReleaseInfoIndexer;
+import org.alliancegenome.indexer.indexers.*;
+import org.alliancegenome.indexer.indexers.curation.*;
 
 public enum IndexerConfig {
-	
+
 	// Neo Indexers
 	GeneIndexer("gene", GeneIndexer.class, 4, 359, 359, 8, 1),
 	DatasetIndexer("dataset", DatasetIndexer.class, 4, 566, 566, 8, 1),
 	DiseaseIndexer("disease", DiseaseIndexer.class, 4, 680, 680, 8, 1),
 	AlleleIndexer("allele", AlleleIndexer.class, 4, 1517, 1517, 8, 1),
 	GoIndexer("go", GoIndexer.class, 4, 914, 914, 8, 1),
-	ModelIndexer("model", ModelIndexer.class, 4, 1426, 1426, 8, 1),
-	
+	ModelIndexer("model", ModelIndexer.class, 4, 1500, 1426, 4, 1),
+
 	// Curation Indexers
 	DiseaseAnnotationIndexer("diseaseAnnotation", DiseaseAnnotationCurationIndexer.class, 1, 1500, 1500, 2, 1),
 	GeneGeneticInteractionIndexers("geneGeneticInteraction", GeneGeneticInteractionCurationIndexer.class, 4, 1500, 1500, 2, 1),
@@ -34,6 +21,7 @@ public enum IndexerConfig {
 	PhenotypeAnnotationIndexer("phenotypeAnnotation", PhenotypeAnnotationCurationIndexer.class, 4, 1500, 1500, 2, 1),
 	ReleaseInfoIndexer("release", ReleaseInfoIndexer.class, 1, 1, 1, 1, 1),
 	DiseaseSummaryIndexer("diseaseSummary", DiseaseSummaryCurationIndexer.class, 4, 1500, 1500, 4, 1),
+	AffectedGenomicModelIndexer("affectedGenomicModels", AffectedGenomicModelIndexer.class, 4, 1500, 1500, 8, 1),
 	GeneToGeneOrthologyIndexer("geneToGeneOrthology", GeneToGeneOrthologyIndexer.class, 4, 1500, 1500, 8, 1),
 	GOSearchResultCurationIndexer("goSearchResult", GOSearchResultCurationIndexer.class, 4, 1500, 1500, 8, 1),
 	//GeneExpressionAnnotationIndexer("geneExpressionAnnotation", GeneExpressionAnnotationIndexer.class, 4, 1500, 1500, 2, 1),
---
+++ agr_indexer/src/main/java/org/alliancegenome/indexer/indexers/AffectedGenomicModelIndexer.java
@@ -0,0 +1,71 @@
+package org.alliancegenome.indexer.indexers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.alliancegenome.core.config.ConfigHelper;
+import org.alliancegenome.curation_api.interfaces.document.ModelDocumentInterface;
+import org.alliancegenome.curation_api.model.document.es.AffectedGenomicModelDocument;
+import org.alliancegenome.curation_api.response.SearchResponse;
+import org.alliancegenome.indexer.RestConfig;
+import org.alliancegenome.indexer.config.IndexerConfig;
+import org.apache.commons.collections.CollectionUtils;
+import si.mazi.rescu.RestProxyFactory;
+
+import java.util.HashMap;
+import java.util.concurrent.LinkedBlockingDeque;
+
+@Slf4j
+public class AffectedGenomicModelIndexer extends Indexer {
+
+	private final ModelDocumentInterface modelApi = RestProxyFactory.createProxy(ModelDocumentInterface.class, ConfigHelper.getCurationApiUrl(), RestConfig.config);
+
+	private final HashMap<String, Object> params = new HashMap<>() {{
+		put("internal", false);
+		put("obsolete", false);
+	}};
+
+	public AffectedGenomicModelIndexer(IndexerConfig config) {
+		super(config);
+	}
+
+	@Override
+	protected void index() {
+		try {
+			SearchResponse<AffectedGenomicModelDocument> diseaseSummaryResponse = modelApi.findDocuments(0, 0, params);
+			int totalPages = (int) (diseaseSummaryResponse.getTotalResults() / indexerConfig.getBufferSize());
+			LinkedBlockingDeque<String> queue = new LinkedBlockingDeque<>();
+			for (int i = 0; i <= totalPages; i++) {
+				queue.add(String.valueOf(i));
+			}
+			initiateThreading(queue);
+		} catch (Exception e) {
+			log.error("Error while indexing...", e);
+			System.exit(-1);
+		}
+	}
+
+	@Override
+	protected void startSingleThread(LinkedBlockingDeque<String> queue) {
+		while (true) {
+			try {
+				if (queue.isEmpty()) {
+					return;
+				}
+				String page = queue.takeFirst();
+				SearchResponse<AffectedGenomicModelDocument> response = modelApi.findDocuments(Integer.valueOf(page), indexerConfig.getBufferSize(), params);
+				if (response == null) {
+					return;
+				}
+				// not every affected genomic model has a document associated to a gene
+				if (CollectionUtils.isEmpty(response.getResults())) {
+					continue;
+				}
+				indexDocuments(response.getResults());
+			} catch (Exception e) {
+				log.error("Error while indexing...", e);
+				System.exit(-1);
+				return;
+			}
+		}
+	}
+
+}
