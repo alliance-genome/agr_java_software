9da3c91664953bfc97d3f87e8394b9cbc397e255
Christian Pich <cmpich@zfin.org>
2025-05-26 11:55:16 +0200

    ZFIN-4073 fix filtering on model name

--- agr_java_core/src/main/java/org/alliancegenome/es/index/site/schema/Mapping.java
+++ agr_java_core/src/main/java/org/alliancegenome/es/index/site/schema/Mapping.java
@@ -105,7 +105,7 @@ public class Mapping extends Builder {
 		new FieldBuilder(builder, "whereExpressed", "text").keyword().build(); // gene, dataset
 
 		new FieldBuilder(builder, "associatedSpecies", "text").keyword().synonym().sort().build(); // go, disease
-		new FieldBuilder(builder, "model.agmFullName.displayText", "text").keyword().synonym().sort().build(); // go, disease
+		new FieldBuilder(builder, "model.agmFullName.displayText", "text").keyword().synonym().sort().build(); //
 		new FieldBuilder(builder, "definition", "text").standardText().build(); // go, disease
 		
 		new FieldBuilder(builder, "models", "text").keyword().autocomplete().build(); // gene, disease
