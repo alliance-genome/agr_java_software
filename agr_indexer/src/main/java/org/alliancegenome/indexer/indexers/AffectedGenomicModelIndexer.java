c69b71f0381b1d07a0dd4491164fab8577fa396d
Christian Pich <cmpich@zfin.org>
2025-06-23 16:11:07 +0200

    Add DEBUG INFO. Will remove later.

--- agr_indexer/src/main/java/org/alliancegenome/indexer/indexers/AffectedGenomicModelIndexer.java
+++ agr_indexer/src/main/java/org/alliancegenome/indexer/indexers/AffectedGenomicModelIndexer.java
@@ -64,7 +64,7 @@ public class AffectedGenomicModelIndexer extends Indexer {
 				response.getResults().stream()
 					.filter(affectedGenomicModelDocument -> affectedGenomicModelDocument.getModel().getPrimaryExternalId().equals("ZFIN:ZDB-FISH-150901-10520")
 						|| affectedGenomicModelDocument.getModel().getPrimaryExternalId().equals("ZFIN:ZDB-FISH-150901-4211"))
-					.forEach(affectedGenomicModelDocument -> System.out.println("DEBUG_MODE: " + affectedGenomicModelDocument.getModel()));
+					.forEach(affectedGenomicModelDocument -> log.info("DEBUG_MODE: " + affectedGenomicModelDocument.getModel()));
 
 				indexDocuments(response.getResults());
 			} catch (Exception e) {
