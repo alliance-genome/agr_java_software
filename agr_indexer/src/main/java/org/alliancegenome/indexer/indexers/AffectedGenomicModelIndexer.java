de3421c3d1c33d2d5c56913a2addce48d5342f2a
Christian Pich <cmpich@zfin.org>
2025-06-25 16:11:30 +0200

    SCRUM-4073 remove debug code, fix disease sorting field (#1345)

--- agr_indexer/src/main/java/org/alliancegenome/indexer/indexers/AffectedGenomicModelIndexer.java
+++ agr_indexer/src/main/java/org/alliancegenome/indexer/indexers/AffectedGenomicModelIndexer.java
@@ -61,11 +61,6 @@ public class AffectedGenomicModelIndexer extends Indexer {
 				if (CollectionUtils.isEmpty(response.getResults())) {
 					continue;
 				}
-				response.getResults().stream()
-					.filter(affectedGenomicModelDocument -> affectedGenomicModelDocument.getModel().getPrimaryExternalId().equals("ZFIN:ZDB-FISH-150901-10520")
-						|| affectedGenomicModelDocument.getModel().getPrimaryExternalId().equals("ZFIN:ZDB-FISH-150901-4211"))
-					.forEach(affectedGenomicModelDocument -> log.info("DEBUG_MODE: " + affectedGenomicModelDocument.getModel()));
-
 				indexDocuments(response.getResults());
 			} catch (Exception e) {
 				log.error("Error while indexing...", e);
--- agr_java_core/src/main/java/org/alliancegenome/es/index/site/schema/VariantMapping.java
+++ agr_java_core/src/main/java/org/alliancegenome/es/index/site/schema/VariantMapping.java
@@ -18,7 +18,7 @@ public class VariantMapping extends Mapping {
 			new FieldBuilder(builder, "alterationType", "text").keyword().build();
 			new FieldBuilder(builder, "genes", "text").keyword().build();
 			new FieldBuilder(builder, "associatedPhenotype", "text").keyword().sort().build();
-			new FieldBuilder(builder, "diseaseTerms", "text").keyword().sort().build();
+			new FieldBuilder(builder, "diseaseTerms.name", "text").keyword().sort().build();
 
 			builder.startObject("allele");
 			builder.startObject("properties");
