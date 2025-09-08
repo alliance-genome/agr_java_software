9cb38248e90c6ee0d071b8924d3dabd037f7bcce
Christian Pich <cmpich@zfin.org>
2025-09-08 21:37:51 +0200

    SCRUM-4072 new indexer for transgenic allele summary docs

--- agr_indexer/src/main/java/org/alliancegenome/indexer/indexers/TransgenicAlleleIndexer.java
+++ agr_indexer/src/main/java/org/alliancegenome/indexer/indexers/TransgenicAlleleIndexer.java
@@ -2,16 +2,22 @@ package org.alliancegenome.indexer.indexers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.alliancegenome.api.entity.TransgenicAlleleSummaryDocument;
 import org.alliancegenome.core.config.ConfigHelper;
 import org.alliancegenome.curation_api.interfaces.document.TransgenicAlleleDocumentInterface;
-import org.alliancegenome.curation_api.model.document.es.TransgenicAlleleDocument;
+import org.alliancegenome.curation_api.model.document.es.TransgenicAlleleDTO;
+import org.alliancegenome.curation_api.model.entities.Allele;
+import org.alliancegenome.curation_api.model.entities.Construct;
+import org.alliancegenome.curation_api.model.entities.Gene;
+import org.alliancegenome.curation_api.model.entities.TransgenicAlleleConstruct;
 import org.alliancegenome.curation_api.response.SearchResponse;
+import org.alliancegenome.es.util.ProcessDisplayHelper;
 import org.alliancegenome.indexer.RestConfig;
 import org.alliancegenome.indexer.config.IndexerConfig;
-import org.apache.commons.collections.CollectionUtils;
+import org.jetbrains.annotations.NotNull;
 import si.mazi.rescu.RestProxyFactory;
 
-import java.util.HashMap;
+import java.util.*;
 import java.util.concurrent.LinkedBlockingDeque;
 
 @Slf4j
@@ -32,43 +38,67 @@ public class TransgenicAlleleIndexer extends Indexer {
 
 	@Override
 	protected void index() {
-		try {
-			SearchResponse<TransgenicAlleleDocument> diseaseSummaryResponse = transgenicAlleleApi.findDocuments(0, 0, params);
-			int totalPages = (int) (diseaseSummaryResponse.getTotalResults() / indexerConfig.getBufferSize());
-			LinkedBlockingDeque<String> queue = new LinkedBlockingDeque<>();
-			for (int i = 0; i <= totalPages; i++) {
-				queue.add(String.valueOf(i));
-			}
-			initiateThreading(queue);
-		} catch (Exception e) {
-			log.error("Error while indexing...", e);
-			System.exit(-1);
-		}
+		indexTransgenicAlleleSummary();
 	}
+
+	private void indexTransgenicAlleleSummary() {
+		SearchResponse<TransgenicAlleleDTO> diseaseSummaryResponse = transgenicAlleleApi.findDocuments(0, 0, params);
+		ProcessDisplayHelper display = new ProcessDisplayHelper(2000);
+		display.startProcess("Pulling Transgenic Alleles from curation", diseaseSummaryResponse.getTotalResults());
+		Map<Allele, TransgenicAlleleSummaryDocument> documentMap = new LinkedHashMap<>();
+		int batchSize = 500;
+		int maxPage = (int) (diseaseSummaryResponse.getTotalResults() / batchSize);
+		for (int page = 0; page <= maxPage; page++) {
+			SearchResponse<TransgenicAlleleDTO> response = transgenicAlleleApi.findDocuments(page, batchSize, params);
+			for (TransgenicAlleleDTO da : response.getResults()) {
+				if (da == null) {
+					continue;
+				}
+				TransgenicAlleleSummaryDocument document = documentMap.computeIfAbsent(da.getAllele(), allele -> {
+					TransgenicAlleleSummaryDocument doc = new TransgenicAlleleSummaryDocument();
+					doc.setAllele(allele);
+					return doc;
+				});
+				List<TransgenicAlleleConstruct> constructList = document.getTransgenicAlleleConstructs();
+				if (constructList == null) {
+					constructList = new ArrayList<>();
+					document.setTransgenicAlleleConstructs(constructList);
+				}
+				TransgenicAlleleConstruct construct = new TransgenicAlleleConstruct();
+				construct.setConstruct(da.getConstruct());
+				construct.setExpressedGenes(getExpressedGenes(da.getConstruct()));
+				construct.setRegulatoryGenes(getRegulatoryGenes(da.getConstruct()));
+				constructList.add(construct);
+			}
+			display.progressProcess(response.getReturnedRecords().longValue());
+		}
+		indexDocuments(documentMap.values());
+	}
+
+	private List<Gene> getRegulatoryGenes(Construct construct) {
+		return getGenes(construct, "is_regulated_by");
+	}
+
+	@NotNull
+	private static List<Gene> getGenes(Construct construct, String relationName) {
+		List<Gene> expressedGenes = new ArrayList<>();
+		construct.getConstructGenomicEntityAssociations().forEach(constructGenomicEntityAssociation -> {
+			if (constructGenomicEntityAssociation.getConstructGenomicEntityAssociationObject() instanceof Gene gene) {
+				if (constructGenomicEntityAssociation.getRelation().getName().equals(relationName)) {
+					expressedGenes.add(gene);
+				}
+			}
+		});
+		return expressedGenes;
+	}
+
+	private List<Gene> getExpressedGenes(Construct construct) {
+		return getGenes(construct, "expresses");
+	}
+
 
 	@Override
 	protected void startSingleThread(LinkedBlockingDeque<String> queue) {
-		while (true) {
-			try {
-				if (queue.isEmpty()) {
-					return;
-				}
-				String page = queue.takeFirst();
-				SearchResponse<TransgenicAlleleDocument> response = transgenicAlleleApi.findDocuments(Integer.valueOf(page), indexerConfig.getBufferSize(), params);
-				if (response == null) {
-					return;
-				}
-				// not every affected genomic model has a document associated to a gene
-				if (CollectionUtils.isEmpty(response.getResults())) {
-					continue;
-				}
-				indexDocuments(response.getResults());
-			} catch (Exception e) {
-				log.error("Error while indexing...", e);
-				System.exit(-1);
-				return;
-			}
-		}
 	}
 
 	@Override
---
+++ agr_java_core/src/main/java/org/alliancegenome/api/entity/TransgenicAlleleSummaryDocument.java
@@ -0,0 +1,30 @@
+package org.alliancegenome.api.entity;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.JsonView;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.alliancegenome.curation_api.model.document.es.ESDocument;
+import org.alliancegenome.curation_api.model.entities.Allele;
+import org.alliancegenome.curation_api.model.entities.TransgenicAlleleConstruct;
+import org.alliancegenome.neo4j.view.View;
+
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@JsonPropertyOrder({"allele", "transgenicAlleleConstructs"})
+@JsonView({View.TransgenicAlleleAPI.class})
+public class TransgenicAlleleSummaryDocument extends ESDocument {
+
+	public TransgenicAlleleSummaryDocument() {
+		setCategory("transgenic_allele_summary");
+	}
+
+	private Allele allele;
+	public List<TransgenicAlleleConstruct> transgenicAlleleConstructs;
+	private Boolean hasDiseaseAnnotations;
+	private Boolean hasPhenotypeAnnotations;
+
+
+}
