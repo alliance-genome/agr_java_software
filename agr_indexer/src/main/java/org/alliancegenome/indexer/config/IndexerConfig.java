4f56b14bfd13368d46348e5431865b7a5b9f799e
Christian Pich <cmpich@zfin.org>
2025-09-24 20:34:48 +0200

    do not run the GeneToGeneOrthologyIndexer in parallel (#1389)

--- agr_indexer/src/main/java/org/alliancegenome/indexer/config/IndexerConfig.java
+++ agr_indexer/src/main/java/org/alliancegenome/indexer/config/IndexerConfig.java
@@ -44,7 +44,7 @@ public enum IndexerConfig {
 	AlleleSummaryIndexer("alleleSummary", AlleleSummaryCurationIndexer.class, 4, 500, 500, 4, 1, true),
 	AffectedGenomicModelIndexer("affectedGenomicModels", AffectedGenomicModelIndexer.class, 4, 1500, 1500, 8, 1, true),
 	TransgenicAlleleIndexer("transgenicAlleles", TransgenicAlleleIndexer.class, 1, 1500, 1500, 8, 1, true),
-	GeneToGeneOrthologyIndexer("geneToGeneOrthology", GeneToGeneOrthologyIndexer.class, 4, 1500, 1500, 8, 1, true),
+	GeneToGeneOrthologyIndexer("geneToGeneOrthology", GeneToGeneOrthologyIndexer.class, 4, 1500, 1500, 8, 1, false),
 	GOSearchResultCurationIndexer("goSearchResult", GOSearchResultCurationIndexer.class, 4, 1500, 1500, 8, 1, true),
 	//GeneExpressionAnnotationIndexer("geneExpressionAnnotation", GeneExpressionAnnotationIndexer.class, 4, 1500, 1500, 2, 1),
 	//GeneSearchResultCurationIndexer("geneSearchResult", GeneSearchResultCurationIndexer.class, 4, 250, 1000, 4, 1),
