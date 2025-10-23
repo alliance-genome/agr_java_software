e65ee6d2e31d393dd3f1f3f580e3673136056f00
VarunReddy1111 <varunreddy.go@gmail.com>
2025-10-23 13:21:17 -0500

    Added alpha sort for gene expression annotations

--- agr_api/src/main/java/org/alliancegenome/api/service/ExpressionESService.java
+++ agr_api/src/main/java/org/alliancegenome/api/service/ExpressionESService.java
@@ -75,11 +75,11 @@ public class ExpressionESService extends ESService {
 
 		SpeciesType type = SpeciesType.getTypeByID(focusTaxonId);
 
-		sortingSetMap.put("species", "geneExpressionAnnotation.expressionAnnotationSubject.taxon.curie.keyword");
-		sortingSetMap.put("gene", "geneExpressionAnnotation.expressionAnnotationSubject.geneSymbol.displayText.keyword");
-		sortingSetMap.put("location", "geneExpressionAnnotation.whereExpressedStatement.keyword");
-		sortingSetMap.put("stage", "geneExpressionAnnotation.whenExpressedStageName.keyword");
-		sortingSetMap.put("assay", "geneExpressionAnnotation.expressionAssayUsed.name.keyword");
+		sortingSetMap.put("species", "geneExpressionAnnotation.expressionAnnotationSubject.taxon.name.keyword");
+		sortingSetMap.put("gene", "geneExpressionAnnotation.expressionAnnotationSubject.geneSymbol.displayText.sort");
+		sortingSetMap.put("location", "geneExpressionAnnotation.whereExpressedStatement.sort");
+		sortingSetMap.put("stage", "geneExpressionAnnotation.whenExpressedStageName.sort");
+		sortingSetMap.put("assay", "geneExpressionAnnotation.expressionAssayUsed.name.sort");
 		sortingSetMap.put("default", "speciesOrder." + type.getTaxonIDPart());
 
 		String sortField = sortingSetMap.get(pagination.getSortBy());
