5933f05bd2cffa0b57a8c34db0c84f1cddbb14ce
VarunReddy1111 <varunreddy.go@gmail.com>
2025-11-13 17:48:00 -0600

    Gene expression annotations downloads de consolidation changes

--- agr_java_core/src/main/java/org/alliancegenome/core/translators/tdf/ExpressionToTdfTranslator.java
+++ agr_java_core/src/main/java/org/alliancegenome/core/translators/tdf/ExpressionToTdfTranslator.java
@@ -1,10 +1,12 @@
 package org.alliancegenome.core.translators.tdf;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.StringJoiner;
 
 import org.alliancegenome.core.config.ConfigHelper;
 import org.alliancegenome.curation_api.model.document.es.GeneExpressionDocument;
+import org.apache.commons.collections.CollectionUtils;
 
 public class ExpressionToTdfTranslator {
 
@@ -25,32 +27,36 @@ public class ExpressionToTdfTranslator {
 		builder.append(ConfigHelper.getJavaLineSeparator());
 
 		annotations.forEach(expressionDetail -> {
-			StringJoiner joiner = new StringJoiner("\t");
-			if (isMultipleGenes) {
-				joiner.add(expressionDetail.getGeneExpressionAnnotation().getExpressionAnnotationSubject().getTaxon().getName());
-				joiner.add(expressionDetail.getGeneExpressionAnnotation().getExpressionAnnotationSubject().getGeneSymbol().getDisplayText());
-				joiner.add(expressionDetail.getGeneExpressionAnnotation().getExpressionAnnotationSubject().getPrimaryExternalId());
+
+			int crossRefSize = CollectionUtils.isNotEmpty(expressionDetail.getGeneExpressionAnnotation().getCrossReferences()) ? expressionDetail.getGeneExpressionAnnotation().getCrossReferences().size() : 0;
+			int pubSize = CollectionUtils.isNotEmpty(expressionDetail.getReferenceId()) ? expressionDetail.getReferenceId().size() : 0;
+
+			int numOfAnnotations = Math.max(crossRefSize, pubSize);
+			for (int i=0;i<numOfAnnotations;i++) {
+				StringJoiner joiner = new StringJoiner("\t");
+				if (isMultipleGenes) {
+					joiner.add(expressionDetail.getGeneExpressionAnnotation().getExpressionAnnotationSubject().getTaxon().getName());
+					joiner.add(expressionDetail.getGeneExpressionAnnotation().getExpressionAnnotationSubject().getGeneSymbol().getDisplayText());
+					joiner.add(expressionDetail.getGeneExpressionAnnotation().getExpressionAnnotationSubject().getPrimaryExternalId());
+				}
+				joiner.add(expressionDetail.getGeneExpressionAnnotation().getWhereExpressedStatement());
+				joiner.add(expressionDetail.getGeneExpressionAnnotation().getWhenExpressedStageName());
+				joiner.add(expressionDetail.getGeneExpressionAnnotation().getExpressionAssayUsed().getName());
+
+				String crossRefs = "";
+				if (CollectionUtils.isNotEmpty(expressionDetail.getGeneExpressionAnnotation().getCrossReferences()) && i<crossRefSize) {
+					crossRefs = expressionDetail.getGeneExpressionAnnotation().getCrossReferences().get(i).getDisplayName();
+				}
+				joiner.add(crossRefs);
+
+				String publications = "";
+				if (CollectionUtils.isNotEmpty(expressionDetail.getReferenceId()) && i<pubSize) {
+					publications = expressionDetail.getReferenceId().get(i);
+				}
+				joiner.add(publications);
+				builder.append(joiner.toString());
+				builder.append(ConfigHelper.getJavaLineSeparator());
 			}
-			joiner.add(expressionDetail.getGeneExpressionAnnotation().getWhereExpressedStatement());
-			joiner.add(expressionDetail.getGeneExpressionAnnotation().getWhenExpressedStageName());
-			joiner.add(expressionDetail.getGeneExpressionAnnotation().getExpressionAssayUsed().getName());
-			String crossRefs = "";
-			if (expressionDetail.getGeneExpressionAnnotation().getCrossReferences() != null) {
-				StringJoiner crossRefJoiner = new StringJoiner(",");
-				expressionDetail.getGeneExpressionAnnotation().getCrossReferences().forEach(crossReference -> crossRefJoiner.add(crossReference.getDisplayName()));
-				crossRefs = crossRefJoiner.toString();
-			}
-			joiner.add(crossRefs);
-			// add list of publications
-			String publications = "";
-			if (expressionDetail.getReferenceId() != null) {
-				StringJoiner pubJoiner = new StringJoiner(",");
-				expressionDetail.getReferenceId().forEach(reference -> pubJoiner.add(reference));
-				publications = pubJoiner.toString();
-			}
-			joiner.add(publications);
-			builder.append(joiner.toString());
-			builder.append(ConfigHelper.getJavaLineSeparator());
 		});
 
 		return builder.toString();
