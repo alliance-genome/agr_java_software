60f9a32ea204f9d6d7b364f46d01e3d5a2f7b4bd
Christian Pich <cmpich@zfin.org>
2025-11-18 21:11:54 +0100

    Scrum 5363 (#1430)
    * SCRUM-4072 fix sorting variable
    
    * SCRUM-5363 Indexer for Variant Summary
    
    * SCRUM-5363 remove unused code
    
    * upgrade to 0.43.9

---
+++ agr_java_core/src/main/java/org/alliancegenome/api/entity/VariantSummaryDocument.java
@@ -0,0 +1,29 @@
+package org.alliancegenome.api.entity;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.JsonView;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.alliancegenome.curation_api.model.document.es.ESDocument;
+import org.alliancegenome.curation_api.model.entities.Allele;
+import org.alliancegenome.curation_api.model.entities.associations.CuratedVariantGenomicLocationAssociation;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@JsonPropertyOrder({"category", "variant"})
+@JsonView(value = {org.alliancegenome.curation_api.view.View.TransgenicAllelesDocument.class})
+public class VariantSummaryDocument extends ESDocument {
+
+	{
+		category = "variant_summary";
+	}
+
+	public VariantSummaryDocument() {
+
+	}
+
+	public CuratedVariantGenomicLocationAssociation variant;
+	public Allele allele;
+
+
+}
