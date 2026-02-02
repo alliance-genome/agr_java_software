fabb2b8e758fb0d4b97ca317bc3fbb7b80d4551e
VarunReddy1111 <varunreddy.go@gmail.com>
2026-02-01 22:46:38 -0600

    Fixed IT

--- agr_api/src/test/java/org/alliancegenome/api/tests/integration/AlleleIT.java
+++ agr_api/src/test/java/org/alliancegenome/api/tests/integration/AlleleIT.java
@@ -27,6 +27,7 @@ import org.alliancegenome.api.service.VariantService;
 import org.alliancegenome.cache.CacheAlliance;
 import org.alliancegenome.cache.repository.helper.JsonResultResponse;
 import org.alliancegenome.core.config.ConfigHelper;
+import org.alliancegenome.curation_api.model.document.es.AlleleSummaryDocument;
 import org.alliancegenome.es.model.query.FieldFilter;
 import org.alliancegenome.es.model.query.Pagination;
 import org.alliancegenome.neo4j.entity.DiseaseAnnotation;
@@ -417,7 +418,7 @@ public class AlleleIT {
 	@Ignore
 	public void checkAllelesGeneTableSynonymFilter() {
 		GeneController ctrl = new GeneController();
-		JsonResultResponse<Allele> response = ctrl.getAllelesPerGene("RGD:2219", 10, 1, "", "true", "", "brc", "", "", "", "", "allele");
+		JsonResultResponse<AlleleSummaryDocument> response = ctrl.getAllelesPerGene("RGD:2219", 10, 1, "", "true", "", "brc", "", "", "", "", "allele");
 		assertNotNull(response);
 
 	}
