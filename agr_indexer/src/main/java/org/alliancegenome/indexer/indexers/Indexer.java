bbd94b264f675d629d8002ae4809e99a683d2c10
Olin Blodgett <nilo@nilosplace.net>
2026-02-10 17:41:00 -0500

    Refactored the gene expression indexer

--- agr_indexer/src/main/java/org/alliancegenome/indexer/indexers/Indexer.java
+++ agr_indexer/src/main/java/org/alliancegenome/indexer/indexers/Indexer.java
@@ -201,8 +201,10 @@ public abstract class Indexer extends Thread {
 
 	protected <T> List<List<T>> partition(List<T> list, int size) {
 		List<List<T>> parts = new ArrayList<>();
-		for (int i = 0; i < list.size(); i += size) {
-			parts.add(new ArrayList<>(list.subList(i, Math.min(i + size, list.size()))));
+		if(list != null) {
+			for (int i = 0; i < list.size(); i += size) {
+				parts.add(new ArrayList<>(list.subList(i, Math.min(i + size, list.size()))));
+			}
 		}
 		return parts;
 	}
