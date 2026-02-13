b6ef444453aa6540fab2fc6f63a3daf098cc822c
VarunReddy1111 <varunreddy.go@gmail.com>
2026-02-13 09:30:34 -0600

    IT Tests fix

--- agr_api/src/test/java/org/alliancegenome/api/tests/unit/TestES.java
+++ agr_api/src/test/java/org/alliancegenome/api/tests/unit/TestES.java
@@ -13,7 +13,7 @@ public class TestES {
 	
 	public static void main(String[] args) throws IOException {
 		GeneController ctrl = new GeneController();
-		JsonResultResponse<AlleleSummaryDocument> res = ctrl.getAllelesPerGene("HGNC:6190", 10, 1, null, "true", "", "", "", "intron_variant", "", "", "allele|allele with multiple associated variants|allele with one associated variant|variant");
+		JsonResultResponse<AlleleSummaryDocument> res = ctrl.getAllelesPerGene("HGNC:6190", 10, 1, null, "true", "", "", "", "", "intron_variant", "", "", "allele|allele with multiple associated variants|allele with one associated variant|variant");
 		System.out.println("DONE!!" + "RESULTS SIZE:" + res.getResults().size());
 		EsClientFactory.getDefaultEsClient().close();
 
