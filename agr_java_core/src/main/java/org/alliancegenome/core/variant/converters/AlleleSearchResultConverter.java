b0369a3481be41f331149799e6bb5058dff0a9ac
Christian Pich <cmpich@zfin.org>
2026-03-27 05:09:52 +0100

    rename attributes

--- agr_java_core/src/main/java/org/alliancegenome/core/variant/converters/AlleleSearchResultConverter.java
+++ agr_java_core/src/main/java/org/alliancegenome/core/variant/converters/AlleleSearchResultConverter.java
@@ -125,7 +125,7 @@ public class AlleleSearchResultConverter {
 					.distinct()
 					.toList();
 				if (!variantTypes.isEmpty()) {
-					searchDoc.setVariantTypes(variantTypes);
+					searchDoc.setVariantType(variantTypes);
 				}
 
 				Set<String> consequences = new HashSet<>();
@@ -150,7 +150,7 @@ public class AlleleSearchResultConverter {
 					}
 				}
 				if (!consequences.isEmpty()) {
-					searchDoc.setMolecularConsequences(consequences);
+					searchDoc.setMolecularConsequence(consequences);
 				}
 			}
 
--- agr_java_core/src/main/java/org/alliancegenome/es/model/AlleleSearchResultDocument.java
+++ agr_java_core/src/main/java/org/alliancegenome/es/model/AlleleSearchResultDocument.java
@@ -29,10 +29,8 @@ public class AlleleSearchResultDocument extends ESDocument {
 	private String species;
 	private Double popularity;
 	private String alterationType;
-	@JsonProperty("variantType")
-	private List<String> variantTypes;
-	@JsonProperty("molecularConsequence")
-	private Set<String> molecularConsequences;
+	private List<String> variantType;
+	private Set<String> molecularConsequence;
 	private Set<String> constructExpressedComponent;
 	private Set<String> constructRegulatoryRegion;
 	private Set<String> constructKnockdownComponent;
