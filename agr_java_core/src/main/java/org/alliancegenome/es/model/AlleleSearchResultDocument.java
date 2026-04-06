9c6e0cda34018862391c7684f199f4b257f00d61
Christian Pich <cmpich@zfin.org>
2026-04-06 15:51:58 +0200

    SCRUM-5881 add gene synonyms to allele search doc

--- agr_java_core/src/main/java/org/alliancegenome/es/model/AlleleSearchResultDocument.java
+++ agr_java_core/src/main/java/org/alliancegenome/es/model/AlleleSearchResultDocument.java
@@ -31,6 +31,7 @@ public class AlleleSearchResultDocument extends ESDocument {
 	private Set<String> constructRegulatoryRegion;
 	private Set<String> constructKnockdownComponent;
 	private List<String> synonyms;
+	private Set<String> geneSynonyms;
 	private List<String> secondaryIds;
 	private List<String> genes;
 	private List<String> diseases;
