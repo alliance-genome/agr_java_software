c8537d425a8e41c5c1100ac611684201ac929790
Olin Blodgett <nilo@nilosplace.net>
2026-05-06 18:52:25 -0400

    SCRUM-6020 populate VariantCrossReferences with allele primaryExternalId

--- agr_file_generator/src/main/java/org/alliancegenome/filegenerator/config/FileGeneratorConfig.java
+++ agr_file_generator/src/main/java/org/alliancegenome/filegenerator/config/FileGeneratorConfig.java
@@ -319,7 +319,7 @@ public enum FileGeneratorConfig {
 		m.put("VariantId", "_variantId");
 		m.put("VariantSymbol", "_variantSymbol");
 		m.put("VariantSynonyms", "_variantSynonyms");
-		m.put("VariantCrossReferences", "_unavailable");
+		m.put("VariantCrossReferences", "allele.primaryExternalId");
 		m.put("AlleleAssociatedGeneId", "alleleOfGene.primaryExternalId");
 		m.put("AlleleAssociatedGeneSymbol", "alleleOfGene.geneSymbol.displayText");
 		m.put("VariantAffectedGeneId", "_variantAffectedGeneId");
