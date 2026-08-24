f0774c1d61d0265882190881d36422b75e4053d9
Olin Blodgett <nilo@nilosplace.net>
2026-08-24 10:58:55 -0400

    SCRUM-6307 Publicize Alliance API Changes mailing list on the Swagger page
    Add a notice to the OpenAPI info description pointing 3rd party API
    consumers at the new alliance-api-changes mailing list, with hyperlinks
    to the release notes page and the mailman sign up page.

--- agr_api/src/main/java/org/alliancegenome/api/application/RestApplication.java
+++ agr_api/src/main/java/org/alliancegenome/api/application/RestApplication.java
@@ -9,7 +9,21 @@ import jakarta.ws.rs.core.Application;
 @ApplicationPath("/api")
 @OpenAPIDefinition(
 	info = @Info(
-		description = "This is the Alliance Genome Java API for access to the Data",
+		description = """
+			This is the Alliance Genome Java API for access to the Data
+
+			**Please note:** A mailing list has been established to alert developers that make use of \
+			Alliance of Genome Resources public APIs to any potentially breaking changes to, or new \
+			data added to, our APIs. Alerts will be announced by email, but can also be seen on the \
+			Alliance Release Notes page:
+
+			[https://www.alliancegenome.org/release-notes](https://www.alliancegenome.org/release-notes)
+
+			Archives of all prior messages to this list will be available at the Archive available at \
+			the URL below. Please register/subscribe for this mailing list at the following URL:
+
+			[https://mailman.stanford.edu/mailman/listinfo/alliance-api-changes](https://mailman.stanford.edu/mailman/listinfo/alliance-api-changes)
+			""",
 		title = "Alliance of Genome Resources API",
 		version = "1.0 Beta"
 	)
